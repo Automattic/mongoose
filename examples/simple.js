625bcd5980ca95c736b018c7900b2e9b3dcfd91e
Nathan White <nw@nwhite.net>
2010-05-14 16:25:48 -0400

    looking at deprecating noSchema, 'use' instead

--- examples/simple.js
+++ examples/simple.js
@@ -1,18 +1,21 @@
 var sys = require('sys'),
+    object = require('../lib/utils/object'),
     inspect = function(item){ sys.puts(sys.inspect(item)); },
     Mongoose = require('../../mongoose/').Mongoose, db, Simple;
     
     db = Mongoose.connect('mongodb://localhost/test'); // connect to mongo
     
     Simple = Mongoose.noSchema('simple',db); // no model, direct access to 'simple' collection.
+    Simple.drop(); // empties collection
     
     var s1 = new Simple({x : 2, y : 3}).save(); // create a new document and save
-    
-    Simple.find().each(function(doc){ // query for documents in 'simple' collection
+
+    Simple.find().one(function(doc){ // query for documents in 'simple' collection
+    sys.puts('in each');
       inspect(doc); // output
-    }).then(function(){ // promise (execute after query)
-      Simple.drop(function(){ // clear collection
-        Simple.close(); // close event loop
-      });
+      doc.y = 5;
+      doc.save();
+    },true).then(function(){ // promise (execute after query)
+       Simple.close(); // close event loop
     });
     

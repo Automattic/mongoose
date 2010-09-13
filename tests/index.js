8e0ccf8144314ef327745411c3caf571ddf8d90d
Nathan White <nw@nwhite.net>
2010-09-13 14:28:41 -0700

    fixed connect test

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,4 @@
-var assert = require('assert'),
-    mongoose = require('../');
+var assert = require('assert');
 
 function now(){
   return Math.round(Date.now() + Math.random() * 100);
@@ -8,26 +7,26 @@ function now(){
 function timeout(goose){
   return setTimeout(function(){
     assert.ok(false, 'Connection timeout');
-  }, 5000);
+  },5000);
 }
 
 module.exports = {
   
-  'test connecting to mongodb (uri / callback signature)': function(){
-    var timer = timeout(mongoose);
+  'test connecting to mongodb': function(){
+    var mongoose = require('../'),
+        timer = timeout(mongoose);
     mongoose.connect('mongodb://localhost/' + now(), function(){
       clearTimeout(timer);
-      assert.ok(mongoose.connected, 'Connected with uri/callback signature');
+      assert.ok(mongoose.connected, 'Connected using uri / callback signature');
+      
       mongoose.disconnect(function(){
         assert.ok(!mongoose.connected);
         
         var timer = timeout(mongoose);
         mongoose.connect('mongodb://localhost/' + now(), { some: 'option' }, function(){
           clearTimeout(timer);
-          assert.ok(mongoose.connected, 'Connected with uri/options/callback signature');
-          mongoose.disconnect(function(){
-            assert.ok(!mongoose.connected);
-          });
+          assert.ok(mongoose.connected, 'Connected using uri / options / callback signature');
+          mongoose.disconnect();
         });
       });
     });

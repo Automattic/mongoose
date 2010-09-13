793317654600b85cff8fd3bc70cd355562f1c834
Guillermo Rauch <rauchg@gmail.com>
2010-09-13 16:45:45 -0700

    Added placeholders for documents Added (failing) test for schema structure

--- tests/schema.js
+++ tests/schema.js
@@ -1,5 +1,6 @@
 var assert = require('assert')
-  , Schema = require('../lib/mongoose/schema');
+  , Schema = require('../lib/mongoose/schema')
+  , TypeSchema = require('../lib/mongoose/type')
 
 module.exports = {
   
@@ -8,6 +9,41 @@ module.exports = {
     assert.ok(a.indexes({}) == a);
     assert.ok(a.setters({}) == a);
     assert.ok(a.getters({}) == a);
+  },
+  
+  'test schema compilation': function(){
+    var a = new Schema('Model');
+    a.string('name')
+        .validate(function(){
+          
+        })
+        .get(function(){
+          
+        })
+        .set(function(){
+          
+        })
+      .number('age')
+        .validate(function(){
+          
+        })
+        .get(function(){
+          
+        })
+        .index(-1)
+      .array('interests', new Schema()
+                            .string('title')
+                            .date('created_at'));
+    
+    var paths = a.paths;
+    assert.ok(paths['name'] instanceof TypeSchema);
+    assert.ok(paths['age'] instanceof TypeSchema);
+    assert.ok(paths['interests'] instanceof TypeSchema);
+    assert.ok(paths['interests.title'] === undefined);
+    
+    a._compile();
+    
+    assert.ok(a.paths['interests.title'] instanceof TypeSchema);
   }
   
 };

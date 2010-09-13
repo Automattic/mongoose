020b9c062a4dd3cf55f9e15783edea5719e0d79f
Guillermo Rauch <rauchg@gmail.com>
2010-09-13 14:06:04 -0700

    Connection tests passing Faked disconnect callback

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
-var assert = require('assert');
+var assert = require('assert'),
+    mongoose = require('../');
 
 function now(){
   return Math.round(Date.now() + Math.random() * 100);
@@ -12,21 +13,21 @@ function timeout(goose){
 
 module.exports = {
   
-  'test connecting to mongodb': function(){
-    var mongoose = require('../'),
-        timer = timeout(mongoose);
+  'test connecting to mongodb (uri / callback signature)': function(){
+    var timer = timeout(mongoose);
     mongoose.connect('mongodb://localhost/' + now(), function(){
       clearTimeout(timer);
-      assert.ok(mongoose.connected, 'Connected using uri / callback signature');
-      
+      assert.ok(mongoose.connected, 'Connected with uri/callback signature');
       mongoose.disconnect(function(){
         assert.ok(!mongoose.connected);
         
         var timer = timeout(mongoose);
         mongoose.connect('mongodb://localhost/' + now(), { some: 'option' }, function(){
           clearTimeout(timer);
-          assert.ok(mongoose.connected, 'Connected using uri / options / callback signature');
-          mongoose.disconnect();
+          assert.ok(mongoose.connected, 'Connected with uri/options/callback signature');
+          mongoose.disconnect(function(){
+            assert.ok(!mongoose.connected);
+          });
         });
       });
     });

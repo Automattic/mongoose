6dd22c5cc5e5049b10654b9b677e10f94ab8db00
Guillermo Rauch <rauchg@gmail.com>
2010-09-12 17:57:56 -0700

    Restructured submodules into ./support Added string pluralization into util.js + tests Beginnings of schema declarator class

--- Makefile
+++ Makefile
@@ -1,6 +1,2 @@
-SH = sh
-NODE = node
-
 test:
-	@$(NODE) tests/unit/run.js
-	tests/integration/support/expresso/bin/expresso tests/integration/*.js
+	support/expresso/bin/expresso tests/*.js
---
+++ tests/util.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+
+var testStrings = [
+  'person','man','woman','child','sex','move','Person','Man','Woman','Child',
+  'Sex','Move','equipment','information','rice','money','species','series',
+  'fish','sheep','deer','Equipment','Information','Rice','Money','Species',
+  'Series','Fish','Sheep','Deer','octopus','wolf','potato','fool','blue',
+  'bus','student','tuna','Octopus','Wolf','Potato','Fool','Blue','Bus',
+  'Student','Tuna','news','News','mouse','Mouse','information','Information',
+  'ox','Ox','virus','Virus','archive','Archive','louse','Louse','curve','Curve'
+];
+
+var pluralize = require('../lib/mongoose/util').string.pluralize;
+
+module.exports = {
+  
+  'test model name pluralization': function(){
+    assert.ok(pluralize('person') == 'people');
+    assert.ok(pluralize('woman') == 'women');
+    assert.ok(pluralize('sheep') == 'sheep');
+    assert.ok(pluralize('information') == 'information');
+    assert.ok(pluralize('money') == 'money');
+    assert.ok(pluralize('virus') == 'viruses');
+    assert.ok(pluralize('test') == 'tests');
+    assert.ok(pluralize('bully') == 'bullies');
+    assert.ok(pluralize('survey') == 'surveys');
+  }
+  
+};

cfe6a710ef4063d689b85ccda516fd145e8150b8
Nathan White <nw@nwhite.net>
2010-09-13 17:56:07 -0700

    added tests for type definitions and schema chaining

--- tests/schema.js
+++ tests/schema.js
@@ -1,5 +1,6 @@
 var assert = require('assert')
-  , Schema = require('../lib/mongoose/schema');
+  , Schema = require('../lib/mongoose/schema')
+  , TypeSchema = require('../lib/mongoose/type');
 
 module.exports = {
   
@@ -8,6 +9,13 @@ module.exports = {
     assert.ok(a.indexes({}) == a);
     assert.ok(a.setters({}) == a);
     assert.ok(a.getters({}) == a);
+  },
+  
+  'test standard types': function(){
+    var a = new Schema();
+    assert.ok(typeof a.string == 'function');
+    a.string('test').number('age');
+    assert.ok(a.registry['test'] instanceof TypeSchema);
   }
   
 };

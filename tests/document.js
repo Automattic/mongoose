e20e2836c43ebf766def20933b566b2af284d3e8
Nathan White <nw@nwhite.net>
2010-09-24 02:55:17 -0700

    adding more tests

--- tests/document.js
+++ tests/document.js
@@ -113,6 +113,182 @@ module.exports = {
     
     var instance = new SetupFn();
     assert.ok(instance._schema['id'].type == 'virtual');
+  },
+  
+  'test isNew flag': function(){
+    var SimpleUser = mongoose.SimpleUser;
+    
+    var user = new SimpleUser({name: 'Nate'}); 
+    assert.ok(user.isNew == true);
+    
+    var user = new SimpleUser({name: 'Nathan'}, true);
+    assert.ok(user.isNew == false); 
+  },
+  
+  'test hydrated()': function(){
+    var User = mongoose.SimpleUser;
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    }, true);
+    
+    assert.ok(user.hydrated('name') == true);
+    assert.ok(user.hydrated('contact.city') == true);
+    assert.ok(user.hydrated('contact.state') == false);
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    });
+    
+    assert.ok(user.hydrated('name') == false);
+    assert.ok(user.hydrated('contact.city') == false);
+    assert.ok(user.hydrated('contact.state') == false);    
+  },
+
+  'test isDirty()': function(){
+    var User = mongoose.SimpleUser;
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    }, true);
+    
+    assert.ok(user.isDirty('name') == false);
+    assert.ok(user.isDirty('contact.city') == false);
+    assert.ok(user.isDirty('contact.state') == false);
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    });
+    
+    assert.ok(user.isDirty('name') == true);
+    assert.ok(user.isDirty('contact.city') == true);
+    assert.ok(user.isDirty('contact.state') == false);    
+  },
+  
+  'test interal doc value when initialized': function(){
+    var User = mongoose.SimpleUser;
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    }, true);
+    
+    assert.ok(user._.doc.name == 'Nathan');
+    assert.ok(user._.doc.contact.city == 'SF');
+    assert.ok(user._.doc.contact.state == undefined);
+    
+    var user = new User({
+      name: 'Nathan',
+      contact: {
+        city: 'SF'
+      }
+    });
+    
+    assert.ok(user._.doc.name == 'NATHAN');
+    assert.ok(user._.doc.contact.city == 'SF');
+    assert.ok(user._.doc.contact.state == undefined);    
+  },
+  
+  'test getters/setters': function(){
+    var User = mongoose.SimpleUser;
+    
+    var user = new User();
+    user.set('name', 'Nathan');
+    user.set('contact.city', 'SF');
+    
+    assert.ok(user.get('name') == 'nathan');
+    assert.ok(user._.doc.name == 'NATHAN');
+    assert.ok(user.get('contact.city') == 'SF');
+    assert.ok(user.isDirty('name') == true);
+    assert.ok(user.isDirty('contact.city') == true);
+    assert.ok(Object.keys(user._.doc).length == 2);
+    assert.ok(Object.keys(user._.doc.contact).length == 1);
+  },
+  
+  'test dot notation getters/setters': function(){
+    var User = mongoose.SimpleUser;
+    
+    var user = new User();
+    user.name = 'Nathan';
+    user.contact.city = 'SF';
+    
+    assert.ok(user.name == 'nathan');
+    assert.ok(user._.doc.name == 'NATHAN');
+    assert.ok(user.contact.city == 'SF');
+    assert.ok(user.isDirty('name') == true);
+    assert.ok(user.isDirty('contact.city') == true);
+    assert.ok(Object.keys(user._.doc).length == 2);
+    assert.ok(Object.keys(user._.doc.contact).length == 1);
+  },
+  
+  'test deep nested structures': function(){
+    var document = mongoose.define;
+    document('SuperNested')
+      .string('something')
+      .object('super', document()
+        .object('deep', document()
+          .object('nested', document()
+            .string('property'))));
+            
+    var SuperNested = mongoose.SuperNested;
+    
+    var nested = new SuperNested();
+    
+    assert.ok(Object.keys(nested._.doc).length == 0);
+    
+    nested.super.deep.nested.property = 'cool';
+    assert.ok(Object.keys(nested._.doc).length == 1);
+    assert.ok(nested.get('super.deep.nested.property') == 'cool');
+    assert.ok(nested._.doc.super.deep.nested.property == 'cool');
+    
+  },
+  
+  'test virtuals': function(){
+    var document = mongoose.define;
+    document('VirtualTest')
+      .string('first')
+      .string('last')
+      .virtual('full_name')
+        .get(function(){
+          return this.first + ' ' + this.last;
+        })
+        .set(function(val){
+          return this.nested.control =  val;
+        })
+      .object('nested', document()
+        .string('control')
+        .virtual('test')
+          .get(function(){
+            return this.last + ' ' + this.first;
+          })
+      );
+        
+    var Virt = mongoose.VirtualTest;
+    
+    var virtual = new Virt();
+    assert.ok(Object.keys(virtual._.doc).length == 0);
+    virtual.first = 'Nathan';
+    virtual.last = 'White';
+    assert.ok(virtual._.doc.last == 'White');
+    assert.ok(virtual.full_name == 'Nathan White');
+    virtual.full_name = 'bizarre';
+    assert.ok(virtual.nested.control == 'bizarre');
+    assert.ok(Object.keys(virtual._.dirty).length == 3);
+    assert.ok(virtual.nested.test == 'White Nathan');
   }
   
 }

f1677ecccc0632df56a4637ae8583911c77184d7
bnoguchi <brian.noguchi@gmail.com>
2010-10-18 16:08:26 -0700

    Got TypeSchema.prototype.extend to work via just passing in the name of the type (this is advertised as behavior in the README but actually wasn't implemented yet).

--- tests/types.js
+++ tests/types.js
@@ -31,7 +31,7 @@ module.exports = {
     assert.ok(str instanceof TypeSchema);
   },
   
-  'test extending types': function(){
+  'test extending types via type reference': function(){
     var mongoose = require('../'),
         type = mongoose.type,
         str = type('string'),
@@ -41,10 +41,24 @@ module.exports = {
             return complete( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value) );
           });
     
-    assert.ok(email.setters.length == 1);
+    assert.equal(email.setters.length, 1);
     assert.ok(email.parent == 'string');
     assert.ok(typeof email.validators['email'] == 'function');
     
+  },
+
+  'test extending types via type name reference': function () {
+    var mongoose = require('../'),
+        type = mongoose.type,
+        email = type('email2')
+          .extend('string')
+          .validate('email',function(val,complete){
+            return complete( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value) );
+          });
+    
+    assert.equal(email.setters.length, 1);
+    assert.ok(email.parent == 'string');
+    assert.ok(typeof email.validators['email'] == 'function');
   }
   
 };

4cfb476ec81205cc22db2dbcf62173dd6655815e
Tj Holowaychuk <tj@vision-media.ca>
2010-10-21 13:45:02 -0700

    console.log()

--- tests/query.js
+++ tests/query.js
@@ -75,7 +75,6 @@ module.exports = {
     User.find({ 'name.first': 'TJ' }).all(function(docs){
       assert.length(docs, 1);
       User.findById(docs[0]._id, function(doc){
-        console.log(doc.name.first);
         assert.equal('TJ', doc.name.first);
         done();
       });

01961631880f340940687dc89a0bfa1318c057a5
Tj Holowaychuk <tj@vision-media.ca>
2010-10-21 11:35:21 -0700

    Added user example

---
+++ examples/user.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('../lib/mongoose')
+  , document = mongoose.define;
+
+var db = mongoose.connect('mongodb://localhost/mongoose');
+
+document('User')
+  .oid('_id')
+  .object('name',
+    document()
+      .string('first')
+      .string('last'))
+  .object('contact',
+    document()
+      .string('email')
+      .string('phone'))
+  .number('age');
+
+var User = mongoose.User;
+
+db.on('open', function(){
+  var tj = new User({ name: 'tj', age: 23 });
+
+  tj.save(function(errors){
+    if (errors) throw errors[0];
+    User.find({ name: 'tj' }).one(function(user){
+      console.log(user);
+      db.close();
+    });
+  });
+});

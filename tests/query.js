e7be1965d9bb5205b41abb8c731af97f81035c77
Tj Holowaychuk <tj@vision-media.ca>
2010-10-25 09:51:17 -0700

    Added Query#fields()

--- tests/query.js
+++ tests/query.js
@@ -209,6 +209,18 @@ module.exports = {
     });
   },
   
+  'test find() partial select with several fields': function(assert, done){
+    User
+      .find({ 'name.first': 'Nathan' })
+      .fields({ name: true, age: true }).all(function(docs){
+        assert.length(docs, 1);
+        assert.equal('Nathan', docs[0].name.first);
+        assert.equal(33, docs[0].age);
+        assert.eql({}, docs[0].contact);
+        done();
+    });
+  },
+  
   'test find() partial select field omission': function(assert, done){
     User.find({ 'name.first': 'Nathan' }, 'name').all(function(docs){
       assert.length(docs, 1);

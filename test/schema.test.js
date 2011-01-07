fe93ff43385c7dc3f94df321d6f3aea6a42b1d51
Guillermo Rauch <rauchg@gmail.com>
2011-01-06 17:25:55 -0800

    Added schema test for all the types of keys

--- test/schema.test.js
+++ test/schema.test.js
@@ -5,6 +5,7 @@
 
 var mongoose = require('./common').mongoose
   , Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
   , SchemaTypes = mongoose.SchemaTypes;
 
 /**
@@ -12,6 +13,37 @@ var mongoose = require('./common').mongoose
  */
 
 module.exports = {
+
+  'different schema types support': function(){
+    var Ferret = new Schema({
+        name      : String
+      , owner     : ObjectId
+      , fur       : String
+      , color     : { type: String }
+      , age:      : Number
+      , checkins  : [Checkin]
+      , friends   : [ObjectId]
+    });
+
+    var Checkin = new Schema({
+        date      : Date 
+      , location  : {
+            lat:    Number
+          , lng:    Number
+        }
+    });
+
+    Ferret.get('name').should.be.instanceof(SchemaType.String);
+    Ferret.get('owner').should.be.instanceof(SchemaType.ObjectId);
+    Ferret.get('fur').should.be.instanceof(SchemaType.String);
+    Ferret.get('color').should.be.instanceof(SchemaType.String);
+    Ferret.get('age').should.be.instanceof(SchemaType.Number);
+    Ferret.get('checkins').should.be.instanceof(SchemaType.DocumentArray);
+    Ferret.get('friends').should.be.instanceof(SchemaType.Array);
+
+    Checkin.get('date').should.be.instanceof(SchemaType.Date);
+    Checkin.get('location').should.be.instanceof(SchemaType.Object);
+  },
 
   'dot notation support for accessing keys': function(){
     var Person = new Schema({

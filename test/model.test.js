ee4a03ec153f0e8292e13cd0c469c5fc4808b68f
Guillermo Rauch <rauchg@gmail.com>
2011-01-09 11:32:21 -0800

    Added model tests setup Added model instantiation isNew test

--- test/model.test.js
+++ test/model.test.js
@@ -1,8 +1,45 @@
 
+/**
+ * Test dependencies.
+ */
+
+var start = require('./common')
+  , mongoose = start.mongoose
+  , random = require('mongoose/utils').random;
+
+/**
+ * Setup.
+ */
+
+var BlogPost = new Schema({
+    title     : String
+  , slug      : String
+  , date      : Date
+  , meta      : {
+        date      : Date
+      , visitors  : Number
+    }
+  , published : Boolean
+  , comments  : [Comments]
+});
+
+var Comments = new Schema({
+    title     : String
+  , date      : Date
+  , body      : String
+  , comments  : [Comments]
+});
+
+Mongoose.model('BlogPost', BlogPost, 'blogposts_' + random());
+
 module.exports = {
 
-  'test model': function(){
+  'test a model isNew flag when instantiating': function(){
+    var db = start()
+      , BlogPost = db.model('BlogPost');
 
+    var post = new BlogPost();
+    post.isNew.should.be.true;
   }
 
 };

dd74025a9bfc2f166b3f18d2abc4d6b2c484909e
Guillermo Rauch <rauchg@gmail.com>
2011-01-09 11:52:07 -0800

    Added test for presence of model schema

--- test/model.test.js
+++ test/model.test.js
@@ -5,7 +5,8 @@
 
 var start = require('./common')
   , mongoose = start.mongoose
-  , random = require('mongoose/utils').random;
+  , random = require('mongoose/utils').random
+  , Schema = mongoose.Schema;
 
 /**
  * Setup.
@@ -41,6 +42,14 @@ module.exports = {
     var post = new BlogPost();
     post.isNew.should.be.true;
     db.close();
+  },
+
+  'test presence of model schema': function(){
+    var db = start()
+      , BlogPost = db.model('BlogPost');
+
+    BlogPost.schema.should.be.an.instanceof(Schema);
+    BlogPost.prototype.schema.should.be.an.instanceof(Schema);
   }
 
 };

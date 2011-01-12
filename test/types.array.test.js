80f849b8189860b0136106b32f765f0a35d6ec52
Guillermo Rauch <rauchg@gmail.com>
2011-01-11 18:30:26 -0800

    Added specific tests for MongooseArray

---
+++ test/types.array.test.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('./common').mongoose
+  , MongooseArray = mongoose.Types.Array;
+
+/**
+ * Test.
+ */
+
+module.exports = {
+
+  'test that a mongoose array behaves and quacks like an array': function(){
+    var a = new MongooseArray();
+
+    a.should.be.an.instanceof(Array);
+    a.should.be.an.instanceof(MongooseArray);
+    Array.isArray(a).should.be.true;
+    Array.isArray(a.atomics).should.be.true;
+  }
+
+};

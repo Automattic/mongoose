63fb86908441b467ca3782fab8461403a4a3cae1
Guillermo Rauch <rauchg@gmail.com>
2011-01-14 16:03:41 -0800

    Fixed MongooseDocumentArray and added tests

---
+++ test/types.documentarray.test.js
@@ -0,0 +1,26 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('./common').mongoose
+  , MongooseArray = mongoose.Types.Array
+  , MongooseDocumentArray = mongoose.Types.DocumentArray;
+
+/**
+ * Test.
+ */
+
+module.exports = {
+
+  'test that a mongoose array behaves and quacks like an array': function(){
+    var a = new MongooseDocumentArray();
+
+    a.should.be.an.instanceof(Array);
+    a.should.be.an.instanceof(MongooseArray);
+    a.should.be.an.instanceof(MongooseDocumentArray);
+    Array.isArray(a).should.be.true;
+    Array.isArray(a.atomics).should.be.true;
+  }
+
+};

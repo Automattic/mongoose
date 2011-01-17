f3738d3bd912e079c765443dda4bab87171aa5b5
Guillermo Rauch <rauchg@gmail.com>
2011-01-16 16:56:33 -0800

    Added more pending tests

--- test/model.test.js
+++ test/model.test.js
@@ -8,13 +8,16 @@ var start = require('./common')
   , mongoose = start.mongoose
   , random = require('mongoose/utils').random
   , Schema = mongoose.Schema
+  , SchemaType = mongoose.SchemaType
+  , CastError = SchemaType.CastError
+  , ValidationError = SchemaType.ValidationError
   , ObjectId = Schema.ObjectId
   , DocumentObjectId = mongoose.Types.ObjectId
   , DocumentArray = mongoose.Types.DocumentArray
   , EmbeddedDocument = mongoose.Types.Document
   , MongooseNumber = mongoose.Types.Number
   , MongooseArray = mongoose.Types.Array
-  , MongooseErrror = mongoose.Error;
+  , MongooseError = mongoose.Error;
 
 /**
  * Setup.
@@ -199,11 +202,7 @@ module.exports = {
     db.close();
   },
 
-  'test adding a subdocument with `commit`': function(){
-
-  },
-
-  'test adding a subdocument with `add`': function(){
+  'test adding a subdocument with native methods': function(){
 
   },
 
@@ -256,38 +255,38 @@ module.exports = {
       , comments    : [ { title: 'Test', date: new Date, body: 'Test' } ]
     });
 
+    post.isModified('comments').should.be.false;
     post.get('comments')[0].set('title', 'Woot');
     post.isModified('comments').should.be.true;
 
     db.close();
   },
 
-  'test isModified on a MongooseArray with atomics': function(){
+  'test isModified on a MongooseArray with atomics methods': function(){
     // COMPLETEME
     var db = start()
       , BlogPost = db.model('BlogPost');
 
     var post = new BlogPost()
-    post.init({
-        title       : 'Test'
-      , slug        : 'test'
-      , date        : new Date
-    });
+    post.init({});
+
+    post.isModified('owners').should.be.false;
+    post.get('owners').$push(new DocumentObjectId);
+    post.isModified('owners').should.be.true;
 
     db.close();
   },
 
-  'test isModified on a MongooseArray with `commit`': function(){
+  'test isModified on a MongooseArray with native methods': function(){
     // COMPLETEME
     var db = start()
       , BlogPost = db.model('BlogPost');
 
     var post = new BlogPost()
-    post.init({
-        title       : 'Test'
-      , slug        : 'test'
-      , date        : new Date
-    });
+    post.init({});
+
+    post.isModified('owners').should.be.false;
+    post.get('owners').push(new DocumentObjectId);
 
     db.close();
   },
@@ -349,6 +348,7 @@ module.exports = {
 
     post.save(function(err){
       err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(CastError);
       db.close();
     });
   },
@@ -380,6 +380,7 @@ module.exports = {
     
     post.save(function(err){
       err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(CastError);
       db.close();
     });
   },
@@ -389,31 +390,228 @@ module.exports = {
       , BlogPost = db.model('BlogPost')
       , threw = false;
 
+    var post = new BlogPost()
+    post.init({
+        title       : 'Test'
+      , slug        : 'test'
+      , comments    : [ { title: 'Test', date: new Date, body: 'Test' } ]
+    });
 
+    post.get('comments')[0].set('date', new Date);
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(CastError);
+      db.close();
+    });
+  },
+
+  'test casting error when adding a subdocument': function(){
+    var db = start()
+      , BlogPost = db.model('BlogPost')
+      , threw = false;
+
+    var post = new BlogPost()
+
+    try {
+      post.get('comments').push({
+          date: 'Bad date'
+      });
+    } catch (e) {
+      threw = true;
+    }
+
+    threw.should.be.false;
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(CastError);
+      db.close();
+    });
   },
 
   'test validation': function(){
-    
+    mongoose.model('TestValidation', new Schema({
+        simple: { type: String, required: true }
+    });
+
+    var db = start()
+      , TestValidation = db.model('TestValidation');
+
+    var post = new TestValidation();
+    post.set('simple', '');
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+
+      post.set('simple', 'here');
+      post.save(function(err){
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
   
   'test nested validation': function(){
+    mongoose.model('TestNestedValidation', new Schema({
+        nested: {
+            required: { type: String, required: true }
+        }
+    });
 
+    var db = start()
+      , TestNestedValidation = db.model('TestNestedValidation');
+
+    var post = new TestNestedValidation();
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+
+      post.set('nested.required', 'here');
+      post.save(function(err){
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
 
   'test validation in subdocuments': function(){
+    var Subdocs = new Schema({
+        required: { type: String, required: true }
+    });
 
+    mongoose.model('TestSubdocumentsValidation', new Schema({
+        items: [Subdocs]
+    });
+
+    var db = start()
+      , TestSubdocumentsValidation = db.model('TestSubdocumentsValidation');
+
+    var post = new TestSubdocumentsValidation();
+
+    post.get('items').push({ required: '' });
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+
+      post.get('items')[0].set('required', true);
+      post.save(function(err){
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
 
   'test async validation': function(){
+    var executed = false;
 
+    function validator(v, fn){
+      setTimeout(function () {
+        executed = true;
+        fn(v !== '');
+      }, 50);
+    };
+    mongoose.model('TestAsyncValidation', new Schema({
+        async: { type: Date, validate: [validator, 'async validator'] }
+    });
+
+    var db = start()
+      , TestAsyncValidation = db.model('TestAsyncValidation');
+
+    var post = new TestAsyncValidation();
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+      executed.should.be.true;
+      executed = false;
+
+      post.set('async', new Date);
+      post.save(function(err){
+        executed.should.be.true;
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
 
   'test nested async validation': function(){
+    var executed = false;
 
+    function validator(fn){
+      setTimeout(function (v, fn) {
+        executed = true;
+        fn(v !== '');
+      }, 50);
+    };
+    
+    mongoose.model('TestNestedAsyncValidation', new Schema({
+        nested: {
+            async: { type: Date, validate: [validator, 'async validator'] }
+        }
+    });
+
+    var db = start()
+      , TestNestedAsyncValidation = db.model('TestNestedAsyncValidation');
+
+    var post = new TestNestedAsyncValidation();
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+      executed.should.be.true;
+      executed = false;
+
+      post.set('nested.async', new Date);
+      post.save(function(err){
+        executed.should.be.true;
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
 
   'test async validation in subdocuments': function(){
+    var executed = false;
 
+    function validator (v, fn) {
+      setTimeout(function(){
+        executed = true;
+        fn(v !== '');
+      }, 50);
+    };
+
+    var Subdocs = new Schema({
+        required: { type: String, validate: [validator, 'async in subdocs'] }
+    });
+
+    mongoose.model('TestSubdocumentsAsyncValidation', new Schema({
+        items: [Subdocs]
+    });
+
+    var db = start()
+      , Test = db.model('TestSubdocumentsAsyncValidation');
+
+    var post = new Test();
+
+    post.get('items').push({ required: '' });
+
+    post.save(function(err){
+      err.should.be.an.instanceof(MongooseError);
+      err.should.be.an.instanceof(ValidationError);
+      executed.should.be.true;
+      executed = false;
+
+      post.get('items')[0].set({ required: 'here' });
+      post.save(function(err){
+        executed.should.be.true;
+        should.strictEqual(err, null);
+        db.close();
+      });
+    });
   },
 
   'test defaults application': function(){

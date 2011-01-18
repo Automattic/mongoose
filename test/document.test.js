59482dbed18b5abc30db08ec0aefc4dcfcbd48aa
Guillermo Rauch <rauchg@gmail.com>
2011-01-18 12:15:38 -0800

    Added middleware tests (document level)

---
+++ test/document.test.js
@@ -0,0 +1,265 @@
+
+/**
+ * Module dependencies.
+ */
+
+var start = require('./common')
+  , should = require('should')
+  , mongoose = start.mongoose
+  , Schema = mongoose.Schema
+  , Document = require('mongoose/document');
+
+/**
+ * Test Document constructor.
+ */
+
+function TestDocument () {
+  Document.apply(this, arguments);
+};
+
+/**
+ * Inherits from Document.
+ */
+
+TestDocument.prototype.__proto__ = Document.prototype;
+
+/**
+ * Set a dummy schema to simulate compilation.
+ */
+
+TestDocument.prototype.schema = new Schema();
+
+/**
+ * Method subject to hooks. Simply fires the callback once the hooks are
+ * executed.
+ */
+
+TestDocument.prototype.hooksTest = function(fn){
+  fn(null);
+};
+
+/**
+ * Apply hooks.
+ */
+
+Document.registerHooks.call(TestDocument, 'hooksTest');
+
+/**
+ * Test.
+ */
+
+module.exports = {
+
+  'test hooks system': function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , awaiting = 0
+      , called = false;
+
+    // serial
+    doc.pre('hooksTest', function(next){
+      steps++;
+      setTimeout(function(){
+        // make sure next step hasn't executed yet
+        steps.should.eql(1);
+        next();
+      }, 50);
+    });
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+      next();
+    });
+
+    // parallel
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      setTimeout(function(){
+        steps.should.eql(4);
+      }, 50);
+      setTimeout(function(){
+        steps++;
+        done();
+      }, 100);
+      next();
+    });
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      setTimeout(function(){
+        steps.should.eql(4);
+      }, 50);
+      setTimeout(function(){
+        steps++;
+        done();
+      }, 100);
+      next();
+    });
+
+    doc.hooksTest(function(err){
+      should.strictEqual(err, null);
+      steps++;
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(7);
+      called.should.be.true;
+    });
+  },
+
+  'test that calling next twice doesnt break': function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , called = false;
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+      next();
+      next();
+    });
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+      next();
+    });
+
+    doc.hooksTest(function(err){
+      should.strictEqual(err, null);
+      steps++;
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(3);
+      called.should.be.true;
+    });
+  },
+
+  'test that calling done twice doesnt break': function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , called = false;
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done();
+      done();
+    });
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done();
+      done();
+    });
+
+    doc.hooksTest(function(err){
+      should.strictEqual(err, null);
+      steps++;
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(3);
+      called.should.be.true;
+    });
+  },
+
+  'test that calling done twice on the same doesnt mean completion':
+  function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , called = false;
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done();
+      done();
+    });
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+    });
+
+    doc.hooksTest(function(err){
+      assert.strictEqual(err, null);
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(2);
+      called.should.be.false;
+    });
+  },
+
+  'test hooks system errors from a serial hook': function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , called = false;
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+      next();
+    });
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+      next(new Error);
+    });
+
+    doc.pre('hooksTest', function(next){
+      steps++;
+    });
+
+    doc.hooksTest(function(err){
+      err.should.be.an.instanceof(Error);
+      steps++;
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(3);
+      called.should.be.true;
+    });
+  },
+
+  'test hooks system errors from a parallel hook': function(beforeExit){
+    var doc = new TestDocument()
+      , steps = 0
+      , called = false;
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done();
+    });
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done();
+    });
+
+    doc.pre('hooksTest', function(next, done){
+      steps++;
+      next();
+      done(new Error);
+    });
+
+    doc.hooksTest(function(err){
+      err.should.be.an.instanceof(Error);
+      steps++;
+      called = true;
+    });
+
+    beforeExit(function(){
+      steps.should.eql(4);
+      called.should.be.true;
+    });
+  }
+
+};

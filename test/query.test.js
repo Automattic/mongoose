eba48dda25346fffe580cd7af4132c1a952ef7d1
Guillermo Rauch <rauchg@gmail.com>
2011-01-27 10:58:38 -0800

    Added failing tests for Query#skip, Query#limit, Query#timeout, Query#sort

---
+++ test/query.test.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Query = require('mongoose/query').Query;
+
+/**
+ * Test.
+ */
+
+module.exports = {
+
+  'test Query#skip': function () {
+    var query = new Query();
+
+    query.skip(50);
+    query.options.skip.should.eql(50);
+  },
+
+  'test Query#limit': function () {
+    var query = new Query();
+
+    query.limit(3);
+    query.options.limit.should.eql(3);
+  },
+
+  'test Query#timeout': function () {
+    var query = new Query();
+
+    query.timeout(1000);
+    query.options.timeout.should.eql(1000);
+  },
+
+  'test Query#sort': function () {
+    var query = new Query();
+
+    query.sort({ test: 1, nope: -1 });
+    query.options.sort.should.eql([['test', 1], ['nope', -1]]);
+  }
+
+};

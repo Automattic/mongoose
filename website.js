29fcb998b594e6cd877b182087624fa0fc00ed1b
Guillermo Rauch <rauchg@gmail.com>
2011-02-01 00:46:45 -0800

    Added website generation tool from markdown, base template, and generated website

---
+++ website.js
@@ -0,0 +1,21 @@
+
+var fs = require('fs')
+  , template = fs.readFileSync('template.html').toString()
+  , markdown = require('markdown');
+
+function process (input, output) {
+  fs.writeFileSync(
+      output
+    , template.replace('<!-- CONTENT -->', 
+        markdown.parse(fs.readFileSync(input).toString())
+      )
+  );
+};
+
+process('README.md', 'index.html');
+process('docs/defaults.md', 'docs/defaults.html');
+process('docs/embedded-documents.md', 'docs/embedded-documents.html');
+process('docs/indexes.md', 'docs/indexes.html');
+process('docs/middleware.md', 'docs/middleware.html');
+process('docs/model-definition.md', 'docs/model-definition.html');
+process('docs/validation.md', 'docs/validation.html');

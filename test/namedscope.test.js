63fe15892be07364104b4095a849c5a73e991e86
bnoguchi <brian.noguchi@gmail.com>
2011-02-14 01:19:44 -0800

    Forgot to add named scope tests mentioned in last commit msg.

---
+++ test/namedscope.test.js
@@ -0,0 +1,204 @@
+//BoundQuery
+//
+//Query.prototype.with(criteria)
+//Query.prototype.with(criteria, callback)
+//Query.prototype.with(path, val, callback)
+//
+//Query.prototype.with(criteria).find()
+//Query.prototype.with(criteria).remove()
+//Query.prototype.with(criteria).findOne()
+//Query.prototype.with(criteria).update()
+//Query.prototype.with(criteria).count()
+//
+//var query = User.find(...);
+//
+//var twenties = new Query().with('age').gte(20).lt(30);
+//var male = new Query().with('gender', 'male');
+//var active = new Query().with('lastLogin').gte(+new Date - (24 * 3600 * 1000));
+//
+//User.twenties = Query.with('age').gte(20).lt(30);
+//User.male = Query.with('gender', 'male');
+//User.active = Query.with('lastLogin').get(+new Date - (24 * 3600 * 1000));
+//
+//User.namedScope({
+//    twenties: Query.with('age').gte(20).lt(30)
+//  , male: Query.with('gender', 'male')
+//  , lastLogin: Query.with('lastLogin').get(+new Date - (24 * 3600 * 1000))
+//});
+//
+//User.namedScope('twenties').with('age').gte(20).lt(30);
+//
+//User.namedScope('olderThan', function (age) {
+//  return this.with('age').gt(age);
+//});
+//
+//User.find(twenties, male, active, function (err, found) {
+//});
+//
+//// twenties.male OR twenties.active
+//User.find(twenties.male, twenties.active, function (err, found) {
+//});
+//
+//User.find(olderThan(20).male, olderThan(30).active, function (err, found) {
+//});
+//
+//User.twenties.male.active.find(callback);
+//
+//User.twenties.male.active.remove(callback);
+
+/**
+ * Test dependencies.
+ */
+
+var start = require('./common')
+  , should = require('should')
+  , mongoose = start.mongoose
+  , random = require('mongoose/utils').random
+  , Schema = mongoose.Schem;a
+  , _24hours = 24 * 3600 * 1000
+  , ago24hours = 
+
+/**
+ * Setup.
+ */
+
+var UserSchema = new Schema({
+    age: Number
+  , gender: {type: String, enum: ['male', 'female']}
+  , lastLogin: Date
+});
+
+UserSchema.namedScope('olderThan', function (age) {
+  return this.with('age').gt(age);
+});
+
+UserSchema.namedScope('youngerThan', function (age) {
+  return this.with(age).lt(age);
+});
+
+UserSchema.namedScope('twenties').olderThan(19).youngerThan(30);
+
+UserSchema.namedScope('male').with('gender', 'male');
+
+UserSchema.namedScope('female').with('gender', 'female');
+
+UserSchema.namedScope('active', function () {
+  return this.with('lastLogin').gte(+new Date - _24hours)
+});
+
+mongoose.model('User', UserSchema);
+
+module.exports = {
+  'basic named scopes should work': function () {
+    var db = start()
+      , User = db.model('User', 'users_' + random());
+    User.create({gender: 'male'}, function (err, _) {
+      should.strictEqual(err, null);
+      User.create({gender: 'male'}, function (err, _) {
+        should.strictEqual(err, null);
+        User.create({gender: 'female'}, function (err, _) {
+          should.strictEqual(err, null);
+          User.male.find( function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(2);
+          });
+        });
+      });
+    });
+  },
+  'dynamic named scopes should work': function () {
+    var db = start()
+      , User = db.model('User', 'users_' + random());
+    User.create({age: 21}, function (err, _) {
+      should.strictEqual(err, null);
+      User.create({gender: 22}, function (err, _) {
+        should.strictEqual(err, null);
+        User.create({gender: 19}, function (err, _) {
+          should.strictEqual(err, null);
+          User.olderThan(20).find( function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(2);
+          });
+        });
+      });
+    });
+  },
+  'named scopes built on top of dynamic named scopes should work': function () {
+    var db = start()
+      , User = db.model('User', 'users_' + random());
+    User.create({age: 21}, function (err, _) {
+      should.strictEqual(err, null);
+      User.create({gender: 22}, function (err, _) {
+        should.strictEqual(err, null);
+        User.create({gender: 19}, function (err, _) {
+          should.strictEqual(err, null);
+          User.twenties.find( function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(2);
+          });
+        });
+      });
+    });
+  },
+  'chaining named scopes should work': function () {
+    var db = start()
+      , User = db.model('User', collection);
+    User.create({age: 21, gender: 'male', lastLogin: (+new Date) - _24hours - 3600}, function (err, _) {
+      should.strictEqual(err, null);
+      User.create({age: 45, gender: 'male', lastLogin: +new Date}, function (err, match) {
+        should.strictEqual(err, null);
+        User.create({age: 50, gender: 'female', lastLogin: +new Date}, function (err, _) {
+          should.strictEqual(err, null);
+          User.olderThan(40).active.male.find( function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(1);
+            found[0]._id.should.eql(match._id);
+          });
+        });
+      });
+    });
+  },
+  'using chained named scopes in a find': function () {
+    var db = start()
+      , User = db.model('User', collection);
+    User.create({age: 21, gender: 'male', lastLogin: (+new Date) - _24hours - 3600}, function (err, _) {
+      should.strictEqual(err, null);
+      User.create({age: 45, gender: 'male', lastLogin: +new Date}, function (err, match) {
+        should.strictEqual(err, null);
+        User.create({age: 50, gender: 'female', lastLogin: +new Date}, function (err, _) {
+          should.strictEqual(err, null);
+          User.find(olderThan(40).active.male, function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(1);
+            found[0]._id.should.eql(match._id);
+          });
+        });
+      });
+    });
+  },
+  'using multiple chained named scopes in a find to do an OR': function () {
+    var db = start()
+      , User = db.model('User', collection);
+    var db = start()
+      , User = db.model('User', collection);
+    User.create(
+        { age: 21, gender: 'male', lastLogin: (+new Date) - _24hours - 3600}
+      , {age: 45, gender: 'male', lastLogin: +new Date}
+      , {age: 50, gender: 'female', lastLogin: +new Date}
+      , {age: 35, gender: 'female', lastLogin: +new Date}
+      , function (err, a, b, c, d) {
+          should.strictEqual(err, null);
+          User.find(twenties.active.male, thirties.active.female, function (err, found) {
+            db.close();
+            should.strictEqual(err, null);
+            found.should.have.length(2);
+          });
+        }
+    );
+  },
+};

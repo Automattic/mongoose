eb3a409e66ef0c1af480876435d9b33d54891856
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-04-11 13:28:07 -0400

    add failing test; MongooseNumber should cast '' to null

--- test/types.number.test.js
+++ test/types.number.test.js
@@ -4,7 +4,9 @@
  */
 
 var mongoose = require('./common').mongoose
-  , MongooseNumber = mongoose.Types.Number;
+  , MongooseNumber = mongoose.Types.Number
+  , SchemaNumber = mongoose.Schema.Types.Number
+  , should = require('should')
 
 /**
  * Test.
@@ -20,6 +22,11 @@ module.exports = {
     a.toString().should.eql('5');
 
     (a._atomics.constructor).should.eql(Object);
+  },
+
+  'an empty string casts to null': function () {
+    var n = new SchemaNumber();
+    should.strictEqual(n.cast(''), null);
   }
 
 };

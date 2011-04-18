16e4618cbecf704739290806cb3d6fe6d2df56b9
Tj Holowaychuk <tj@vision-media.ca>
2011-04-18 14:52:50 -0700

    improved test

--- test/types.array.test.js
+++ test/types.array.test.js
@@ -7,6 +7,7 @@ var start = require('./common')
   , should = require('should')
   , mongoose = require('./common').mongoose
   , Schema = mongoose.Schema
+  , random = require('mongoose/utils').random
   , MongooseArray = mongoose.Types.Array;
 
 var User = new Schema({
@@ -40,27 +41,40 @@ module.exports = {
   'test indexOf()': function(){
     var db = start()
       , a = new MongooseArray
-      , User = db.model('User')
-      , Pet = db.model('Pet');
+      , User = db.model('User', 'users_' + random())
+      , Pet = db.model('Pet', 'pets' + random());
 
     var tj = new User({ name: 'tj' })
       , tobi = new Pet({ name: 'tobi' })
       , loki = new Pet({ name: 'loki' })
-      , jane = new Pet({ name: 'jane' });
+      , jane = new Pet({ name: 'jane' })
+      , pets = [];
 
     tj.pets.push(tobi);
     tj.pets.push(loki);
     tj.pets.push(jane);
 
-    tj.save(function(err){
-      User.findOne({ name: 'tj' }, function(err, user){
-        should.equal(null, err, 'error in callback');
-        user.pets.should.have.length(3);
-        user.pets.indexOf(tobi.id).should.equal(0);
-        user.pets.indexOf(loki.id).should.equal(1);
-        user.pets.indexOf(jane.id).should.equal(2);
-        db.close();
+    var pending = 3;
+  
+    [tobi, loki, jane].forEach(function(pet){
+      pet.save(function(){
+        --pending || done();
       });
     });
+
+    function done() {
+      Pet.find({}, function(err, pets){
+        tj.save(function(err){
+          User.findOne({ name: 'tj' }, function(err, user){
+            should.equal(null, err, 'error in callback');
+            user.pets.should.have.length(3);
+            user.pets.indexOf(tobi.id).should.equal(0);
+            user.pets.indexOf(loki.id).should.equal(1);
+            user.pets.indexOf(jane.id).should.equal(2);
+            db.close();
+          });
+        });
+      });
+    }
   }
 };

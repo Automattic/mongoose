840630e0ccd04934a03525e0d145f7b435e84033
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-07-17 22:00:33 -0400

    remove unused util.erase

--- test/utils.test.js
+++ test/utils.test.js
@@ -20,20 +20,6 @@ var ActiveRoster = StateMachine.ctor('require', 'init', 'modify');
 
 module.exports = {
 
-  'test array erasing util': function(){
-    function fn(){};
-    var arr = [fn, 'test', 1]
-      , arr2 = [fn, 'a', 'b', fn, 'c'];
-
-    utils.erase(arr, fn);
-    arr.should.have.length(2);
-    arr.should.eql(['test', 1]);
-
-    utils.erase(arr2, fn);
-    arr2.should.have.length(3);
-    arr2.should.eql(['a', 'b', 'c']);
-  },
-
   'should detect a path as required if it has been required': function () {
     var ar = new ActiveRoster();
     ar.require('hello');

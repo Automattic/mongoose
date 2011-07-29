63fd97eaa53594ed81fa97d8b1e2f6d4db6f43a5
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-07-29 09:41:50 -0400

    tweak contrast

---
+++ static.js
@@ -0,0 +1,16 @@
+
+var static = require('node-static');
+var server = new static.Server('.', { cache: 0 });
+
+require('http').createServer(function (req, res) {
+  req.on('end', function () {
+    server.serve(req, res, function (err) {
+      if (err) {
+        console.error(err, req.url);
+        res.writeHead(err.status, err.headers);
+        res.end();
+      }
+    });
+  });
+}).listen(8080);
+

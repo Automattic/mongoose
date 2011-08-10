74246e62a8f8e44eba861e2eb99cf248fbc2ea62
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-08-10 14:10:43 -0400

    fixed; can query/update with documents with subdocs
    closes #454

--- test/query.test.js
+++ test/query.test.js
@@ -680,7 +680,7 @@ module.exports = {
     params.ids.should.be.instanceof(Array);
     params.ids[0].toString().should.eql(id.toString());
     params.comments.should.be.instanceof(Array);
-    params.comments[0].toObject().should.eql(castedComment);
+    params.comments[0].should.eql(castedComment);
     params.strings.should.be.instanceof(Array);
     params.strings[0].should.eql('Hi there');
     params.numbers.should.be.instanceof(Array);

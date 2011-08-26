6d270a054ad46acc9bf161874c1064d4c673d8f0
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-08-26 10:14:27 -0400

    add errors page

--- website.js
+++ website.js
@@ -3,7 +3,7 @@ var fs = require('fs')
   , template = fs.readFileSync('template.html').toString()
   , markdown = require('markdown');
 
-function process (input, output) {
+function convert (input, output) {
   fs.writeFileSync(
       output
     , template.replace('<!-- CONTENT -->', 
@@ -12,18 +12,22 @@ function process (input, output) {
   );
 };
 
-//process('README.md', 'index.html');
-process('docs/defaults.md', 'docs/defaults.html');
-process('docs/embedded-documents.md', 'docs/embedded-documents.html');
-process('docs/finding-documents.md', 'docs/finding-documents.html');
-process('docs/indexes.md', 'docs/indexes.html');
-process('docs/middleware.md', 'docs/middleware.html');
-process('docs/model-definition.md', 'docs/model-definition.html');
-process('docs/schematypes.md', 'docs/schematypes.html');
-process('docs/validation.md', 'docs/validation.html');
-process('docs/virtuals.md', 'docs/virtuals.html');
-process('docs/getters-setters.md', 'docs/getters-setters.html');
-process('docs/methods-statics.md', 'docs/methods-statics.html');
-process('docs/plugins.md', 'docs/plugins.html');
-process('docs/dbrefs.md', 'docs/dbrefs.html');
-process('docs/migration-guide.md', 'docs/migration-1x-2x.html');
+if (process.argv.length > 2 && 'index' === process.argv[2]) {
+  convert('README.md', 'index.html');
+}
+
+convert('docs/defaults.md', 'docs/defaults.html');
+convert('docs/embedded-documents.md', 'docs/embedded-documents.html');
+convert('docs/finding-documents.md', 'docs/finding-documents.html');
+convert('docs/indexes.md', 'docs/indexes.html');
+convert('docs/middleware.md', 'docs/middleware.html');
+convert('docs/model-definition.md', 'docs/model-definition.html');
+convert('docs/schematypes.md', 'docs/schematypes.html');
+convert('docs/validation.md', 'docs/validation.html');
+convert('docs/virtuals.md', 'docs/virtuals.html');
+convert('docs/getters-setters.md', 'docs/getters-setters.html');
+convert('docs/methods-statics.md', 'docs/methods-statics.html');
+convert('docs/plugins.md', 'docs/plugins.html');
+convert('docs/dbrefs.md', 'docs/dbrefs.html');
+convert('docs/errors.md', 'docs/errors.html');
+convert('docs/migration-guide.md', 'docs/migration-1x-2x.html');

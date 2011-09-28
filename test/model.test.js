d3a84667735f81f02e9049d9d38e8fc6e7a2d3ba
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-09-28 16:03:56 -0400

    failing test; modified states not reset after save

--- test/model.test.js
+++ test/model.test.js
@@ -523,6 +523,34 @@ module.exports = {
     });
   },
 
+  'modified states are reset after save runs': function () {
+    var db = start()
+      , B = db.model('BlogPost', collection)
+      , pending = 2;
+
+    var b = new B;
+
+    b.numbers.push(3);
+    b.save(function (err) {
+      should.strictEqual(null, err);
+      --pending || find();
+    });
+
+    b.numbers.push(3);
+    b.save(function (err) {
+      should.strictEqual(null, err);
+      --pending || find();
+    });
+
+    function find () {
+      B.findById(b, function (err, b) {
+        db.close();
+        should.strictEqual(null, err);
+        b.numbers.length.should.equal(2);
+      });
+    }
+  },
+
   'test isModified when modifying keys': function(){
     var db = start()
       , BlogPost = db.model('BlogPost', collection);

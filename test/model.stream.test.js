2c3ae79f6b6d16ac93057f458c5172a88ec04b91
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-11-17 09:44:48 -0500

    stream interface for cursors
    start of #614
    
    need more tests

---
+++ test/model.stream.test.js
@@ -0,0 +1,76 @@
+
+/**
+ * Test dependencies.
+ */
+
+var start = require('./common')
+  , should = require('should')
+  , mongoose = start.mongoose
+  , random = require('../lib/utils').random
+  , Query = require('../lib/query')
+  , Schema = mongoose.Schema
+  , SchemaType = mongoose.SchemaType
+  , CastError = SchemaType.CastError
+  , ObjectId = Schema.ObjectId
+  , MongooseBuffer = mongoose.Types.Buffer
+  , DocumentObjectId = mongoose.Types.ObjectId
+  , fs = require('fs')
+
+var names = fs.readFileSync(__dirname + '/testnames.txt', 'utf8').split(' ')
+
+/**
+ * Setup.
+ */
+
+var Person = new Schema({
+    name: String
+});
+
+mongoose.model('PersonForStream', Person);
+var collection = 'personforstream_' + random();
+
+;(function setup () {
+  var db = start()
+    , P = db.model('PersonForStream', collection)
+
+  var people = names.map(function (name) {
+    return { name: name };
+  });
+
+  P.create(people, function (err) {
+    should.strictEqual(null, err);
+    db.close();
+    assignExports();
+  });
+})()
+
+function assignExports () {
+  exports['cursor stream'] = function () {
+    var db = start()
+      , P = db.model('PersonForStream', collection)
+      , i = 0
+
+    var stream = P.find({}).stream();
+    stream.on('data', function (doc) {
+      console.error(doc);
+      if (!(++i % 100)) {
+        console.error('pausing...');
+        stream.pause();
+        setTimeout(function () {
+          console.error('... resuming');
+          stream.resume();
+        }, 1000);
+      }
+    });
+    stream.on('error', function (err) {
+      console.error('ERROR!');
+      console.error(err.stack);
+    });
+    stream.on('close', function () {
+      console.error('closed');
+      P.remove(function () {
+        db.close();
+      });
+    });
+  }
+}

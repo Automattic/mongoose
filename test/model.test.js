40c9d5d559d81417a0fb6bc9773671d91d24cca6
Paul Wells <paulwe@gmail.com>
2012-01-03 14:07:31 -0800

    Added unit test for pull request #662

--- test/model.test.js
+++ test/model.test.js
@@ -724,6 +724,46 @@ module.exports = {
     	});
     });
   },
+  
+  'test nested structure created by merging': function() {
+    var db = start();
+    
+    var MergedSchema = new Schema({
+      a: {
+        foo: String
+      }
+    });
+    
+    MergedSchema.add({
+      a: {
+        b: {
+          bar: String
+        }
+      }
+    });
+    
+    mongoose.model('Merged', MergedSchema);
+    var Merged = db.model('Merged', 'merged_' + Math.random());
+    
+    var merged = new Merged({
+      a: {
+          foo: 'baz'
+        , b: {
+            bar: 'qux'
+          }
+      }
+    });
+    
+    merged.save(function(err) {
+      should.strictEqual(null, err);
+      Merged.findById(merged.id, function(err, found) {
+        should.strictEqual(null, err);
+        found.a.foo.should.eql('baz');
+        found.a.b.bar.should.eql('qux');
+        db.close();
+      });
+    });
+  },
 
   // GH-342
   'over-writing a number should persist to the db': function () {

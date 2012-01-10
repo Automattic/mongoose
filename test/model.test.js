3deb5f8280cb27a840228531af8d434da2448565
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-01-10 10:46:21 -0500

    oc

--- test/model.test.js
+++ test/model.test.js
@@ -724,16 +724,17 @@ module.exports = {
     	});
     });
   },
-  
+
+  // gh-662
   'test nested structure created by merging': function() {
     var db = start();
-    
+
     var MergedSchema = new Schema({
       a: {
         foo: String
       }
     });
-    
+
     MergedSchema.add({
       a: {
         b: {
@@ -741,10 +742,10 @@ module.exports = {
         }
       }
     });
-    
+
     mongoose.model('Merged', MergedSchema);
     var Merged = db.model('Merged', 'merged_' + Math.random());
-    
+
     var merged = new Merged({
       a: {
           foo: 'baz'
@@ -753,14 +754,14 @@ module.exports = {
           }
       }
     });
-    
+
     merged.save(function(err) {
       should.strictEqual(null, err);
       Merged.findById(merged.id, function(err, found) {
+        db.close();
         should.strictEqual(null, err);
         found.a.foo.should.eql('baz');
         found.a.b.bar.should.eql('qux');
-        db.close();
       });
     });
   },

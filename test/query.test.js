f3d5015a2727a8054c7bc72caad436095f9c5fb3
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-03-23 17:03:01 -0400

    added; updateOne support

--- test/query.test.js
+++ test/query.test.js
@@ -865,6 +865,47 @@ module.exports = {
     query.options.batchSize.should.equal(10);
   },
 
+  'Query#setOptions': function () {
+    var q = new Query;
+    q.setOptions({ thing: "cat" });
+    q.setOptions({ populate: ['fans'] });
+    q.setOptions({ batchSize: 10 });
+    q.setOptions({ limit: 4 });
+    q.setOptions({ skip: 3 });
+    q.setOptions({ sort: ['blah', -1] });
+    q.setOptions({ asc: 'ascending' });
+    q.setOptions({ hint: { index1: 1, index2: -1 }});
+
+    q.options.thing.should.equal('cat');
+    q.options.populate.fans.should.eql({ fields: undefined, conditions: undefined, options: undefined });
+    q.options.batchSize.should.eql(10);
+    q.options.limit.should.eql(4);
+    q.options.skip.should.eql(3);
+    q.options.sort.length.should.eql(2);
+    q.options.sort[0][0].should.equal('blah');
+    q.options.sort[0][1].should.equal(-1);
+    q.options.sort[1][0].should.equal('ascending');
+    q.options.sort[1][1].should.equal(1);
+    q.options.hint.index1.should.equal(1);
+    q.options.hint.index2.should.equal(-1);
+
+    var db = start();
+    var Product = db.model('Product', 'Product_setOptions_test');
+    Product.create(
+        { numbers: [3,4,5] }
+      , { strings: 'hi there'.split(' ') } , function (err, doc1, doc2) {
+
+      should.strictEqual(null, err);
+
+      Product.find().setOptions({ limit: 1, desc: '_id' }).exec(function (err, docs) {
+        db.close();
+        should.strictEqual(null, err);
+        docs.length.should.equal(1);
+        docs[0].id.should.equal(doc2.id);
+      });
+    });
+  },
+
   'empty updates are not run': function () {
     var q = new Query;
     ;(!!q._castUpdate({})).should.be.false;

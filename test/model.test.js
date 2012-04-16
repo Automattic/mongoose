a4d33f3dd2ab890357a4e491811daed9c18922b8
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-04-16 10:18:17 -0700

    fix isNew on sub doc after insertion error
    relates to #837

--- test/model.test.js
+++ test/model.test.js
@@ -5,6 +5,7 @@
 
 var start = require('./common')
   , should = require('should')
+  , assert = require('assert')
   , mongoose = start.mongoose
   , random = require('../lib/utils').random
   , Query = require('../lib/query')
@@ -553,6 +554,29 @@ module.exports = {
         post.get('comments')[0].isNew.should.be.false;
         post.get('comments')[0].comments[0].isNew.should.be.false;
         db.close();
+      });
+    });
+  },
+
+  'isNew on parent and subdocs on failed inserts': function () {
+    var db = start()
+
+    var schema = new Schema({
+        name: { type: String, unique: true }
+      , em: [new Schema({ x: Number })]
+    }, { collection: 'testisnewonfail_'+random() });
+    var A = db.model('isNewOnFail', schema);
+    var a = new A({ name: 'i am new', em: [{ x: 1 }] });
+    a.save(function (err) {
+      should.strictEqual(null, err);
+      assert.equal(a.isNew, false);
+      assert.equal(a.em[0].isNew, false);
+      var b = new A({ name: 'i am new', em: [{x:2}] });
+      b.save(function (err) {
+        db.close();
+        assert(err);
+        assert.equal(b.isNew, true);
+        assert.equal(b.em[0].isNew, true);
       });
     });
   },

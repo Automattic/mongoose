eeef07215fb0d6cc2282870c4de4e7b905f94bac
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-05-15 11:21:39 -0700

    backport string regex $options to 2.x

--- test/model.querying.test.js
+++ test/model.querying.test.js
@@ -2371,4 +2371,19 @@ module.exports = {
       S.find({ _id: s._id }).exec(done);
     });
   },
+
+  'regex with options': function () {
+    var db = start()
+       , B = db.model('BlogPostB', collection)
+
+    var post = new B({ title: '$option queries' });
+    post.save(function (err) {
+      should.strictEqual(null, err);
+      B.findOne({ title: { $regex: ' QUERIES$', $options: 'i' }}, function (err, doc) {
+        db.close();
+        should.strictEqual(null, err, err && err.stack);
+        doc.id.should.eql(post.id);
+      })
+    });
+  }
 };

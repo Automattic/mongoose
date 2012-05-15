fbc7555bc624fae9b46312a1619281c6d979776e
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-05-15 11:25:27 -0700

    fix test

--- test/model.test.js
+++ test/model.test.js
@@ -3503,10 +3503,10 @@ module.exports = {
       , {title: 'interoperable find as promise 2'}
       , function (err, createdOne, createdTwo) {
       should.strictEqual(err, null);
-      var query = BlogPost.find({title: 'interoperable find as promise 2'});
+      var query = BlogPost.find({title: 'interoperable find as promise 2'}).sort('_id', 1)
       var promise = query.run();
       promise.addBack(function (err, found) {
-        should.strictEqual(err, null);
+        should.strictEqual(err, null, err && err.stack);
         found.length.should.equal(2);
         found[0].id;
         found[1].id;

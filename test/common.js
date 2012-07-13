761f6cc6a9cc14dbe8a4b348958a96366c421d31
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-07-13 08:54:29 -0700

    docs

--- test/common.js
+++ test/common.js
@@ -61,44 +61,6 @@ Collection.prototype.onClose = function(){
 };
 
 /**
- * Assert that a connection is open or that mongoose connections are open.
- * Examples:
- *    mongoose.should.be.connected;
- *    db.should.be.connected;
- *
- * @api public
- */
-
-// should.Assertion
-//Assertion.prototype.__defineGetter__('connected', function(){
-  //if (this.obj instanceof Mongoose)
-    //this.obj.connections.forEach(function(connected){
-      //c.should.be.connected;
-    //});
-  //else
-    //this.obj.readyState.should.eql(1);
-//});
-
-/**
- * Assert that a connection is closed or that a mongoose connections are closed.
- * Examples:
- *    mongoose.should.be.disconnected;
- *    db.should.be.disconnected;
- *
- * @api public
- */
-
-// should.Assertion
-//Assertion.prototype.__defineGetter__('disconnected', function(){
-  //if (this.obj instanceof Mongoose)
-    //this.obj.connections.forEach(function(){
-      //c.should.be.disconnected;
-    //});
-  //else
-    //this.obj.readyState.should.eql(0);
-//});
-
-/**
  * Create a connection to the test database.
  * You can set the environmental variable MONGOOSE_TEST_URI to override this.
  *

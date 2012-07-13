585e736ca7bfa4ab24754757837ad8f4e8602142
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-07-13 16:49:20 -0700

    handle specifying index along with sparse/unique option
    closes #1004

--- test/schema.type.test.js
+++ test/schema.type.test.js
@@ -15,4 +15,15 @@ describe('schematype', function(){
     var a = new Schema({ thought: { type: String, select: true }});
     assert.equal(true, a.path('thought').selected);
   })
+
+  it('properly handles specifying index in combination with unique or sparse', function(){
+    var s = new Schema({ name: { type: String, index: true, unique: true }});
+    assert.deepEqual(s.path('name')._index, { unique: true });
+    var s = new Schema({ name: { type: String, unique: true, index: true }});
+    assert.deepEqual(s.path('name')._index, { unique: true });
+    var s = new Schema({ name: { type: String, index: true, sparse: true }});
+    assert.deepEqual(s.path('name')._index, { sparse: true });
+    var s = new Schema({ name: { type: String, sparse: true, index: true }});
+    assert.deepEqual(s.path('name')._index, { sparse: true });
+  })
 })

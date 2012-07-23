04f75fcb2fc5d9c132c6e0f7db7749ec9177af83
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-07-23 12:52:55 -0700

    fixed field selection option on subdocs
    fixes #1022 on 2.x

--- test/schema.select.test.js
+++ test/schema.select.test.js
@@ -23,12 +23,14 @@ describe('schema select option', function(){
     var schema = new Schema({
         thin: Boolean
       , name: { type: String, select: false}
+      , docs: [new Schema({ bool: Boolean, name: { type: String, select: false }})]
     });
 
     var S = db.model('ExcludingBySchemaType', schema);
-    S.create({ thin: true, name: 'the excluded' },function (err, s) {
+    S.create({ thin: true, name: 'the excluded', docs:[{bool:true, name: 'test'}] },function (err, s) {
       assert.ifError(err);
       assert.equal(s.name, 'the excluded');
+      assert.equal(s.docs[0].name, 'test');
 
       var pending = 3;
       function cb (err, s) {
@@ -40,11 +42,12 @@ describe('schema select option', function(){
         if (Array.isArray(s)) s = s[0];
         assert.strictEqual(null, err);
         assert.equal(false, s.isSelected('name'));
+        assert.equal(false, s.isSelected('docs.name'));
         assert.strictEqual(undefined, s.name);
       }
 
-      S.findById(s).select('-thin').exec(cb);
-      S.find({ _id: s._id }).select('thin').exec(cb);
+      S.findById(s).select({thin:0, 'docs.bool':0}).exec(cb);
+      S.find({ _id: s._id }).select('thin docs.bool').exec(cb);
       S.findById(s, cb);
     });
   });
@@ -55,12 +58,14 @@ describe('schema select option', function(){
     var schema = new Schema({
         thin: Boolean
       , name: { type: String, select: true }
+      , docs: [new Schema({ bool: Boolean, name: { type: String, select: true }})]
     });
 
     var S = db.model('IncludingBySchemaType', schema);
-    S.create({ thin: true, name: 'the included' },function (err, s) {
+    S.create({ thin: true, name: 'the included', docs:[{bool:true, name: 'test'}] },function (err, s) {
       assert.ifError(err);
       assert.equal(s.name, 'the included');
+      assert.equal(s.docs[0].name, 'test');
 
       var pending = 2;
       function cb (err, s) {
@@ -71,11 +76,12 @@ describe('schema select option', function(){
         if (Array.isArray(s)) s = s[0];
         assert.strictEqual(null, err);
         assert.strictEqual(true, s.isSelected('name'));
+        assert.strictEqual(true, s.isSelected('docs.name'));
         assert.equal(s.name, 'the included');
       }
 
-      S.findById(s).select('-thin').exec(cb);
-      S.find({ _id: s._id }).select('thin').exec(cb);
+      S.findById(s).select({thin:0, 'docs.bool':0}).exec(cb);
+      S.find({ _id: s._id }).select('thin docs.bool').exec(cb);
     });
   });
 
@@ -85,10 +91,12 @@ describe('schema select option', function(){
     var selected = new Schema({
         thin: Boolean
       , name: { type: String, select: true }
+      , docs: [new Schema({ name: { type: String, select: true }, bool: Boolean })]
     });
     var excluded = new Schema({
         thin: Boolean
       , name: { type: String, select: false }
+      , docs: [new Schema({ name: { type: String, select: false }, bool: Boolean})]
     });
 
     var S = db.model('OverriddingSelectedBySchemaType', selected);
@@ -96,11 +104,12 @@ describe('schema select option', function(){
 
     var pending = 4;
 
-    S.create({ thin: true, name: 'the included' },function (err, s) {
+    S.create({ thin: true, name: 'the included', docs: [{name:'test',bool: true}] },function (err, s) {
       assert.ifError(err);
       assert.equal(s.name, 'the included');
+      assert.equal(s.docs[0].name, 'test');
 
-      S.find({ _id: s._id }).select('thin name').exec(function (err, s) {
+      S.find({ _id: s._id }).select('thin name docs.bool docs.name').exec(function (err, s) {
         if (!--pending) {
           db.close();
           done();
@@ -109,11 +118,15 @@ describe('schema select option', function(){
         assert.ifError(err);
         assert.strictEqual(true, s.isSelected('name'));
         assert.strictEqual(true, s.isSelected('thin'));
+        assert.strictEqual(true, s.isSelected('docs.name'));
+        assert.strictEqual(true, s.isSelected('docs.bool'));
         assert.equal(s.name, 'the included');
+        assert.equal(s.docs[0].name, 'test');
         assert.ok(s.thin);
+        assert.ok(s.docs[0].bool);
       });
 
-      S.findById(s).select({'name':0}).exec(function (err, s) {
+      S.findById(s).select({'name':0, 'docs.name':0}).exec(function (err, s) {
         if (!--pending) {
           db.close();
           done();
@@ -121,16 +134,21 @@ describe('schema select option', function(){
         assert.strictEqual(null, err);
         assert.equal(false, s.isSelected('name'))
         assert.equal(true, s.isSelected('thin'))
+        assert.equal(false, s.isSelected('docs.name'))
+        assert.equal(true, s.isSelected('docs.bool'))
         assert.strictEqual(undefined, s.name);
+        assert.strictEqual(undefined, s.docs[0].name);
         assert.equal(true, s.thin);
+        assert.equal(true, s.docs[0].bool);
       });
     });
 
-    E.create({ thin: true, name: 'the excluded' },function (err, e) {
+    E.create({ thin: true, name: 'the excluded',docs:[{name:'test',bool:true}] },function (err, e) {
       assert.ifError(err);
       assert.equal(e.name, 'the excluded');
+      assert.equal(e.docs[0].name, 'test');
 
-      E.find({ _id: e._id }).select('thin name').exec(function (err, e) {
+      E.find({ _id: e._id }).select('thin name docs.name docs.bool').exec(function (err, e) {
         if (!--pending) {
           db.close();
           done();
@@ -139,11 +157,15 @@ describe('schema select option', function(){
         assert.strictEqual(null, err);
         assert.equal(true, e.isSelected('name'));
         assert.equal(true, e.isSelected('thin'));
+        assert.equal(true, e.isSelected('docs.name'));
+        assert.equal(true, e.isSelected('docs.bool'));
         assert.equal(e.name, 'the excluded');
+        assert.equal(e.docs[0].name, 'test');
         assert.ok(e.thin);
+        assert.ok(e.docs[0].bool);
       });
 
-      E.findById(e).select({'name':0}).exec(function (err, e) {
+      E.findById(e).select({'name':0, 'docs.name': 0}).exec(function (err, e) {
         if (!--pending) {
           db.close();
           done();
@@ -151,8 +173,12 @@ describe('schema select option', function(){
         assert.strictEqual(null, err);
         assert.equal(e.isSelected('name'),false)
         assert.equal(e.isSelected('thin'), true);
+        assert.equal(e.isSelected('docs.name'),false)
+        assert.equal(e.isSelected('docs.bool'), true);
         assert.strictEqual(undefined, e.name);
+        assert.strictEqual(undefined, e.docs[0].name);
         assert.strictEqual(true, e.thin);
+        assert.strictEqual(true, e.docs[0].bool);
       });
     });
   })
@@ -196,6 +222,25 @@ describe('schema select option', function(){
 
     var M = db.model('SelectingOnly_idWithExcludedSchemaType', schema);
     M.find().select({_id:1, name:0}).exec(function (err) {
+      assert.ok(err instanceof Error, 'conflicting path selection error should be instance of Error');
+
+      M.find().select('_id').exec(function (err) {
+        db.close();
+        assert.ifError(err, err && err.stack);
+        done();
+      });
+    });
+  });
+
+  it('selecting _id works with excluded schematype path on sub doc', function (done) {
+    var db = start();
+
+    var schema = new Schema({
+        docs: [new Schema({name: { type: String, select: false}})]
+    });
+
+    var M = db.model('SelectingOnly_idWithExcludedSchemaType', schema);
+    M.find().select({_id:1, 'docs.name':0}).exec(function (err) {
       assert.ok(err instanceof Error, 'conflicting path selection error should be instance of Error');
 
       M.find().select('_id').exec(function (err) {

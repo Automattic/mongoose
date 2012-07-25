e130f3a14baf6390be62d2b0cfc55da951b30a3d
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-07-25 12:40:30 -0700

    fixed; unsetting a default array
    fixes #758

--- test/model.test.js
+++ test/model.test.js
@@ -4088,4 +4088,20 @@ describe('model', function(){
       });
     });
   })
+
+  describe('unsetting a default value', function(){
+    it('should be ignored (gh-758)', function(done){
+      var db = start();
+      var M = db.model('758', new Schema({ s: String, n: Number, a: Array }));
+      M.collection.insert({ }, { safe: true }, function (err) {
+        assert.ifError(err);
+        M.findOne(function (err, m) {
+          assert.ifError(err);
+          m.s = m.n = m.a = undefined;
+          assert.equal(undefined, m._delta());
+          done();
+        });
+      });
+    })
+  })
 });

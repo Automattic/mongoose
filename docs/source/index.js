13bf43c9aab9aeaab6cab402e2132b808c03a9a1
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-07-31 17:38:04 -0700

    add prior page

--- docs/source/index.js
+++ docs/source/index.js
@@ -1,5 +1,5 @@
-var dir = 'docs/';
 
 exports['index.jade'] = require('./home')
 exports['docs/api.jade'] = require('./api')
 exports['docs/index.jade'] = {}; //require('./gettingstarted')
+exports['docs/prior.jade'] = require('./prior')

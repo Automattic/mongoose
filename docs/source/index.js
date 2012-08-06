97c1df8f4b67a9fea89949f26442c6f71785dd26
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-08-05 19:30:02 -0700

    moar doxxxx

--- docs/source/index.js
+++ docs/source/index.js
@@ -8,3 +8,6 @@ exports['docs/schematypes.jade'] = require('./guide')
 exports['docs/middleware.jade'] = require('./middleware')
 exports['docs/plugins.jade'] = require('./plugins')
 exports['docs/embedded.jade'] = { guide: true }
+exports['docs/models.jade'] = { guide: true }
+exports['docs/queries.jade'] = { guide: true }
+exports['docs/documents.jade'] = { guide: true }

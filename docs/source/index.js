56f811c450a3281b90da96234111223f4840c98a
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-08-07 10:23:15 -0700

    more docs

--- docs/source/index.js
+++ docs/source/index.js
@@ -13,3 +13,6 @@ exports['docs/queries.jade'] = { guide: true }
 exports['docs/documents.jade'] = { guide: true, docs: true }
 exports['docs/populate.jade'] = { guide: true }
 exports['docs/validation.jade'] = { guide: true }
+exports['docs/migration.jade'] = { guide: true }
+exports['docs/contributing.jade'] = { guide: true }
+exports['docs/faq.jade'] = { guide: true }

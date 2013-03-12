3c5f0b99b1710df2be5af1843fca3a28a337fc8b
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2013-03-12 08:19:16 -0700

    fixed; empty object mixed defaults
    Now interpreted as functions that return new empty objects.
    
    To restore old behavior (a shared object between all documents)
    set the "shared" option to true.
    
      new Schema({ data: { type: Mixed, default: {}, shared: true }})
    
    fixes #1380

---
+++ test/schema.mixed.test.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var start = require('./common')
+  , mongoose = new start.mongoose.Mongoose
+  , assert = require('assert')
+  , Schema = mongoose.Schema
+
+describe('schematype mixed', function(){
+  describe('empty object defaults (gh-1380)', function(){
+    it('are interpreted as fns that return new empty objects', function(done){
+      var s = Schema({ mix: { type: Schema.Types.Mixed, default: {} }})
+      var M = mongoose.model('M1', s);
+      var m1 = new M;
+      var m2 = new M;
+      m1.mix.val = 3;
+      assert.equal(3, m1.mix.val);
+      assert.equal(undefined, m2.mix.val);
+      done();
+    })
+    it('can be forced to share the object between documents', function(done){
+      // silly but necessary for backwards compatibility
+      var s = Schema({ mix: { type: Schema.Types.Mixed, default: {}, shared: true }})
+      var M = mongoose.model('M2', s);
+      var m1 = new M;
+      var m2 = new M;
+      m1.mix.val = 3;
+      assert.equal(3, m1.mix.val);
+      assert.equal(3, m2.mix.val);
+      done();
+    })
+  })
+})

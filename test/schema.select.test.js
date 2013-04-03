bf7bee94e99fb9088386c9c87b9e98f19c773aed
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2013-04-03 13:53:07 -0700

    fixed; +field conflict with $slice
    closes #1370

--- test/schema.select.test.js
+++ test/schema.select.test.js
@@ -288,40 +288,31 @@ describe('schema select option', function(){
     })
   })
 
-  it('forcing inclusion of a deselected schema path works', function (done) {
-    var db = start();
-    var excluded = new Schema({
-        thin: Boolean
-      , name: { type: String, select: false }
-      , docs: [new Schema({ name: { type: String, select: false },  bool: Boolean })]
-    });
+  describe('forcing inclusion of a deselected schema path', function () {
+    it('works', function (done) {
+      var db = start();
+      var excluded = new Schema({
+          thin: Boolean
+        , name: { type: String, select: false }
+        , docs: [new Schema({ name: { type: String, select: false },  bool: Boolean })]
+      });
 
-    var M = db.model('ForcedInclusionOfPath', excluded);
-    M.create({ thin: false, name: '1 meter', docs:[{name:'test', bool:false}] }, function (err, d) {
-      assert.ifError(err);
-
-      M.findById(d)
-      .select('+name +docs.name')
-      .exec(function (err, doc) {
+      var M = db.model('ForcedInclusionOfPath', excluded);
+      M.create({ thin: false, name: '1 meter', docs:[{name:'test', bool:false}] }, function (err, d) {
         assert.ifError(err);
-        assert.equal(false, doc.thin);
-        assert.equal('1 meter', doc.name);
-        assert.equal(false, doc.docs[0].bool);
-        assert.equal('test', doc.docs[0].name);
-        assert.equal(d.id, doc.id);
 
         M.findById(d)
-        .select('+name -thin +docs.name -docs.bool')
+        .select('+name +docs.name')
         .exec(function (err, doc) {
           assert.ifError(err);
-          assert.equal(undefined, doc.thin);
+          assert.equal(false, doc.thin);
           assert.equal('1 meter', doc.name);
-          assert.equal(undefined, doc.docs[0].bool);
+          assert.equal(false, doc.docs[0].bool);
           assert.equal('test', doc.docs[0].name);
           assert.equal(d.id, doc.id);
 
           M.findById(d)
-          .select('-thin +name -docs.bool +docs.name')
+          .select('+name -thin +docs.name -docs.bool')
           .exec(function (err, doc) {
             assert.ifError(err);
             assert.equal(undefined, doc.thin);
@@ -331,21 +322,49 @@ describe('schema select option', function(){
             assert.equal(d.id, doc.id);
 
             M.findById(d)
-            .select('-thin -docs.bool')
+            .select('-thin +name -docs.bool +docs.name')
             .exec(function (err, doc) {
-              db.close();
               assert.ifError(err);
               assert.equal(undefined, doc.thin);
-              assert.equal(undefined, doc.name);
+              assert.equal('1 meter', doc.name);
               assert.equal(undefined, doc.docs[0].bool);
-              assert.equal(undefined, doc.docs[0].name);
+              assert.equal('test', doc.docs[0].name);
               assert.equal(d.id, doc.id);
-              done();
+
+              M.findById(d)
+              .select('-thin -docs.bool')
+              .exec(function (err, doc) {
+                db.close();
+                assert.ifError(err);
+                assert.equal(undefined, doc.thin);
+                assert.equal(undefined, doc.name);
+                assert.equal(undefined, doc.docs[0].bool);
+                assert.equal(undefined, doc.docs[0].name);
+                assert.equal(d.id, doc.id);
+                done();
+              });
             });
           });
         });
       });
     });
+
+    it('works with query.slice (gh-1370)', function(done){
+      var db = start();
+      var M = db.model("1370", new Schema({ many: { type: [String], select: false }}));
+
+      M.create({ many: ["1", "2", "3", "4", "5"] }, function (err) {
+        if (err) return done(err);
+
+        var query = M.findOne().select("+many").where("many").slice(2);
+
+        query.exec(function (err, doc) {
+          if (err) return done(err);
+          assert.equal(2, doc.many.length);
+          done();
+        });
+      });
+    })
   });
 
   it('conflicting schematype path selection should not error', function (done) {

44c52eec655af2558c8f823105596d00f4e4030e
EJ Bensing <ebensing@rm-dash-r.com>
2013-06-05 17:06:21 -0700

    statics example added

---
+++ examples/statics/person.js
@@ -0,0 +1,21 @@
+
+// import the necessary modules
+var mongoose = require('../../lib');
+var Schema = mongoose.Schema;
+
+// create an export function to encapsulate the model creation
+module.exports = function() {
+  // define schema
+  var PersonSchema = new Schema({
+    name : String,
+    age : Number,
+    birthday : Date
+  });
+
+  // define a static
+  PersonSchema.statics.findPersonByName = function (name, cb) {
+    this.find({ name : new RegExp(name, 'i') }, cb);
+  };
+
+  mongoose.model('Person', PersonSchema);
+};

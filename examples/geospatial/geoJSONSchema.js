aec0d093649ed569c7cfd42c028b332ed253cb66
EJ Bensing <ebensing@rm-dash-r.com>
2013-06-07 16:18:38 -0700

    added a geoJSON example

---
+++ examples/geospatial/geoJSONSchema.js
@@ -0,0 +1,22 @@
+
+// import the necessary modules
+var mongoose = require('../../lib');
+var Schema = mongoose.Schema;
+
+// create an export function to encapsulate the model creation
+module.exports = function() {
+  // define schema
+  // NOTE : This object must conform *precisely* to the geoJSON specification
+  // you cannot embed a geoJSON doc inside a model or anything like that- IT
+  // MUST BE VANILLA
+  var LocationObject = new Schema({
+    loc: {
+      type: { type: String }, 
+      coordinates: []
+    }
+  });
+  // define the index
+  LocationObject.index({ loc : '2dsphere' });
+
+  mongoose.model('Location', LocationObject);
+};

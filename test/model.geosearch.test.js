98c13d146f35ce291af0bd3c9dee441ef196ec9c
EJ Bensing <ebensing@rm-dash-r.com>
2013-07-01 11:40:11 -0700

    Added casting/lean support for geoSearch
    also, updated the tests. everything now casts appropriately.

--- test/model.geosearch.test.js
+++ test/model.geosearch.test.js
@@ -21,7 +21,7 @@ schema.index({ "pos" : "geoHaystack", type : 1},{ bucketSize : 1});
 mongoose.model('Geo', schema);
 
 describe('model', function(){
-  describe('geoSearch', function () {
+  describe.only('geoSearch', function () {
     it('works', function (done) {
 
       var db = start();
@@ -51,7 +51,47 @@ describe('model', function(){
           assert.equal(results.results[0].pos.length, 2);
           assert.equal(results.results[0].pos[0], 10);
           assert.equal(results.results[0].pos[1], 10);
+          assert.equal(results.results[0].id, geos[0].id);
+          assert.ok(results.results[0] instanceof Geo);
+          Geo.remove(function () {
+            db.close();
+            done();
+          });
+        });
+      }
+    });
+    it('works with lean', function (done) {
+
+      var db = start();
+      var Geo = db.model('Geo');
+      assert.ok(Geo.geoSearch instanceof Function);
+
+      var geos = [];
+      geos[0] = new Geo({ pos : [10,10], type : "place"});
+      geos[1] = new Geo({ pos : [15,5], type : "place"});
+      geos[2] = new Geo({ pos : [20,15], type : "house"});
+      geos[3] = new Geo({ pos : [1,-1], type : "house"});
+      var count = geos.length;
+
+      for (var i=0; i < geos.length; i++) {
+        geos[i].save(function () {
+          --count || next();
+        });
+      }
+
+      function next() {
+        Geo.geoSearch({ type : "place" }, { near : [9,9], maxDistance : 5, lean : true }, function (err, results, stats) {
+          assert.ifError(err);
+          assert.equal(1, results.results.length);
+          assert.equal(1, results.ok);
+
+          assert.equal(results.results[0].type, 'place');
+          assert.equal(results.results[0].pos.length, 2);
+          assert.equal(results.results[0].pos[0], 10);
+          assert.equal(results.results[0].pos[1], 10);
           assert.equal(results.results[0]._id, geos[0].id);
+          assert.strictEqual(results.results[0].id, undefined);
+          assert.ok(!(results.results[0] instanceof Geo));
           Geo.remove(function () {
             db.close();
             done();

f89d4d5fb76e847f08a15032f6be159468f7e7e8
EJ Bensing <ebensing@rm-dash-r.com>
2013-07-25 15:19:08 -0700

    added 2 new benchmarks for population

--- benchmarks/benchjs/population.js
+++ benchmarks/benchjs/population.js
@@ -29,6 +29,42 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
     , date      : Date
     , body      : String
   });
+  var dummy1Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy2Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy3Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy4Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy5Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy6Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy7Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy8Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
+  var dummy9Schema = new Schema({
+    title : String,
+    isThisTest : Boolean
+  });
   var BlogPost = new Schema({
       title     : String
     , author    : String
@@ -44,6 +80,15 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
     , tags      : [String]
     , owners    : [ObjectId]
     , comments  : [{ type : ObjectId, ref : 'Comment' }]
+    , dummy1    : [{ type : ObjectId, ref : 'Dummy1' }]
+    , dummy2    : [{ type : ObjectId, ref : 'Dummy2' }]
+    , dummy3    : [{ type : ObjectId, ref : 'Dummy3' }]
+    , dummy4    : [{ type : ObjectId, ref : 'Dummy4' }]
+    , dummy5    : [{ type : ObjectId, ref : 'Dummy5' }]
+    , dummy6    : [{ type : ObjectId, ref : 'Dummy6' }]
+    , dummy7    : [{ type : ObjectId, ref : 'Dummy7' }]
+    , dummy8    : [{ type : ObjectId, ref : 'Dummy8' }]
+    , dummy9    : [{ type : ObjectId, ref : 'Dummy9' }]
     , def       : { type: String, default: 'kandinsky' }
   });
 
@@ -58,20 +103,92 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
     numbers : [1,2,7,10,23432],
     tags : ['test', 'BENCH', 'things', 'more things'],
     def : 'THANGS!!!',
-    comments : []
+    comments : [],
+    dummy1 : [],
+    dummy2 : [],
+    dummy3 : [],
+    dummy4 : [],
+    dummy5 : [],
+    dummy6 : [],
+    dummy7 : [],
+    dummy8 : [],
+    dummy9 : []
   };
   var commentData = {
     title : 'test comment',
     date : new Date(),
     body : 'this be some crazzzyyyyy text that would go in a comment'
   };
+  var dummyData = {
+    title : "dummy data~",
+    isThisTest : true
+  };
   var Comments = mongoose.model('Comment', commentSchema);
   var BlogPost = mongoose.model('BlogPost', BlogPost);
+  var Dummy1 = mongoose.model('Dummy1', dummy1Schema);
+  var Dummy2 = mongoose.model('Dummy2', dummy2Schema);
+  var Dummy3 = mongoose.model('Dummy3', dummy3Schema);
+  var Dummy4 = mongoose.model('Dummy4', dummy4Schema);
+  var Dummy5 = mongoose.model('Dummy5', dummy5Schema);
+  var Dummy6 = mongoose.model('Dummy6', dummy6Schema);
+  var Dummy7 = mongoose.model('Dummy7', dummy7Schema);
+  var Dummy8 = mongoose.model('Dummy8', dummy8Schema);
+  var Dummy9 = mongoose.model('Dummy9', dummy9Schema);
   var cIds = [];
-  var cn = 500;
+  var dIds = [];
+  for (var i=0; i < 9; i++) {
+    dIds.push([]);
+  }
+
+  var cn = 5000;
   for (var i=0; i < 500; i++) {
     Comments.create(commentData, function (err, com) {
       cIds.push(com.id);
+      --cn || cont();
+    });
+    Dummy1.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[0].push(d.id);
+      --cn || cont();
+    });
+    Dummy2.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[1].push(d.id);
+      --cn || cont();
+    });
+    Dummy3.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[2].push(d.id);
+      --cn || cont();
+    });
+    Dummy4.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[3].push(d.id);
+      --cn || cont();
+    });
+    Dummy5.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[4].push(d.id);
+      --cn || cont();
+    });
+    Dummy6.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[5].push(d.id);
+      --cn || cont();
+    });
+    Dummy7.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[6].push(d.id);
+      --cn || cont();
+    });
+    Dummy8.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[7].push(d.id);
+      --cn || cont();
+    });
+    Dummy9.create(dummyData, function (err, d) {
+      if (err) throw err;
+      dIds[8].push(d.id);
       --cn || cont();
     });
   }
@@ -86,6 +203,9 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
     blogData.comments.push(getNextcId());
     blog[4] = blogData;
 
+    blog[5] = utils.clone(blogData);
+    blog[6] = utils.clone(blogData);
+
     for (var i=0; i < 10; i++) {
       blog[0].comments.push(getNextcId());
     }
@@ -98,9 +218,28 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
     for (var i=0; i < 10000; i++) {
       blog[3].comments.push(getNextcId());
     }
+    for (var i=0; i < 100; i++) {
+      blog[5].comments.push(getNextcId());
+      blog[6].comments.push(getNextcId());
+
+      blog[5].dummy1.push(getNextdId(0));
+      blog[5].dummy2.push(getNextdId(1));
+      blog[5].dummy3.push(getNextdId(2));
+      blog[5].dummy4.push(getNextdId(3));
+
+      blog[6].dummy1.push(getNextdId(0));
+      blog[6].dummy2.push(getNextdId(1));
+      blog[6].dummy3.push(getNextdId(2));
+      blog[6].dummy4.push(getNextdId(3));
+      blog[6].dummy5.push(getNextdId(4));
+      blog[6].dummy1.push(getNextdId(5));
+      blog[6].dummy2.push(getNextdId(6));
+      blog[6].dummy3.push(getNextdId(7));
+      blog[6].dummy4.push(getNextdId(8));
+    }
 
     // insert all of the data here
-    var count = 5;
+    var count = 7;
     for (var i=0; i < blog.length; i++) {
       // use some closure magic to make sure we retain the index
       (function(c) {
@@ -115,21 +254,56 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
   }
 
   var ci = 0;
+  var di = [];
+  for (var i=0; i < 9; i++) {
+    di.push(0);
+  }
 
   function getNextcId() {
     ci = ++ci % cIds.length;
     return cIds[ci];
   }
 
+  function getNextdId(i) {
+    di[i] = ++di[i] % dIds[i].length;
+    return dIds[i][di[i]];
+  }
+
+
   function closeDB() {
-    var close = false;
+    var close = 11;
     BlogPost.remove(function () {
-      close && mongoose.disconnect();
-      close = true;
+      --close || mongoose.disconnect();
     });
     Comments.remove(function () {
-      close && mongoose.disconnect();
-      close = true;
+      --close || mongoose.disconnect();
+    });
+    Dummy1.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy2.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy3.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy4.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy5.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy6.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy7.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy8.remove(function () {
+      --close || mongoose.disconnect();
+    });
+    Dummy9.remove(function () {
+      --close || mongoose.disconnect();
     });
   }
 
@@ -173,7 +347,24 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function (err) {
         deferred.resolve();
       });
     }
+  }).add('Populate - 5 properties', {
+    defer : true,
+    fn : function (deferred) {
+      blog[5].populate('comments dummy1 dummy2 dummy3 dummy4', function (err) {
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  }).add('Populate - 10 properties', {
+    defer : true,
+    fn : function (deferred) {
+      blog[6].populate('comments dummy1 dummy2 dummy3 dummy4 dummy5 dummy6 dummy7 dummy8 dummy9', function (err) {
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
   })
+
   .on('cycle', function (evt) {
     if (process.env.MONGOOSE_DEV) {
       console.log(String(evt.target));

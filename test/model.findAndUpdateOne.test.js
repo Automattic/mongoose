e7df792153f1e6a8c181436a266135ed4bb3d421
Aaron Heckmann <aaron@recurly.com>
2014-02-03 09:02:26 -0800

    fixed; findOneAnd..() with sort
    closes #1887

--- test/model.findAndUpdateOne.test.js
+++ test/model.findAndUpdateOne.test.js
@@ -661,12 +661,10 @@ describe('model: findByIdAndUpdate:', function(){
   it('supports v3 sort string syntax', function(done){
     var db = start()
       , M = db.model(modelname, collection)
-      , _id = new DocumentObjectId
 
-    db.close();
-
-    var now = new Date
-      , query;
+    var now = new Date;
+    var _id = new DocumentObjectId;
+    var query;
 
     query = M.findByIdAndUpdate(_id, { $set: { date: now }}, { sort: 'author -title' });
     assert.equal(2, Object.keys(query.options.sort).length);
@@ -677,7 +675,24 @@ describe('model: findByIdAndUpdate:', function(){
     assert.equal(2, Object.keys(query.options.sort).length);
     assert.equal(1, query.options.sort.author);
     assert.equal(-1, query.options.sort.title);
-    done();
+
+    // gh-1887
+    M.create(
+        { title: 1, meta: {visitors: 0}}
+      , { title: 2, meta: {visitors: 10}}
+      , { title: 3, meta: {visitors: 5}}
+      , function (err, a,b,c) {
+      if (err) return done(err);
+
+      M.findOneAndUpdate({}, { title: 'changed' })
+      .sort({ 'meta.visitors': -1 })
+      .exec(function(err, doc) {
+        if (err) return done(err);
+        db.close();
+        assert.equal(10, doc.meta.visitors);
+        done();
+      });
+    });
   })
 
   it('supports v3 sort object syntax', function(done){
@@ -699,6 +714,7 @@ describe('model: findByIdAndUpdate:', function(){
     assert.equal(2, Object.keys(query.options.sort).length);
     assert.equal(1, query.options.sort.author);
     assert.equal(-1, query.options.sort.title);
+
     done();
   });
 

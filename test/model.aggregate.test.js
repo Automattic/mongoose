d99a89174cab17ea9cf9eecb709934d5c928e6fa
Valeri Karpov <valkar207@gmail.com>
2014-05-09 11:48:46 -0400

    Add test for $out helper

--- test/model.aggregate.test.js
+++ test/model.aggregate.test.js
@@ -35,6 +35,8 @@ describe('model aggregate', function(){
   var project = { $project: { maxAge: 1, _id: 0 }};
   var db, A, maxAge;
 
+  var mongo26_or_greater = false;
+
   before(function(done){
     db = start()
     A = db.model('Aggregate', collection)
@@ -52,7 +54,12 @@ describe('model aggregate', function(){
 
     A.create(docs, function (err) {
       assert.ifError(err);
-      done();
+      start.mongodVersion(function (err, version) {
+        if (err) throw err;
+        mongo26_or_greater = 2 < version[0] || (2 == version[0] && 6 <= version[1]);
+        if (!mongo26_or_greater) console.log('not testing mongodb 2.6 features');
+        done();
+      });
     });
   })
 
@@ -103,6 +110,30 @@ describe('model aggregate', function(){
     it('when returning Aggregate', function(done) {
       assert(A.aggregate(project) instanceof Aggregate);
       done();
+    });
+
+    it('can use helper for $out', function(done) {
+      if (!mongo26_or_greater) {
+        return done();
+      }
+
+      this.timeout(4000);
+
+      var outputCollection = 'aggregate_output_' + random(); 
+      var promise = A.aggregate()
+        .group(group.$group)
+        .project(project.$project)
+        .out(outputCollection)
+        .exec(function(error, result) {
+          assert.ifError(error);
+          A.db.collection(outputCollection).find().toArray(function(error, documents) {
+            assert.ifError(error);
+            assert.equal(1, documents.length);
+            assert.ok('maxAge' in documents[0]);
+            assert.equal(maxAge, documents[0].maxAge);
+            done();
+          });
+        });
     });
   })
 });

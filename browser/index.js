580d18c68cbf01d66a2e9093a0f9c9761a89a3e7
Valeri Karpov <valkar207@gmail.com>
2014-07-18 09:38:24 -0400

    Eliminate some hackiness from browser document

--- browser/index.js
+++ browser/index.js
@@ -1,3 +1,11 @@
-exports.Document = require('../lib/document');
 exports.Schema = require('../lib/schema');
 exports.Types = require('../lib/types');
+
+var Document = require('../lib/document');
+
+exports.Document = function(obj, schema, skipId) {
+  this.schema = schema;
+
+  Document.apply(this, obj, !!skipId);
+};
+exports.Document.__proto__ = Document;

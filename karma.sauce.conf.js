4e1b464755302668e7de206969c3a2bbdbfb8477
Valeri Karpov <valeri.karpov@10gen.com>
2014-07-23 16:24:31 -0400

    Add bin directory, tweak .npmignore to not include browser component

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './mongoose.browser.js',
+      './bin/mongoose.js',
       './test/browser/*.js'
     ],
 

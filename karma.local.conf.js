b64cfcf93c0f69bef1d85bff117a949771f02a7c
Constantine Melnikov <ka.melnikov@gmail.com>
2014-08-15 16:01:57 +0900

    Remove __proto__

--- karma.local.conf.js
+++ karma.local.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './mongoose.browser.js',
+      './bin/mongoose.js',
       './test/browser/*.js'
     ],
 

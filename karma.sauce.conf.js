21f16c62e2f3230fe616745a40f22b4385a11b11
Valeri Karpov <valkar207@gmail.com>
2014-09-08 16:15:47 -0400

    Quick change to karma sauce config

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -5,13 +5,13 @@ module.exports = function(config) {
       browserName: 'chrome',
       version: '35'
     },
-    /*sl_safari_6: {
+    sl_safari_6: {
       base: 'SauceLabs',
       browserName: 'safari',
       platform: 'OS X 10.8',
       version: '6'
     },
-    sl_safari_7: {
+    /*sl_safari_7: {
       base: 'SauceLabs',
       browserName: 'safari',
       platform: 'OS X 10.9',
@@ -23,12 +23,12 @@ module.exports = function(config) {
       platform: 'Windows 7',
       version: '9'
     },
-    /*sl_ie_10: {
+    sl_ie_10: {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 7',
       version: '10'
-    },*/
+    },
     /*sl_ie_11: {
       base: 'SauceLabs',
       browserName: 'internet explorer',

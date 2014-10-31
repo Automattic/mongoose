205ae18805349e2eb63a50fd862e9ca643841840
Valeri Karpov <valkar207@gmail.com>
2014-10-31 20:16:54 +0000

    Some basics for document harmony test suite

--- test/harmony/document.test_.js
+++ test/harmony/document.test_.js
@@ -1,53 +1,120 @@
 var start = require('../common');
 var mongoose = start.mongoose;
 var Schema = mongoose.Schema;
+var ValidationError = require('../../lib/error/validation');
 var co = require('co');
 var assert = require('assert');
 
-describe('document', function() {
-  describe('#validate', function() {
-    it('works (gh-891)', function(done) {
+/**
+ *  Asynchronous document functions return
+ *  [promises](https://www.npmjs.org/package/mpromise), and so are compatible
+ *  with the ES6 `yield` keyword and libraries like
+ *  [co](https://www.npmjs.org/package/co)
+ */
+describe('Documents in ES6', function() {
+  var db;
+  var collectionNameCounter = 0;
 
-      co(function*() {
-        var db = start();
-        var schema = null;
-        var called = false;
+  var getCollectionName = function() {
+    return 'harmony-documents-validate-' + (++collectionNameCounter);
+  };
 
-        var validate = [function(str){ called = true; return true }, 'BAM'];
+  beforeEach(function() {
+    db = start();
+  });
 
-        schema = new Schema({
-          prop: { type: String, required: true, validate: validate },
-          nick: { type: String, required: true }
-        });
+  afterEach(function(done) {
+    db.close(done);
+  });
+
+  it('validate() integrates with co and the yield keyword', function(done) {
+    co(function*() {
+      var schema = null;
+      var called = false;
+      var shouldSucceed = true;
+      var error;
+
+      var validate = {
+        validator: function(str) {
+          called = true;
+          return shouldSucceed;
+        },
+        message: 'BAM'
+      };
+
+      schema = new Schema({
+        eggs: { type: String, required: true, validate: validate },
+        bacon: { type: Boolean, required: true }
+      });
  
-        var M = db.model('validateSchema', schema, 'gh-891');
-        var m = new M({ prop: 'gh891', nick: 'validation test' });
+      var M = db.model('validateSchema', schema, getCollectionName());
+      var m = new M({ eggs: 'Sunny side up', bacon: false });
 
-        try {
-          yield m.save();
-        } catch(e) {
-          assert.ok(false, e);
-        }
-      
-        assert.equal(true, called);
-        called = false;
+      try {
+        yield m.validate();
+      } catch(e) {
+        error = e;
+      }
 
-        try {
-          m = yield M.findById(m, 'nick').exec();
-        } catch(e) {
-          assert.ok(false, e);
-        }
-        assert.equal(false, called);
-        m.nick = 'gh-891';
+      assert.ok(!error);
+      assert.equal(true, called);
+      called = false;
 
-        try {
-          yield m.save();
-        } catch(e) {
-          assert.ok(false, e);
-        }
-        assert.equal(false, called);
-        done();
-      })();
-    });
+      // The validator function above should now fail
+      shouldSucceed = false;
+      try {
+        yield m.validate();
+      } catch(e) {
+        error = e;
+      }
+
+      assert.ok(error);
+      assert.ok(error instanceof ValidationError);
+
+      done();
+    })();
+  });
+
+  it('save() integrates with co and the yield keyword', function(done) {
+    co(function*() {
+      var error;
+      var schema = new Schema({
+        description: { type: String, required: true },
+      });
+
+      var Breakfast = db.model('breakfast', schema, getCollectionName());
+
+      var goodBreakfast = new Breakfast({ description: 'eggs & bacon' });
+
+      try {
+        yield goodBreakfast.save();
+      } catch(e) {
+        error = e;
+      }
+
+      assert.ifError(error);
+      var result;
+      try {
+        result = yield Breakfast.findOne().exec();
+      } catch(e) {
+        error = e;
+      }
+
+      assert.ifError(error);
+      assert.equal('eggs & bacon', result.description);
+
+      // Should cause a validation error because `description` is required
+      var badBreakfast = new Breakfast({});
+      try {
+        yield badBreakfast.save();
+      } catch(e) {
+        error = e;
+      }
+
+      assert.ok(error);
+      assert.ok(error instanceof ValidationError);
+
+      done();
+    })();
   });
 });

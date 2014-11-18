5c3d122b5f405d095056d383a606ce24602023a0
Constantine Melnikov <ka.melnikov@gmail.com>
2014-11-19 02:40:05 +0800

    add validateSyc tests

--- test/browser/schema.validation.test_.js
+++ test/browser/schema.validation.test_.js
@@ -57,13 +57,13 @@ describe('schema', function(){
       Test.path('complex').enum('d', 'e');
 
       assert.deepEqual(Test.path('complex').enumValues, ['a', 'b', 'c', null, 'd', 'e']);
-      
+
       // with SchemaTypes validate method
       Test.path('state').enum({
         values: 'opening open closing closed'.split(' '),
         message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
       });
-      
+
       assert.equal(Test.path('state').validators.length, 1);
       assert.deepEqual(Test.path('state').enumValues, ['opening', 'open', 'closing', 'closed']);
 
@@ -83,15 +83,15 @@ describe('schema', function(){
       Test.path('complex').doValidate('da', function(err){
         assert.ok(err instanceof ValidatorError);
       });
-      
+
       Test.path('state').doValidate('x', function(err){
         assert.ok(err instanceof ValidatorError);
       });
-      
+
       Test.path('state').doValidate('opening', function(err){
         assert.ifError(err);
       });
-      
+
       Test.path('state').doValidate('open', function(err){
         assert.ifError(err);
       });
@@ -250,25 +250,25 @@ describe('schema', function(){
         done();
       });
 
-    it('number required', function(done){
-      var Edwald = new Schema({
-          friends: { type: Number, required: true }
-      });
+      it('number required', function(done){
+        var Edwald = new Schema({
+            friends: { type: Number, required: true }
+        });
 
-      Edwald.path('friends').doValidate(null, function(err){
-        assert.ok(err instanceof ValidatorError);
-      });
+        Edwald.path('friends').doValidate(null, function(err){
+          assert.ok(err instanceof ValidatorError);
+        });
 
-      Edwald.path('friends').doValidate(undefined, function(err){
-        assert.ok(err instanceof ValidatorError);
-      });
+        Edwald.path('friends').doValidate(undefined, function(err){
+          assert.ok(err instanceof ValidatorError);
+        });
 
-      Edwald.path('friends').doValidate(0, function(err){
-        assert.ifError(err);
-      });
+        Edwald.path('friends').doValidate(0, function(err){
+          assert.ifError(err);
+        });
 
-      done();
-    });
+        done();
+      });
 
       it('date required', function(done){
         var Loki = new Schema({
@@ -559,6 +559,377 @@ describe('schema', function(){
             assert.equal('customType', err.errors.x.kind);
             done();
           });
+        });
+      });
+    });
+
+    describe('sync', function(){
+      it('works', function( done ){
+        var executed = 0;
+
+        function validator (value) {
+          executed++;
+          return value === true;
+        }
+
+        var Animal = new Schema({
+          ferret: { type: Boolean, validate: validator }
+        });
+
+        assert.ifError(Animal.path('ferret').doValidateSync(true));
+        assert.ok(Animal.path('ferret').doValidateSync(false) instanceof Error);
+
+        if (2 === executed) done();
+      });
+
+      it('multiple', function(done) {
+        var executed = 0;
+
+        function validator (value) {
+          executed++;
+          return value === true;
+        }
+
+        var Animal = new Schema({
+          ferret: {
+            type: Boolean,
+            validate: [
+              {
+                'validator': validator,
+                'msg': 'validator1'
+              },
+              {
+                'validator': validator,
+                'msg': 'validator2'
+              }
+            ]
+          }
+        });
+
+        assert.ifError(Animal.path('ferret').doValidateSync(true));
+
+        if (2 === executed) done();
+      });
+
+      it('scope', function(done){
+        var called = false;
+        function validator (value) {
+          assert.equal('b', this.a);
+
+          called = true;
+          return true
+        }
+
+        var Animal = new Schema({
+          ferret: { type: Boolean, validate: validator }
+        });
+
+        var err = Animal.path('ferret').doValidateSync(true, { a: 'b' });
+
+        assert.ifError(err);
+        assert.equal(true, called);
+
+        done();
+      });
+
+      it('ingore async', function(done){
+        function syncValidator (val) {
+          return val == 'sync';
+        }
+
+        var called = false;
+        function asyncValidator (val, respond) {
+          called = true;
+          setTimeout(function(){
+            respond( val == 'async' );
+          }, 0);
+        }
+
+        var Animal = new Schema({
+          simple: { type: Boolean, validate: [ syncValidator, asyncValidator ] },
+          simpleAsync: { type: Boolean, validate: asyncValidator }
+        });
+
+        assert.ifError(Animal.path('simple').doValidateSync('sync'));
+        assert.ok(Animal.path('simple').doValidateSync('sync123') instanceof ValidatorError);
+
+        assert.equal(false, called);
+
+        done();
+      });
+
+      it('subdoc', function(done){
+        function syncValidator (val) {
+          return val == 'sync';
+        }
+
+        var called = false;
+        function asyncValidator (val, respond) {
+          called = true;
+          setTimeout(function(){
+            respond( val == 'async' );
+          }, 0);
+        }
+
+        var Sub = new mongoose.Schema({
+          title: { type: String, required: true },
+          sync: { type: String, validate: [ syncValidator, asyncValidator ] },
+          async: { type: String, validate: asyncValidator }
+        });
+        var SubsSchema = new mongoose.Schema({
+          subs: [ Sub ]
+        });
+
+        var doc = new mongoose.Document({
+          subs: [{}, {}]
+        }, SubsSchema);
+
+        var err = doc.validateSync();
+        assert.ok( err instanceof mongoose.Error.ValidationError );
+        assert.equal('title', err.errors['subs.0.title'].path);
+
+
+        var doc1 = new mongoose.Document({
+          subs: [{
+            title: 'title1'
+          }, {}]
+        }, SubsSchema);
+
+        var err1 = doc1.validateSync();
+        assert.ok( err1 instanceof mongoose.Error.ValidationError );
+        assert.equal('title', err1.errors['subs.1.title'].path);
+
+
+        var doc2 = new mongoose.Document({
+          subs: [{
+            title: 'title0'
+          }, {
+            title: 'title2'
+          }]
+        }, SubsSchema);
+        assert.ifError( doc2.validateSync() );
+
+
+        var doc3 = new mongoose.Document({
+          subs: [{
+            title: 'title0',
+            sync: 'fail'
+          }, {
+            title: 'title2'
+          }]
+        }, SubsSchema);
+
+        var err3 = doc3.validateSync();
+        assert.ok( err3 instanceof mongoose.Error.ValidationError );
+        assert.equal('Validator failed for path `sync` with value `fail`', err3.errors['subs.0.sync'].message);
+
+
+        var doc4 = new mongoose.Document({
+          subs: [{
+            title: 'title0',
+            sync: 'sync',
+            async: 'fail'
+          }, {
+            title: 'title2',
+            sync: 'fail',
+            async: 'async'
+          }]
+        }, SubsSchema);
+
+        var err4 = doc4.validateSync();
+        assert.ok( err4 instanceof mongoose.Error.ValidationError );
+        assert.equal( err4.errors['subs.0.sync'], undefined );
+        assert.ok( err4.errors['subs.1.sync'] );
+
+
+        assert.equal(false, called);
+
+        done();
+      });
+
+      it('string enum', function(done){
+        var Test = new Schema({
+          complex: { type: String, enum: ['a', 'b', undefined, 'c', null] },
+          state: { type: String }
+        });
+
+        // with SchemaTypes validate method
+        Test.path('state').enum({
+          values: 'opening open closing closed'.split(' '),
+          message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
+        });
+
+        assert.ok(Test.path('complex').doValidateSync('x') instanceof ValidatorError);
+        // allow unsetting enums
+        assert.ifError(Test.path('complex').doValidateSync(undefined));
+        assert.ifError(Test.path('complex').doValidateSync(null));
+        assert.ok(Test.path('complex').doValidateSync('da') instanceof ValidatorError);
+
+        assert.ok(Test.path('state').doValidateSync('x') instanceof ValidatorError);
+        assert.ifError(Test.path('state').doValidateSync('opening'));
+        assert.ifError(Test.path('state').doValidateSync('open'));
+
+        done();
+      });
+
+      it('string regexp', function(done){
+        var Test = new Schema({
+          simple: { type: String, match: /[a-z]/ }
+        });
+
+        assert.ifError(Test.path('simple').doValidateSync('az'));
+
+        Test.path('simple').match(/[0-9]/);
+        assert.equal(2, Test.path('simple').validators.length);
+
+        assert.ok(Test.path('simple').doValidateSync('12') instanceof ValidatorError);
+        assert.ifError(Test.path('simple').doValidateSync('a12'));
+        assert.ifError(Test.path('simple').doValidateSync(''));
+        assert.ifError(Test.path('simple').doValidateSync(null));
+        assert.ifError(Test.path('simple').doValidateSync(undefined));
+
+        Test.path('simple').validators = [];
+        Test.path('simple').match(/[1-9]/);
+        assert.ok(Test.path('simple').doValidateSync(0) instanceof ValidatorError);
+
+        done();
+      });
+
+      it('number min and max', function(done){
+        var Tobi = new Schema({
+          friends: { type: Number, max: 15, min: 5 }
+        });
+
+        assert.ifError(Tobi.path('friends').doValidateSync(10));
+
+        var err = Tobi.path('friends').doValidateSync(100);
+        assert.ok(err instanceof ValidatorError);
+        assert.equal('friends', err.path);
+        assert.equal('max', err.kind);
+        assert.equal(100, err.value);
+
+        assert.ok(Tobi.path('friends').doValidateSync(1) instanceof ValidatorError);
+
+        // null is allowed
+        assert.ifError(Tobi.path('friends').doValidateSync(null));
+        done();
+      });
+
+      describe('required', function(){
+        it('string required', function(done){
+          var Test = new Schema({
+            simple: String
+          });
+
+          Test.path('simple').required(true);
+
+          assert.ok(Test.path('simple').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Test.path('simple').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ok(Test.path('simple').doValidateSync('') instanceof ValidatorError);
+          assert.ifError(Test.path('simple').doValidateSync('woot'));
+
+          done();
+        });
+
+        it('string conditional required', function (done) {
+          var Test = new Schema({
+            simple: String
+          });
+
+          var required = true,
+            isRequired = function () {
+              return required;
+            };
+
+          Test.path('simple').required(isRequired);
+
+          assert.ok(Test.path('simple').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Test.path('simple').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ok(Test.path('simple').doValidateSync('') instanceof ValidatorError);
+          assert.ifError(Test.path('simple').doValidateSync('woot'));
+
+          required = false;
+
+          assert.ifError(Test.path('simple').doValidateSync(null));
+          assert.ifError(Test.path('simple').doValidateSync(undefined));
+          assert.ifError(Test.path('simple').doValidateSync(''));
+          assert.ifError(Test.path('simple').doValidateSync('woot'));
+
+          done();
+        });
+
+        it('number required', function(done){
+          var Edwald = new Schema({
+            friends: { type: Number, required: true }
+          });
+
+          assert.ok(Edwald.path('friends').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Edwald.path('friends').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ifError(Edwald.path('friends').doValidateSync(0));
+
+          done();
+        });
+
+        it('date required', function(done){
+          var Loki = new Schema({
+            birth_date: { type: Date, required: true }
+          });
+
+          assert.ok(Loki.path('birth_date').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Loki.path('birth_date').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ifError(Loki.path('birth_date').doValidateSync(new Date()));
+
+          done();
+        });
+
+        it('objectid required', function(done){
+          var Loki = new Schema({
+            owner: { type: ObjectId, required: true }
+          });
+
+          assert.ifError(Loki.path('owner').doValidateSync( new DocumentObjectId() ));
+          assert.ok(Loki.path('owner').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Loki.path('owner').doValidateSync(undefined) instanceof ValidatorError);
+
+          done();
+        });
+
+        it('array required', function(done){
+          var Loki = new Schema({
+            likes: { type: Array, required: true }
+          });
+
+          assert.ok(Loki.path('likes').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Loki.path('likes').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ok(Loki.path('likes').doValidateSync([]) instanceof ValidatorError);
+
+          done();
+        });
+
+        it('boolean required', function(done){
+          var Animal = new Schema({
+            isFerret: { type: Boolean, required: true }
+          });
+
+          assert.ok(Animal.path('isFerret').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Animal.path('isFerret').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ifError(Animal.path('isFerret').doValidateSync(true));
+          assert.ifError(Animal.path('isFerret').doValidateSync(false));
+
+          done();
+        });
+
+        it('mixed required', function(done){
+          var Animal = new Schema({
+            characteristics: { type: Mixed, required: true }
+          });
+
+          assert.ok(Animal.path('characteristics').doValidateSync(null) instanceof ValidatorError);
+          assert.ok(Animal.path('characteristics').doValidateSync(undefined) instanceof ValidatorError);
+          assert.ifError(Animal.path('characteristics').doValidateSync( { aggresive: true } ));
+          assert.ifError(Animal.path('characteristics').doValidateSync('none available'));
+
+          done();
         });
       });
     });

46fa6e5ced69cec27f597e1ed03b95e44468c387
Valeri Karpov <valkar207@gmail.com>
2014-12-18 14:14:21 -0500

    Basic setup for browser docs #2256

--- docs/source/harmony.js
+++ docs/source/harmony.js
@@ -10,33 +10,18 @@ var modelTests = acquit.parse(
   fs.readFileSync('test/harmony/model.test_.js').toString());
 blocks = docTests.concat(queryTests).concat(modelTests);
 
-var trimEachLine = function(str) {
-  var lines = str.split('\n');
-  var result = '';
-  for (var i = 0; i < lines.length; ++i) {
-    var toAdd = lines[i].trim();
-    if (toAdd.indexOf('*') === 0) {
-      toAdd = toAdd.substr('*'.length).trim();
-    }
-    result += (i > 0 ? '\n' : '') + toAdd;
-  }
-
-  return result;
-};
-
 for (var i = 0; i < blocks.length; ++i) {
   var block = blocks[i];
   block.contents = marked(trimEachLine(block.contents));
   if (block.comments && block.comments.length) {
-    console.log(block.comments[0]);
-    block.comments[0] = marked(trimEachLine(block.comments[0]));
+    block.comments[0] = marked(acquit.trimEachLine(block.comments[0]));
   }
 
   for (var j = 0; j < block.blocks.length; ++j) {
     var b = block.blocks[j];
     b.contents = marked(trimEachLine(b.contents));
     if (b.comments && b.comments.length) {
-      b.comments[0] = marked(trimEachLine(b.comments[0]));
+      b.comments[0] = marked(acquit.trimEachLine(b.comments[0]));
     }
   }
 }

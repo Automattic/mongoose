Automattic/mongoose

commit a190d41603ddea10cbaa1734b3543bf06c417108

Fix documentarray id method when using object _id

lib/types/documentarray.js
@@ -121,8 +121,7 @@ MongooseDocumentArray.mixin.id = function (id) {
       sid || (sid = String(id));
       if (sid == _id._id) return this[i];
     } else if (!(_id instanceof ObjectId)) {
-      sid || (sid = String(id));
-      if (sid == _id) return this[i];
+      if (utils.deepEqual(id, _id)) return this[i];
     } else if (casted == _id) {
       return this[i];
     }

test/types.documentarray.test.js
@@ -110,6 +110,26 @@ describe('types.documentarray', function(){
     assert.equal(a.id(id3).title, 'rock-n-roll');
     assert.equal(a.id(sub3._id).title, 'rock-n-roll');
 
+    // test with object as _id
+    var Custom = new Schema({
+        title: { type: String }
+      , _id:   { one: { type: String }, two: { type: String } }
+    });
+
+    var Subdocument = TestDoc(Custom);
+
+    var sub1 = new Subdocument();
+    sub1._id = {one: 'rolling', two: 'rock'};
+    sub1.title = 'to be a rock and not to roll';
+
+    var sub2 = new Subdocument();
+    sub2._id = {one: 'rock', two: 'roll'};
+    sub2.title = 'rock-n-roll';
+
+    var a = new MongooseDocumentArray([sub1,sub2]);
+    assert.notEqual(a.id({one: 'rolling', two: 'rock'}).title, 'rock-n-roll');
+    assert.equal(a.id({one: 'rock', two: 'roll'}).title, 'rock-n-roll');
+
     // test with no _id
     var NoId = new Schema({
         title: { type: String }

commit 29c58b36c44d6264a52a0a134f491fdc002e785f

Fix documentarray id method when _id is null-ish.
Fixes a bug introduced by a190d41.

lib/types/documentarray.js
@@ -117,7 +117,9 @@ MongooseDocumentArray.mixin.id = function (id) {
   for (var i = 0, l = this.length; i < l; i++) {
     _id = this[i].get('_id');
 
-    if (_id instanceof Document) {
+    if (_id === null || typeof _id === 'undefined') {
+      continue;
+    } else if (_id instanceof Document) {
       sid || (sid = String(id));
       if (sid == _id._id) return this[i];
     } else if (!(_id instanceof ObjectId)) {

test/types.documentarray.test.js
@@ -167,6 +167,9 @@ describe('types.documentarray', function(){
       threw = err;
     }
     assert.equal(false, threw);
+    // undefined and null should not match a nonexistent _id
+    assert.strictEqual(null, a.id(undefined));
+    assert.strictEqual(null, a.id(null));
 
     // test when _id is a populated document
     var Custom = new Schema({
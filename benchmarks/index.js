e7530544c3b7aedeb0da6a6ed2f3283bfdc51f55
Christian Murphy <Christian.Murphy@asu.edu>
2015-07-21 08:52:52 -0700

    add preferred this and semicolon rules, lint benchmarks

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -7,27 +7,27 @@ function log () {
 }
 
 var mongoose = require('../')
-  , Schema = mongoose.Schema;
+, Schema = mongoose.Schema;
 
 var DocSchema = new Schema({
-    title: String
+  title: String
 });
 
 var AllSchema = new Schema({
-    string: String
-  , number: Number
-  , date  : Date
-  , bool  : Boolean
-  , buffer: Buffer
-  , objectid: Schema.ObjectId
-  , array : Array
-  , strings: [String]
-  , numbers: [Number]
-  , dates  : [Date]
-  , bools  : [Boolean]
-  , buffers: [Buffer]
-  , objectids: [Schema.ObjectId]
-  , docs     : [DocSchema]
+  string: String
+, number: Number
+, date  : Date
+, bool  : Boolean
+, buffer: Buffer
+, objectid: Schema.ObjectId
+, array : Array
+, strings: [String]
+, numbers: [Number]
+, dates  : [Date]
+, bools  : [Boolean]
+, buffers: [Buffer]
+, objectids: [Schema.ObjectId]
+, docs     : [DocSchema]
 });
 
 var A = mongoose.model('A', AllSchema);
@@ -37,9 +37,9 @@ var totaltime = 0;
 
 // bench the normal way
 // the try building the doc into the document prototype
-// and using inheritance and bench that 
+// and using inheritance and bench that
 //
-// also, bench using listeners for each subdoc vs one 
+// also, bench using listeners for each subdoc vs one
 // listener that knows about all subdocs and notifies
 // them.
 
@@ -49,9 +49,10 @@ function run (label, fn) {
   var start = new Date;
   var total = 10000;
   var i = total;
+  var a;
   while (i--) {
     a = fn();
-    if (i%2) {
+    if (i % 2) {
       a.toObject({ depopulate: true });
     } else {
       if (a._delta) {
@@ -66,87 +67,99 @@ function run (label, fn) {
   numdocs += total;
   log(label + ' took %d seconds for %d docs (%d dps)', time, total, total/time);
   var used = process.memoryUsage();
-  var res = {}
+  var res = {};
   res.rss  = used.rss - started.rss;
   res.heapTotal = used.heapTotal - started.heapTotal;
   res.heapUsed = used.heapUsed - started.heapUsed;
   log('change: ', res);
-  a = res = used = time = started = start = total = i = null
+  a = res = used = time = started = start = total = i = null;
   //console.error(((used.vsize - started.vsize) / 1048576)+' MB');
 }
 
 run('string', function () {
   return new A({
-      string: "hello world"
+    string: "hello world"
   });
-})
+});
+
 run('number', function () {
   return new A({
-      number: 444848484
+    number: 444848484
   });
-})
+});
 run('date', function () {
   return new A({
-     date: new Date
+    date: new Date
   });
-})
+});
+
 run('bool', function () {
   return new A({
-     bool: true
+    bool: true
   });
-})
+});
+
 run('buffer', function () {
   return new A({
-     buffer: new Buffer(0)
+    buffer: new Buffer(0)
   });
-})
+});
+
 run('objectid', function () {
   return new A({
-     objectid: new mongoose.Types.ObjectId()
+    objectid: new mongoose.Types.ObjectId()
   });
-})
+});
+
 run('array of mixed', function () {
   return new A({
-     array: [4,{},[],"asdfa"]
+    array: [4,{},[],"asdfa"]
   });
-})
+});
+
 run('array of strings', function () {
   return new A({
-     strings: ["one","two","three","four"]
+    strings: ["one","two","three","four"]
   });
-})
+});
+
 run('array of numbers', function () {
   return new A({
-     numbers:[72,6493,83984643,348282.55]
+    numbers:[72,6493,83984643,348282.55]
   });
-})
+});
+
 run('array of dates', function () {
   return new A({
-     dates:[new Date, new Date, new Date]
+    dates:[new Date, new Date, new Date]
   });
-})
+});
+
 run('array of bools', function () {
   return new A({
-     bools:[true, false, false, true, true]
+    bools:[true, false, false, true, true]
   });
-})
+});
+
 run('array of buffers', function () {
   return new A({
-     buffers: [new Buffer([33]), new Buffer([12])]
+    buffers: [new Buffer([33]), new Buffer([12])]
   });
-})
+});
+
 run('array of objectids', function () {
   return new A({
-     objectids: [new mongoose.Types.ObjectId]
+    objectids: [new mongoose.Types.ObjectId]
   });
-})
+});
+
 run('array of docs', function () {
   return new A({
-     docs: [ {title: "yo"}, {title:"nowafasdi0fas asjkdfla fa" }]
+    docs: [ {title: "yo"}, {title:"nowafasdi0fas asjkdfla fa" }]
   });
-})
+});
 
 //console.error(a.toObject({depopulate:true}));
 console.error('completed %d docs in %d seconds (%d dps)', numdocs, totaltime,numdocs/totaltime);
 
-// --trace-opt --trace-deopt --trace-bailout 
+// --trace-opt --trace-deopt --trace-bailout
--- benchmarks/mem.js
+++ benchmarks/mem.js
@@ -1,29 +1,29 @@
 
 var mongoose = require('../')
-  , Schema = mongoose.Schema;
+, Schema = mongoose.Schema;
 
-var db = mongoose.connect('localhost', 'testing_bench');
+mongoose.connect('localhost', 'testing_bench');
 
 var DocSchema = new Schema({
-    title: String
+  title: String
 });
 
 var AllSchema = new Schema({
-    string: { type: String, required: true }
-  , number: { type: Number, min: 10 }
-  , date  : Date
-  , bool  : Boolean
-  , buffer: Buffer
-  , objectid: Schema.ObjectId
-  , array : Array
-  , strings: [String]
-  , numbers: [Number]
-  , dates  : [Date]
-  , bools  : [Boolean]
-  , buffers: [Buffer]
-  , objectids: [Schema.ObjectId]
-  , docs     : { type: [DocSchema], validate: function () { return true }}
-  , s: { nest: String }
+  string: { type: String, required: true }
+, number: { type: Number, min: 10 }
+, date  : Date
+, bool  : Boolean
+, buffer: Buffer
+, objectid: Schema.ObjectId
+, array : Array
+, strings: [String]
+, numbers: [Number]
+, dates  : [Date]
+, bools  : [Boolean]
+, buffers: [Buffer]
+, objectids: [Schema.ObjectId]
+, docs     : { type: [DocSchema], validate: function () { return true; }}
+, s: { nest: String }
 });
 
 var A = mongoose.model('A', AllSchema);
@@ -39,16 +39,16 @@ methods.push(function (a, cb) {
   A.findById(a._id, cb);
 }); // 4.6 MB
 methods.push(function (a, cb) {
-  A.where('number', a.number).limit(10).exec(cb)
+  A.where('number', a.number).limit(10).exec(cb);
 }); // 4.8 MB
 methods.push(function (a, cb) {
-  A.where('date', a.date).select('string').limit(10).exec(cb)
+  A.where('date', a.date).select('string').limit(10).exec(cb);
 }); // 3.5 mb
 methods.push(function (a, cb) {
-  A.where('date', a.date).select('string bool').limit(10).exec(cb)
+  A.where('date', a.date).select('string bool').limit(10).exec(cb);
 }); // 3.5 MB
 methods.push(function (a, cb) {
-  A.where('date', a.date).where('array').in(3).limit(10).exec(cb)
+  A.where('date', a.date).where('array').in(3).limit(10).exec(cb);
 }); // 1.82 MB
 methods.push(function (a, cb) {
   A.update({ _id: a._id }, { $addToset: { array: "heeeeello" }}, cb);
@@ -78,7 +78,7 @@ methods.push(function (a, cb) {
   a.s.nest = "aooooooga";
 
   if (i%2)
-  a.toObject({ depopulate: true });
+    a.toObject({ depopulate: true });
   else {
     if (a._delta) {
       a._delta();
@@ -97,32 +97,31 @@ var i = total;
 
 mongoose.connection.on('open', function () {
   mongoose.connection.db.dropDatabase(function () {
-
-    ;(function cycle () {
+    (function cycle () {
       if (0 === i--) return done();
 
       var a = new A({
-          string: "hello world"
-        , number: 444848484
-        , date: new Date
-        , bool: true
-        , buffer: new Buffer(0)
-        , objectid: new mongoose.Types.ObjectId()
-        , array: [4,{},[],"asdfa"]
-        , strings: ["one","two","three","four"]
-        , numbers:[72,6493,83984643,348282.55]
-        , dates:[new Date, new Date, new Date]
-        , bools:[true, false, false, true, true]
-        , buffers: [new Buffer([33]), new Buffer([12])]
-        , objectids: [new mongoose.Types.ObjectId]
-        , docs: [ {title: "yo"}, {title:"nowafasdi0fas asjkdfla fa" }]
+        string: "hello world"
+      , number: 444848484
+      , date: new Date
+      , bool: true
+      , buffer: new Buffer(0)
+      , objectid: new mongoose.Types.ObjectId()
+      , array: [4,{},[],"asdfa"]
+      , strings: ["one","two","three","four"]
+      , numbers:[72,6493,83984643,348282.55]
+      , dates:[new Date, new Date, new Date]
+      , bools:[true, false, false, true, true]
+      , buffers: [new Buffer([33]), new Buffer([12])]
+      , objectids: [new mongoose.Types.ObjectId]
+      , docs: [ {title: "yo"}, {title:"nowafasdi0fas asjkdfla fa" }]
       });
 
       a.save(function (err) {
-        methods[Math.random()*methods.length|0](a, function () {
-          a= u =null;
+        methods[Math.random() * methods.length|0](a, function () {
+          a = u = null;
           process.nextTick(cycle);
-        })
+        });
       });
 
       //if (i%2)
@@ -135,13 +134,13 @@ mongoose.connection.on('open', function () {
         //console.error('rss: %d, vsize: %d, heapTotal: %d, heapUsed: %d',
             //u.rss, u.vsize, u.heapTotal, u.heapUsed);
       //}
-    })()
+    })();
 
     function done () {
-      var time= (new Date - start);
+      var time = (new Date - start);
       var used = process.memoryUsage();
 
-      var res = {}
+      var res = {};
       res.rss  = used.rss - started.rss;
       res.heapTotal = used.heapTotal - started.heapTotal;
       res.heapUsed = used.heapUsed - started.heapUsed;
@@ -153,6 +152,6 @@ mongoose.connection.on('open', function () {
       });
     }
 
-  // --trace-opt --trace-deopt --trace-bailout 
-  })
-})
+  // --trace-opt --trace-deopt --trace-bailout
+  });
+});

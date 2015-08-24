f1bef6a895f30b27b7e0d99af35bb633947489c9
Valeri Karpov <valkar207@gmail.com>
2015-08-24 09:35:07 -0400

    fix; make sure to set _id before __v (Fix #3235)

--- test/model.querying.test.js
+++ test/model.querying.test.js
@@ -1534,17 +1534,17 @@ describe('model: querying:', function(){
       var db = start()
         , BlogPostB = db.model('BlogPostB', collection);
 
-      BlogPostB.create({title: 'first skip'}, function (err) {
+      BlogPostB.create({title: '1 skip'}, function (err) {
         assert.ifError(err);
-        BlogPostB.create({title: 'second skip'}, function (err, second) {
+        BlogPostB.create({title: '2 skip'}, function (err, second) {
           assert.ifError(err);
-          BlogPostB.create({title: 'third skip'}, function (err, third) {
+          BlogPostB.create({title: '3 skip'}, function (err, third) {
             assert.ifError(err);
-            BlogPostB.find({title: /skip$/}).skip(1).limit(2).find( function (err, found) {
+            BlogPostB.find({title: /skip$/}).sort({ title: 1 }).skip(1).limit(2).find( function (err, found) {
               assert.ifError(err);
               assert.equal(2,found.length);
-              assert.equal(found[0].id,second._id);
-              assert.equal(found[1].id,third._id);
+              assert.equal(found[0].id, second._id);
+              assert.equal(found[1].id, third._id);
               db.close();
               done();
             });

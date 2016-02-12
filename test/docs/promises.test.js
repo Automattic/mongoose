4237515684bbab8401e579125f2a3b204765bd83
Valeri Karpov <valkar207@gmail.com>
2016-02-11 20:59:17 -0500

    docs: clarify promiseLibrary for underlying driver re #3869

--- test/docs/promises.test.js
+++ test/docs/promises.test.js
@@ -125,4 +125,32 @@ describe('promises docs', function () {
     done();
     // acquit:ignore:end
   });
+
+  /**
+   * The `mongoose.Promise` property sets the promises mongoose uses. However,
+   * this does **not** affect the underlying MongoDB driver. If you use the
+   * underlying driver, for instance `Model.collection.db.insert()`, you
+   * need to do a little extra work to change the underlying promises library.
+   * Note that the below code assumes mongoose >= 4.4.4.
+   */
+  it('Promises for the MongoDB Driver', function (done) {
+    // acquit:ignore:start
+    if (!global.Promise) {
+      return done();
+    }
+    // acquit:ignore:end
+    var uri = 'mongodb://localhost:27017/mongoose_test';
+    // Use bluebird
+    var options = { promiseLibrary: require('bluebird') };
+    var db = mongoose.createConnection(uri, options);
+
+    Band = db.model('band-promises', { name: String });
+
+    db.on('open', function() {
+      assert.equal(Band.collection.findOne().constructor, require('bluebird'));
+      // acquit:ignore:start
+      done();
+      // acquit:ignore:end
+    });
+  });
 });

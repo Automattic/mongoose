dc0fbbb38d882f1b2eb05d719acd4bec99c1f31b
Billouboq <aurelien.boquet.web@gmail.com>
2016-12-04 00:25:46 +0100

    prettify output

--- benchmarks/benchjs/read.js
+++ benchmarks/benchjs/read.js
@@ -306,7 +306,7 @@ mongoose.connect('mongodb://localhost/mongoose-bench', function(err) {
           out.ops = item.hz;
           outObj[item.name.replace(/\s/g, '')] = out;
         });
-        console.log(JSON.stringify(outObj));
+        console.dir(outObj, {depth: null, colors: true});
       }
     });
     function next() {

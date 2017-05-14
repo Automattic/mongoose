014cbbd095393e7b871f276822e3e6912930469f
Valeri Karpov <valkar207@gmail.com>
2017-05-13 18:54:06 -0600

    refactor: consolidate _plugins and plugins

--- test/model.discriminator.test.js
+++ test/model.discriminator.test.js
@@ -316,8 +316,6 @@ describe('model', function() {
 
       it('merges callQueue with base queue defined before discriminator types callQueue', function(done) {
         assert.equal(Employee.schema.callQueue.length, 8);
-        // PersonSchema.post('save')
-        assert.strictEqual(Employee.schema.callQueue[0], Person.schema.callQueue[0]);
 
         // EmployeeSchema.pre('save')
         var queueIndex = Employee.schema.callQueue.length - 1;

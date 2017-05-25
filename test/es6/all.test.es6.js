4f9d40ad829c5e7a2947e44a647f4d7d5b48015b
Valeri Karpov <valkar207@gmail.com>
2017-05-25 17:02:18 -0600

    test(schema): repro #5227

--- test/es6/all.test.es6.js
+++ test/es6/all.test.es6.js
@@ -25,4 +25,26 @@ describe('bug fixes', function() {
     assert.ok(C === Car);
     done();
   });
+
+  it('allows overwriting base class methods (gh-5227)', function(done) {
+    class BaseModel extends mongoose.Model {
+      getString() {
+        return 'parent';
+      }
+    }
+
+    class GH5227 extends BaseModel {
+      getString() {
+        return 'child';
+      }
+    }
+
+    const UserModel = mongoose.model(GH5227, new mongoose.Schema({}));
+
+    const u = new UserModel({});
+
+    assert.equal(u.getString(), 'child');
+
+    done();
+  });
 });

3d933a83eebd4ecc348564ac6878222626623c53
Valeri Karpov <val@karpov.io>
2018-01-11 14:04:51 -0800

    style: fix lint

--- test/model.querying.test.js
+++ test/model.querying.test.js
@@ -985,9 +985,9 @@ describe('model: querying:', function() {
 
     it('where $exists', function(done) {
       var ExistsSchema = new Schema({
-            a: Number,
-            b: String
-          });
+          a: Number,
+          b: String
+      });
       mongoose.model('Exists', ExistsSchema);
       var Exists = db.model('Exists', 'exists_' + random());
       Exists.create({a: 1}, function(err) {

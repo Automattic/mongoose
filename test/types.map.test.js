298dd60f214fe060ab5131debbb0d9d319d11592
Valeri Karpov <val@karpov.io>
2018-03-27 13:15:31 -0400

    feat(map): add support for validation

---
+++ test/types.map.test.js
@@ -0,0 +1,82 @@
+/**
+ * Module dependencies.
+ */
+
+const assert = require('assert');
+const co = require('co');
+const random = require('../lib/utils').random;
+const start = require('./common');
+
+const mongoose = start.mongoose;
+const Schema = mongoose.Schema;
+
+/**
+ * Test.
+ */
+
+describe('Map', function() {
+  let db;
+
+  before(function() {
+    db = start();
+  });
+
+  after(function(done) {
+    db.close(done);
+  });
+
+  it('validation', function() {
+    let nestedValidateCalls = [];
+    let validateCalls = [];
+    const TestSchema = new mongoose.Schema({
+      v: {
+        type: Map,
+        of: {
+          type: Number,
+          validate: function(v) {
+            nestedValidateCalls.push(v);
+            return v < 4;
+          }
+        },
+        validate: function(v) {
+          validateCalls.push(v);
+          return true;
+        }
+      }
+    });
+
+    const Test = db.model('MapTest', TestSchema);
+
+    return co(function*() {
+      const doc = yield Test.create({ v: { x: 1 } });
+      assert.deepEqual(nestedValidateCalls, [1]);
+      assert.equal(validateCalls.length, 1);
+      assert.equal(validateCalls[0].get('x'), 1);
+
+      assert.ok(doc.v instanceof Map);
+
+      let threw = false;
+
+      try {
+        yield Test.create({ v: { notA: 'number' } });
+      } catch (error) {
+        threw = true;
+        assert.ok(!error.errors['v']);
+        assert.ok(error.errors['v.notA']);
+      }
+      assert.ok(threw);
+
+      doc.v.set('y', 5);
+
+      threw = false;
+      try {
+        yield doc.save();
+      } catch (error) {
+        threw = true;
+        assert.ok(!error.errors['v']);
+        assert.ok(error.errors['v.y']);
+      }
+      assert.ok(threw);
+    });
+  });
+});

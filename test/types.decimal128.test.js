f71c58a4c6917d89869d1e2bcf23b992dc388aa5
Kev <lineus@gmail.com>
2018-05-03 08:26:50 -0400

    fixes #6418

--- test/types.decimal128.test.js
+++ test/types.decimal128.test.js
@@ -25,4 +25,23 @@ describe('types.decimal128', function() {
 
     assert.strictEqual(big.value.toString(), '10000');
   });
+
+  it('uses valueOf method if one exists (gh-6418)', function() {
+    var dec128 = new Schema({
+      value: Schema.Types.Decimal128
+    });
+
+    var BigNum = mongoose.model('gh6418', dec128);
+
+    var obj = {
+      str: '10.123',
+      valueOf: function() {
+        return this.str;
+      }
+    };
+
+    var big = new BigNum({ value: obj });
+
+    assert.strictEqual(big.value.toString(), '10.123');
+  });
 });

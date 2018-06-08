2e5ac5a4ab9b09d69b11c4c170172068277667b8
Valeri Karpov <val@karpov.io>
2018-06-08 10:43:42 -0400

    test: add test coverage for building with webpack
    Fix #6457

---
+++ test/webpack.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const assert = require('assert');
+const utils = require('../lib/utils');
+const webpack = require('webpack');
+
+describe('webpack', function() {
+  it('works', function(done) {
+    this.timeout(30000);
+
+    const config = {
+      entry: ['./browser.js'],
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/i,
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      target: 'node'
+    };
+    webpack(config, utils.tick(function(error, stats) {
+      assert.ifError(error);
+      assert.deepEqual(stats.compilation.errors, []);
+      done();
+    }));
+  });
+});

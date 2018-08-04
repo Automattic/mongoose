d121c4f8396d3f6a7f13923e049ce0cea783a684
Fonger <fonger@users.noreply.github.com>
2018-08-04 17:09:30 +0800

    test(model): add delay to session tests to improve pass rate

--- test/model.test.js
+++ test/model.test.js
@@ -4908,6 +4908,7 @@ describe('Model', function() {
 
       describe('sessions (gh-6362)', function() {
         let MyModel;
+        let delay = ms => done => setTimeout(done, ms);
 
         before(function(done) {
           MyModel = db.model('gh6362', new Schema({ name: String }));
@@ -4963,12 +4964,16 @@ describe('Model', function() {
 
             let lastUse = session.serverSession.lastUse;
 
+            yield delay(1);
+
             let doc = yield MyModel.findOne({}, null, { session });
             assert.strictEqual(doc.$__.session, session);
             assert.strictEqual(doc.$session(), session);
 
             assert.ok(session.serverSession.lastUse > lastUse);
             lastUse = session.serverSession.lastUse;
+
+            yield delay(1);
 
             doc = yield MyModel.findOneAndUpdate({}, { name: 'test2' },
               { session: session });
@@ -4977,6 +4982,8 @@ describe('Model', function() {
 
             assert.ok(session.serverSession.lastUse > lastUse);
             lastUse = session.serverSession.lastUse;
+
+            yield delay(1);
 
             doc.name = 'test3';
 
@@ -4996,6 +5003,8 @@ describe('Model', function() {
 
             let lastUse = session.serverSession.lastUse;
 
+            yield delay(1);
+
             let docs = yield MyModel.find({ _id: doc._id }, null,
               { session: session });
             assert.equal(docs.length, 1);
@@ -5004,6 +5013,8 @@ describe('Model', function() {
 
             assert.ok(session.serverSession.lastUse > lastUse);
             lastUse = session.serverSession.lastUse;
+
+            yield delay(1);
 
             docs[0].name = 'test3';
 
@@ -5023,10 +5034,14 @@ describe('Model', function() {
 
             let lastUse = session.serverSession.lastUse;
 
+            yield delay(1);
+
             let doc = yield MyModel.findOne({}, null, { session });
 
             assert.ok(session.serverSession.lastUse > lastUse);
             lastUse = session.serverSession.lastUse;
+
+            yield delay(1);
 
             doc.name = 'test3';
 

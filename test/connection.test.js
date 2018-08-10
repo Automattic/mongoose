127c75061b6d007a3d42e0b05493c3ec06eeb6ca
Fonger <5862369+Fonger@users.noreply.github.com>
2018-08-10 13:53:24 +0800

    test(connection): adjust delay time to improve ci pass rate

--- test/connection.test.js
+++ test/connection.test.js
@@ -143,7 +143,7 @@ describe('connections:', function() {
           }).
           then(function() {
             return new Promise(function(resolve) {
-              setTimeout(function() { resolve(); }, 50);
+              setTimeout(function() { resolve(); }, 100);
             });
           }).
           then(function() {
@@ -157,7 +157,7 @@ describe('connections:', function() {
           }).
           then(function() {
             return new Promise(function(resolve) {
-              setTimeout(function() { resolve(); }, 2000);
+              setTimeout(function() { resolve(); }, 4000);
             });
           }).
           then(function() {

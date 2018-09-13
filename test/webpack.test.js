6dd98c828f8740a69f90f6863c6bcedb4b5e0ed2
Valeri Karpov <val@karpov.io>
2018-09-13 17:56:44 -0400

    style: fix lint

--- test/webpack.test.js
+++ test/webpack.test.js
@@ -3,7 +3,6 @@
 const acorn = require('acorn');
 const assert = require('assert');
 const fs = require('fs');
-const path = require('path');
 const utils = require('../lib/utils');
 const semver = require('semver');
 

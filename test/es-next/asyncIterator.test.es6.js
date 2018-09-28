2ce540815c9e093f9fb11a9657aabea44b4b5566
Valeri Karpov <val@karpov.io>
2018-09-28 13:58:17 -0400

    feat(aggregate): support for/await/of for Model.aggregate()
    Fix #6737

--- test/es-next/asyncIterator.test.es6.js
+++ test/es-next/asyncIterator.test.es6.js
@@ -11,30 +11,40 @@ const mongoose = start.mongoose;
 
 describe('asyncIterator', function() {
   let db;
+  let Movie;
 
-  before(function() {
-    db = start();
-  });
+  before(async function() {
+    db = await start();
 
-  after(function(done) {
-    db.close(done);
-  });
-
-  it('works (gh-6737)', async function() {
     const schema = new mongoose.Schema({ name: String });
-    const Movie = db.model('gh6737_Movie', schema);
+    Movie = db.model('gh6737_Movie', schema);
 
     await Movie.create([
       { name: 'Kickboxer' },
       { name: 'Ip Man' },
       { name: 'Enter the Dragon' }
     ]);
+  });
 
+  after(function(done) {
+    db.close(done);
+  });
+
+  it('supports for/await/of on a query (gh-6737)', async function() {
     let names = [];
     for await (const doc of Movie.find().sort({ name: 1 })) {
       names.push(doc.name);
     }
 
     assert.deepEqual(names, ['Enter the Dragon', 'Ip Man', 'Kickboxer']);
+  });
+
+  it('supports for/await/of on a query (gh-6737)', async function() {
+    let names = [];
+    for await (const doc of Movie.aggregate([{ $sort: { name: -1 } }])) {
+      names.push(doc.name);
+    }
+
+    assert.deepEqual(names, ['Kickboxer', 'Ip Man', 'Enter the Dragon']);
   });
 });

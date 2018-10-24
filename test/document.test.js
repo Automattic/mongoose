26b7c766a78230ae4b42d05365efadc0c83fb46c
Valeri Karpov <val@karpov.io>
2018-10-24 10:37:16 -0400

    test(document): repro #7145

--- test/document.test.js
+++ test/document.test.js
@@ -6329,4 +6329,27 @@ describe('document', function() {
 
     return Promise.resolve();
   });
+
+  it('supports validator.isUUID as a custom validator (gh-7145)', function() {
+    const schema = new Schema({
+      name: {
+        type: String,
+        validate: [validator.isUUID, 'invalid name']
+      }
+    });
+
+    const Test = db.model('gh7145', schema);
+
+    const doc = new Test({ name: 'not-a-uuid' });
+    const error = doc.validateSync();
+    assert.ok(error instanceof Error);
+    assert.ok(/invalid name/.test(error.message));
+
+    return co(function*() {
+      const error = yield doc.validate().then(() => null, err => err);
+
+      assert.ok(error instanceof Error);
+      assert.ok(/invalid name/.test(error.message));
+    });
+  });
 });

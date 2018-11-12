be66eb2acbf758db6f66c04733d98b2e77d05a20
Valeri Karpov <val@karpov.io>
2018-11-12 11:01:46 -0500

    docs(api): link to bulk write result object in `bulkWrite()` docs
    Fix #7225

--- docs/source/api.js
+++ docs/source/api.js
@@ -80,6 +80,9 @@ function parse() {
             ctx.string = `${ctx.constructor}.${ctx.name}`;
             break;
           case 'return':
+            tag.description = tag.description ?
+              md.parse(tag.description).replace(/^<p>/, '').replace(/<\/p>$/, '') :
+              '';
             ctx.return = tag;
             break;
           case 'inherits':

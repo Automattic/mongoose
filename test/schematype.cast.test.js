b07585060cb84abb561cea0e02e21090c4c8adc5
Valeri Karpov <val@karpov.io>
2018-12-11 13:01:33 -0500

    feat(boolean): add custom casting logic for booleans

--- test/schematype.cast.test.js
+++ test/schematype.cast.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const ObjectId = require('bson').ObjectId;
 const Schema = require('../lib/schema');
 const assert = require('assert');
 
@@ -20,7 +21,7 @@ describe('SchemaType.cast() (gh-7045)', function() {
       return original.objectid(v);
     });
 
-    const objectid = new Schema.ObjectId();
+    const objectid = new Schema.ObjectId('test', { suppressWarning: true });
 
     let threw = false;
     try {
@@ -30,5 +31,57 @@ describe('SchemaType.cast() (gh-7045)', function() {
       assert.equal(error.name, 'CastError');
     }
     assert.ok(threw);
+  });
+
+  it('handles disabling casting', function() {
+    Schema.ObjectId.cast(false);
+
+    const objectid = new Schema.ObjectId('test', { suppressWarning: true });
+
+    let threw = false;
+    try {
+      objectid.cast('000000000000000000000000');
+    } catch (error) {
+      threw = true;
+      assert.equal(error.name, 'CastError');
+    }
+    assert.ok(threw);
+
+    objectid.cast(new ObjectId()); // Should not throw
+  });
+
+  it('handles boolean', function() {
+    Schema.ObjectId.cast(v => {
+      assert.ok(v == null || typeof v === 'string');
+      return original.objectid(v);
+    });
+
+    const objectid = new Schema.ObjectId('test', { suppressWarning: true });
+
+    let threw = false;
+    try {
+      objectid.cast(123);
+    } catch (error) {
+      threw = true;
+      assert.equal(error.name, 'CastError');
+    }
+    assert.ok(threw);
+  });
+
+  it('handles disabling casting', function() {
+    Schema.Types.Boolean.cast(false);
+
+    const b = new Schema.Types.Boolean();
+
+    let threw = false;
+    try {
+      b.cast(1);
+    } catch (error) {
+      threw = true;
+      assert.equal(error.name, 'CastError');
+    }
+    assert.ok(threw);
+
+    b.cast(true); // Should not throw
   });
 });

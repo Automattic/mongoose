1861d87392323456d3a4be7dcb5cc1fda551eba4
Valeri Karpov <val@karpov.io>
2018-12-30 22:34:48 -0500

    refactor(test): move OOP discriminator tests to main test file now that we always use node >= 4

--- test/es-next.test.js
+++ test/es-next.test.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if (parseInt(process.versions.node.split('.')[0], 10) >= 4) {
-  require('./es-next/all.test.es6.js');
-}
-
 if (parseInt(process.versions.node.split('.')[0], 10) >= 10) {
   require('./es-next/asyncIterator.test.es6.js');
 }
--- test/model.discriminator.test.js
+++ test/model.discriminator.test.js
@@ -1179,4 +1179,57 @@ describe('model', function() {
       });
     });
   });
+
+  describe('bug fixes', function() {
+    it('discriminators with classes modifies class in place (gh-5175)', function(done) {
+      class Vehicle extends mongoose.Model { }
+      var V = mongoose.model(Vehicle, new mongoose.Schema());
+      assert.ok(V === Vehicle);
+      class Car extends Vehicle { }
+      var C = Vehicle.discriminator(Car, new mongoose.Schema());
+      assert.ok(C === Car);
+      done();
+    });
+
+    it('allows overwriting base class methods (gh-5227)', function(done) {
+      class BaseModel extends mongoose.Model {
+        getString() {
+          return 'parent';
+        }
+      }
+
+      class GH5227 extends BaseModel {
+        getString() {
+          return 'child';
+        }
+      }
+
+      const UserModel = mongoose.model(GH5227, new mongoose.Schema({}));
+
+      const u = new UserModel({});
+
+      assert.equal(u.getString(), 'child');
+
+      done();
+    });
+
+    it('supports adding properties (gh-5104) (gh-5635)', function(done) {
+      class Shape extends mongoose.Model { };
+      class Circle extends Shape { };
+
+      const ShapeModel = mongoose.model(Shape, new mongoose.Schema({
+        color: String
+      }));
+
+      const CircleModel = ShapeModel.discriminator(Circle, new mongoose.Schema({
+        radius: Number
+      }));
+
+      const circle = new Circle({ color: 'blue', radius: 3 });
+      assert.equal(circle.color, 'blue');
+      assert.equal(circle.radius, 3);
+
+      done();
+    });
+  });
 });

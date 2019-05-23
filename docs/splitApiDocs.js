044f0b126ef3c7048b97a0d82252072ac2412c58
Valeri Karpov <val@karpov.io>
2019-05-23 10:39:22 -0400

    docs: proof of concept for split api docs
    Re: #7812

---
+++ docs/splitApiDocs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const api = require('./source/api');
+const fs = require('fs');
+const jade = require('jade');
+const pkg = require('../package.json');
+
+api.docs.forEach(file => {
+  if (file.name === 'Index') {
+    return;
+  }
+
+  const options = Object.assign({}, file, {
+    package: pkg,
+    docs: api.docs
+  });
+
+  const html = jade.renderFile('./docs/api_split.jade', options);
+  console.log('Write', file.name);
+  fs.writeFileSync(`./docs/api/${file.name.toLowerCase()}.html`, html);
+});

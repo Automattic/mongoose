13c6e79de08da93d60610e0459f7bb949b404853
Valeri Karpov <val@karpov.io>
2019-05-27 12:25:11 -0400

    test(document): repro #7831

--- test/document.test.js
+++ test/document.test.js
@@ -7533,4 +7533,16 @@ describe('document', function() {
       assert.equal(called2, 1);
     });
   });
+
+  it('handles nested properties named `schema` (gh-7831)', function() {
+    const schema = new mongoose.Schema({ nested: { schema: String } });
+    const Model = db.model('gh7831', schema);
+
+    return co(function*() {
+      yield Model.collection.insertOne({ nested: { schema: 'test' } });
+
+      const doc = yield Model.findOne();
+      assert.strictEqual(doc.nested.schema, 'test');
+    });
+  });
 });

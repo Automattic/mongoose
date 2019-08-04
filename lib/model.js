Automattic/mongoose

commit ae8206280fa9861495b6bcf62a8fc7b5d6ca60bf

fix(model): remove $versionError from save options for better debug output
Fix #7570

lib/model.js
@@ -15,6 +15,7 @@ const MongooseMap = require('./types/map');
 const OverwriteModelError = require('./error').OverwriteModelError;
 const PromiseProvider = require('./promise_provider');
 const Query = require('./query');
+const SaveOptions = require('./options/saveOptions');
 const Schema = require('./schema');
 const VersionError = require('./error').VersionError;
 const ParallelSaveError = require('./error').ParallelSaveError;
@@ -362,7 +363,8 @@ Model.prototype.$__save = function(options, callback) {
 
         if (numAffected <= 0) {
           // the update failed. pass an error back
-          const err = options.$versionError || new VersionError(this, version, modifiedPaths);
+          const err = this.$__.$versionError ||
+            new VersionError(this, version, modifiedPaths);
           return callback(err);
         }
 
@@ -449,17 +451,12 @@ Model.prototype.save = function(options, fn) {
     options = undefined;
   }
 
-  if (options != null) {
-    options = utils.clone(options);
-  } else {
-    options = {};
-  }
-
   if (fn) {
     fn = this.constructor.$wrapCallback(fn);
   }
 
-  options.$versionError = generateVersionError(this, this.modifiedPaths());
+  options = new SaveOptions(options);
+  this.$__.$versionError = generateVersionError(this, this.modifiedPaths());
 
   return utils.promiseOrCallback(fn, cb => {
     if (parallelSave) {

lib/options/saveOptions.js
@@ -1,0 +1,14 @@
+'use strict';
+
+const utils = require('../utils');
+
+class SaveOptions {
+  constructor(obj) {
+    if (obj == null) {
+      return;
+    }
+    Object.assign(this, utils.clone(obj));
+  }
+}
+
+module.exports = SaveOptions;

commit 1dc4715f685b65086fcef86e162f2fe51e8b6814

fix(model): delete versionError after saving to prevent memory leak
Fix #8040 introduced in ae8206280fa9861495b6bcf62a8fc7b5d6ca60bf

lib/model.js
@@ -467,6 +467,7 @@ Model.prototype.save = function(options, fn) {
     this.$__save(options, error => {
       this.$__.saving = undefined;
       delete this.$__.saveOptions;
+      delete this.$__.versionError;
 
       if (error) {
         this.$__handleReject(error);
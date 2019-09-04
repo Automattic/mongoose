Automattic/mongoose

commit a33c82eef7bd1768f727f2004f96cd4115157f91

refactor(populate): allow arbitrary options to populate without modifying positional args
Re: #6520

lib/utils.js
@@ -580,21 +580,9 @@ exports.expires = function expires(object) {
  */
 
 function PopulateOptions(obj) {
-  this.path = obj.path;
-  this.match = obj.match;
-  this.select = obj.select;
-  this.options = obj.options;
-  this.model = obj.model;
-  if (obj.populate != null) {
-    this.populate = obj.populate;
-  } else if (typeof obj.subPopulate === 'object') {
+  Object.assign(this, obj);
+  if (typeof obj.subPopulate === 'object') {
     this.populate = obj.subPopulate;
-  }
-  if (obj.justOne != null) {
-    this.justOne = obj.justOne;
-  }
-  if (obj.count != null) {
-    this.count = obj.count;
   }
   this._docs = {};
 }
@@ -633,7 +621,7 @@ exports.populate = function populate(path, select, model, match, options, subPop
     } else {
       obj = { path: path };
     }
-  } else if (typeof model === 'object') {
+  } else if (model != null && typeof model === 'object') {
     obj = {
       path: path,
       select: select,
@@ -703,7 +691,10 @@ function _populateObj(obj) {
 
   const ret = [];
   const paths = obj.path.split(' ');
-  obj.options = exports.clone(obj.options);
+  if (obj.options != null) {
+    obj.options = exports.clone(obj.options);
+  }
+
   for (let i = 0; i < paths.length; ++i) {
     ret.push(new PopulateOptions(Object.assign({}, obj, { path: paths[i] })));
   }

test/query.test.js
@@ -846,39 +846,37 @@ describe('Query', function() {
 
     it('overwrites duplicate paths', function(done) {
       const q = new Query({}, {}, null, p1.collection);
-      const o = {
+      let o = {
         path: 'yellow.brick',
         match: {bricks: {$lt: 1000}},
-        select: undefined,
-        model: undefined,
-        options: undefined,
         _docs: {}
       };
-      q.populate(o);
+      q.populate(Object.assign({}, o));
       assert.equal(Object.keys(q._mongooseOptions.populate).length, 1);
-      assert.deepEqual(o, q._mongooseOptions.populate['yellow.brick']);
+      assert.deepEqual(q._mongooseOptions.populate['yellow.brick'], o);
+
       q.populate('yellow.brick');
+      o = {
+        path: 'yellow.brick',
+        _docs: {}
+      };
       assert.equal(Object.keys(q._mongooseOptions.populate).length, 1);
-      o.match = undefined;
-      assert.deepEqual(o, q._mongooseOptions.populate['yellow.brick']);
+      assert.deepEqual(q._mongooseOptions.populate['yellow.brick'], o);
       done();
     });
 
     it('accepts space delimited strings', function(done) {
       const q = new Query({}, {}, null, p1.collection);
       q.populate('yellow.brick dirt');
-      const o = {
-        path: 'yellow.brick',
-        match: undefined,
-        select: undefined,
-        model: undefined,
-        options: undefined,
-        _docs: {}
-      };
       assert.equal(Object.keys(q._mongooseOptions.populate).length, 2);
-      assert.deepEqual(o, q._mongooseOptions.populate['yellow.brick']);
-      o.path = 'dirt';
-      assert.deepEqual(o, q._mongooseOptions.populate.dirt);
+      assert.deepEqual(q._mongooseOptions.populate['yellow.brick'], {
+        path: 'yellow.brick',
+        _docs: {}
+      });
+      assert.deepEqual(q._mongooseOptions.populate['dirt'], {
+        path: 'dirt',
+        _docs: {}
+      });
       done();
     });
   });

commit b92003b41a4467ff04fd148562885b0e473934ca

Add unit test to demonstrate justOne issue
  - If the query is lean(), a field is selected in the populate, and
    the result only has one document it returns a single document
    instead of correctly returning a single document array. This bug
    was introduced in commit a33c82eef

test/model.populate.test.js
@@ -7823,6 +7823,37 @@ describe('model: populate:', function() {
     });
   });
 
+  it.only('handles virtual justOne if it is not set, is lean, and subfields are selected', function() {
+    const postSchema = new Schema({
+      name: String
+    });
+
+    postSchema.virtual('comments', {
+      ref: 'gh6988_Comment',
+      localField: '_id',
+      foreignField: 'postId'
+    });
+
+    const commentSchema = new Schema({
+      postId: { type: Schema.Types.ObjectId },
+      text: String,
+    });
+
+    const Post = db.model('gh6988_Post', postSchema);
+    const Comment = db.model('gh6988_Comment', commentSchema);
+
+    return co(function*() {
+      const post = yield Post.create({ name: 'n1'});
+      const comment = yield Comment.create({ postId: post._id, text: "a comment" });
+
+      const doc = yield Post.find({}).populate('comments', 'text').lean();
+      assert.ok(Array.isArray(doc[0].comments));
+      assert.equal(doc[0].comments.length, 1);
+      assert.equal(doc[0].comments[0]._id.toHexString(),
+        comment._id.toHexString());
+    });
+  });
+
   it('does not set `justOne` if underneath Mixed (gh-6985)', function() {
     const articleSchema = new Schema({
       title: String,
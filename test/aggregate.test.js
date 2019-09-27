e60db1b1d928a6da02f08e4890356087826e7c24
Valeri Karpov <val@Valeris-MacBook-Air.local>
2019-09-27 14:29:12 -0700

    refactor(cursor): remove dependency on async.times()
    Re: #8073
    Re: #5502

--- test/aggregate.test.js
+++ test/aggregate.test.js
@@ -1288,10 +1288,10 @@ describe('aggregate: ', function() {
         return MyModel.aggregate([{ $sort: { name: 1 } }]).
           cursor().
           exec().
-          eachAsync(checkDoc, { parallel: 2}).then(function() {
+          eachAsync(checkDoc, { parallel: 2 }).then(function() {
             assert.ok(Date.now() - startedAt[1] >= 100);
             assert.equal(startedAt.length, 2);
-            assert.ok(startedAt[1] - startedAt[0] < 50);
+            assert.ok(startedAt[1] - startedAt[0] < 50, `${startedAt[1] - startedAt[0]}`);
             assert.deepEqual(names.sort(), expectedNames);
             done();
           });

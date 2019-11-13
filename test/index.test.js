132ad41d1a28002b3e8fc675d60d5e84fd17519c
Hafez <a.hafez852@gmail.com>
2019-11-13 21:20:51 +0200

    Fix typo

--- test/index.test.js
+++ test/index.test.js
@@ -495,7 +495,7 @@ describe('mongoose module:', function() {
     return Promise.resolve();
   });
 
-  it('throws errors on setting invalid options (gh-6899)', function() {
+  it('throws an error on setting invalid options (gh-6899)', function() {
     let threw = false;
     try {
       mongoose.set('someInvalidOption', true);

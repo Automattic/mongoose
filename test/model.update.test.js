4a55040465eeeecdc0e712a78cab975dba4f8c3f
Valeri Karpov <val@karpov.io>
2020-01-07 21:59:16 -0500

    chore: remove problematic mongoose-long dep and use mongodb 3.4 in tests

--- test/model.update.test.js
+++ test/model.update.test.js
@@ -2230,26 +2230,6 @@ describe('model: update:', function() {
       });
     });
 
-    it('update handles casting with mongoose-long (gh-4283)', function(done) {
-      require('mongoose-long')(mongoose);
-
-      var Model = db.model('gh4283', {
-        number: { type: mongoose.Types.Long }
-      });
-
-      Model.create({ number: mongoose.mongo.Long.fromString('0') }, function(error) {
-        assert.ifError(error);
-        Model.update({}, { $inc: { number: mongoose.mongo.Long.fromString('2147483648') } }, function(error) {
-          assert.ifError(error);
-          Model.findOne({ number: { $type: 18 } }, function(error, doc) {
-            assert.ifError(error);
-            assert.ok(doc);
-            done();
-          });
-        });
-      });
-    });
-
     it('single nested with runValidators (gh-4420)', function(done) {
       var FileSchema = new Schema({
         name: String

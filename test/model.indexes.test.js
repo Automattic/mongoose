3c3505157f101c2d2d759fba8311be66b0112cc1
Hafez <a.hafez852@gmail.com>
2020-04-30 09:56:22 +0200

    test: assert string descendeing index works

--- test/model.indexes.test.js
+++ test/model.indexes.test.js
@@ -339,7 +339,8 @@ describe('model', function() {
     it('creates descending indexes from schema definition(gh-8895)', function() {
       return co(function*() {
         const userSchema = new Schema({
-          name: { type: String, index: -1 }
+          name: { type: String, index: -1 },
+          address: { type: String, index: '-1' }
         });
 
         const User = db.model('User', userSchema);
@@ -349,6 +350,7 @@ describe('model', function() {
         const indexes = yield User.collection.getIndexes();
 
         assert.ok(indexes['name_-1']);
+        assert.ok(indexes['address_-1']);
       });
     });
 

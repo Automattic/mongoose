bea601cba18141630410cff33f76249be2e60565
Valeri Karpov <val@karpov.io>
2020-05-02 16:30:41 -0400

    test(document): repro #8888

--- test/document.test.js
+++ test/document.test.js
@@ -8911,4 +8911,16 @@ describe('document', function() {
       assert.equal(updatedDoc.array[0][0].value, 'world');
     });
   });
+
+  it('reports array cast error with index (gh-8888)', function() {
+    const schema = Schema({ test: [ Number ] },
+      { autoIndex: false, autoCreate: false });
+    const Test = db.model('test', schema);
+
+    const t = new Test({ test: [1, 'world'] });
+    const err = t.validateSync();
+    assert.ok(err);
+    assert.ok(err.errors);
+    assert.ok(err.errors['test.1']);
+  });
 });

3eeccad375fbe85ad1a1031ee3e395fcb83dba02
Valeri Karpov <val@karpov.io>
2020-07-25 14:17:47 -0400

    test(document): repro #9281

--- test/document.test.js
+++ test/document.test.js
@@ -9081,4 +9081,31 @@ describe('document', function() {
       assert.strictEqual(doc.nested.myBool, true);
     });
   });
+
+  it('handles immutable properties underneath single nested subdocs when overwriting (gh-9281)', function() {
+    const SubSchema = Schema({
+      nestedProp: {
+        type: String,
+        immutable: true
+      }
+    }, { strict: 'throw' });
+
+    const TestSchema = Schema({ object: SubSchema }, { strict: 'throw' });
+    const Test = db.model('Test', TestSchema);
+
+    return co(function*() {
+      yield Test.create({ object: { nestedProp: 'A' } });
+      const doc = yield Test.findOne();
+
+      doc.object = {};
+      const err = yield doc.save().then(() => null, err => err);
+
+      assert.ok(err);
+      assert.ok(err.errors['object']);
+      assert.ok(err.message.includes('Path `nestedProp` is immutable'), err.message);
+
+      doc.object = { nestedProp: 'A' };
+      yield doc.save();
+    });
+  });
 });

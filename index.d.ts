a5c98c22f04c33bf114f2ba30553b006c066f864
cjroebuck <tophtfc@gmail.com>
2020-12-08 23:43:30 +0000

    Allow array of validators in SchemaTypeOptions

--- index.d.ts
+++ index.d.ts
@@ -1303,7 +1303,7 @@ declare module "mongoose" {
     alias?: string;
 
     /** Function or object describing how to validate this schematype. See [validation docs](https://mongoosejs.com/docs/validation.html). */
-    validate?: RegExp | [RegExp, string] | Function | [Function , string] | ValidateOpts<T>;
+    validate?: RegExp | [RegExp, string] | Function | [Function , string] | ValidateOpts<T> | ValidateOpts<T>[];
 
     /** Allows overriding casting logic for this individual path. If a string, the given string overwrites Mongoose's default cast error message. */
     cast?: string;

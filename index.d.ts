4271409eb4465396cb2ea1f3e37b70b88bdf5e83
Valeri Karpov <val@karpov.io>
2020-12-19 21:16:30 -0500

    fix(index.d.ts): allow `id` paths with non-string values in TypeScript
    Fix #9723

--- index.d.ts
+++ index.d.ts
@@ -475,7 +475,7 @@ declare module "mongoose" {
     getChanges(): UpdateQuery<this>;
 
     /** The string version of this documents _id. */
-    id?: string;
+    id?: any;
 
     /** Signal that we desire an increment of this documents version. */
     increment(): this;

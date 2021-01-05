ef3038e0746947fed40e71fb2c1524aec7c875bc
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-01-05 17:20:56 -0500

    feat: Implemented setting explain flag

--- test/model.test.js
+++ test/model.test.js
@@ -7082,5 +7082,24 @@ describe('Model', function() {
         assert.equal(user3.name, 'Hafez2');
       });
     });
+
+    it('should set the explain flag on the query (gh-8075)', function() {
+      return co(function*() {
+        const MyModel = mongoose.model('Character', mongoose.Schema({
+          name: String,
+          age: Number,
+          rank: String
+        }));
+        
+        MyModel.create([
+          { name: 'Jean-Luc Picard', age: 59, rank: 'Captain' },
+          { name: 'William Riker', age: 29, rank: 'Commander' },
+          { name: 'Deanna Troi', age: 28, rank: 'Lieutenant Commander' },
+          { name: 'Geordi La Forge', age: 29, rank: 'Lieutenant' },
+          { name: 'Worf', age: 24, rank: 'Lieutenant' }
+        ]);
+        assert.equal(typeof MyModel.exists({}, {explain: true}), 'object');
+      });
+    });
   });
 });

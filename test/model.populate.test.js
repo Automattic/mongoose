0d88bb6ada6edc96899bc835808426e8d529717f
Valeri Karpov <val@karpov.io>
2021-02-03 10:50:30 -0500

    style: get rid of console.logs

--- test/model.populate.test.js
+++ test/model.populate.test.js
@@ -9870,7 +9870,6 @@ describe('model: populate:', function() {
     });
   });
   it('gh-9833', function() {
-    // const util = require('util');
     const Books = db.model('books', new Schema({ name: String, tags: [{ type: Schema.Types.ObjectId, ref: 'tags' }] }));
     const Tags = db.model('tags', new Schema({ author: Schema.Types.ObjectId }));
     const Authors = db.model('authors', new Schema({ name: String }));
@@ -9898,8 +9897,6 @@ describe('model: populate:', function() {
       ];
       const books = yield Books.aggregate(aggregateOptions).exec();
 
-      // console.log('books = ' + util.inspect(books, false, null, true));
-
       const populateOptions = [{
         path: 'tags.author',
         model: 'authors',
@@ -9907,7 +9904,6 @@ describe('model: populate:', function() {
       }];
 
       const populatedBooks = yield Books.populate(books, populateOptions);
-      // console.log('populatedBooks = ' + util.inspect(populatedBooks, false, null, true));
       assert.ok(!Array.isArray(populatedBooks[0].tags[0].author));
     });
   });

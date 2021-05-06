24f6d3b389bbeffcd09e5f1c45f336f43ab95020
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-05-06 14:28:24 -0400

    lint

--- test/errors.validation.test.js
+++ test/errors.validation.test.js
@@ -258,14 +258,14 @@ describe('ValidationError', function() {
     });
   });
   it('should have error name in Cast error gh-10166', function(done) {
-    const testSchema = new Schema({ text: { type: String, required: [true, 'Text is required']}, number: {
+    const testSchema = new Schema({ text: { type: String, required: [true, 'Text is required'] }, number: {
       type: Number, required: [true, 'Number is required'] } });
     const Test = mongoose.model('Test', testSchema);
-    const entry = new Test({ 'text': false, 'number': 'fsfsf' });
+    const entry = new Test({ text: false, number: 'fsfsf' });
     entry.validate(function(error) {
       assert.ok(JSON.parse(JSON.stringify(error.errors.number.message)));
       assert.ok(JSON.parse(JSON.stringify(error.errors.number.name)));
-      });
+    });
     done();
   });
 });

916cf2ecbfda03329228a6a9378ac37c0aa711f8
Valeri Karpov <val@karpov.io>
2021-07-06 18:53:11 -0400

    fix: clean up last test failure for mongodb@4.0.0-beta.6

--- test/query.cursor.test.js
+++ test/query.cursor.test.js
@@ -465,7 +465,7 @@ describe('QueryCursor', function() {
           assert.ok(closed);
           cursor.next(function(error) {
             assert.ok(error);
-            assert.equal(error.name, 'MongoServerError');
+            assert.equal(error.name, 'MongoDriverError');
             done();
           });
         });

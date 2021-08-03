aef43dedf388941beb824354f2a707b78b72bb52
Valeri Karpov <val@karpov.io>
2021-08-03 16:11:26 -0400

    add v6 docs

---
+++ docs/v6/docs/js/navbar-search.js
@@ -0,0 +1,11 @@
+document.getElementById('search-button-nav').onclick = function() {
+  var q = document.getElementById('search-input-nav').value;
+  window.location.href = 'search.html?q=' + encodeURIComponent(q);
+};
+
+var q = document.getElementById('search-input-nav').onkeyup = function(ev) {
+  if (ev.keyCode === 13) {
+    var q = document.getElementById('search-input-nav').value;
+    window.location.href = '/docs/search.html?q=' + encodeURIComponent(q);
+  }
+};

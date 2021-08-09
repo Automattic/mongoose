Automattic/mongoose

commit 1c66cc972c90529bd0ffe8496e9de866b92a97fa

use mongodb's index specification

index.d.ts
@@ -1231,8 +1231,6 @@ declare module 'mongoose' {
   type PreSaveMiddlewareFunction<T> = (this: T, next: (err?: CallbackError) => void, opts: SaveOptions) => void | Promise<void>;
   type PostMiddlewareFunction<ThisType, ResType = any> = (this: ThisType, res: ResType, next: (err?: CallbackError) => void) => void | Promise<void>;
   type ErrorHandlingMiddlewareFunction<ThisType, ResType = any> = (this: ThisType, err: NativeError, res: ResType, next: (err?: CallbackError) => void) => void;
-  
-  type Index = { [key: string]: boolean | number | string | Index }
 
   class Schema<DocType = Document, M extends Model<DocType, any, any> = Model<any, any, any>, SchemaDefinitionType = undefined, TInstanceMethods = ExtractMethods<M>> extends events.EventEmitter {
     /**
@@ -1261,13 +1259,13 @@ declare module 'mongoose' {
 
     
     /** Defines an index (most likely compound) for this schema. */
-    index(fields: Index, options?: IndexOptions): this;
+    index(fields: mongodb.IndexSpecification, options?: IndexOptions): this;
 
     /**
      * Returns a list of indexes that this schema declares, via `schema.index()`
      * or by `index: true` in a path's options.
      */
-    indexes(): Array<Index>;
+    indexes(): Array<any>;
 
     /** Gets a schema option. */
     get(path: string): any;

commit d70ea43992823737999071890dfd88deaf8950a9

Fix type for schema.index() to reflect actual mongoose API
Commit 1c66cc972c90529bd0ffe8496e9de866b92a97fa broke the typing for `Schema.index()` by making `fields` look like mongo's `IndexSpecification`, when it's not: mongoose has its own idea about what the index specification looks like.

(I'm not 100% sure about the changed return value on `Schema.indexes()`... there may be prefixing and a `background` field added, as per https://github.com/Automattic/mongoose/blob/master/lib/helpers/schema/getIndexes.js.)

Fixes #10561.

index.d.ts
@@ -1246,6 +1246,8 @@ declare module 'mongoose' {
   type ExtractQueryHelpers<M> = M extends Model<any, infer TQueryHelpers> ? TQueryHelpers : {};
   type ExtractMethods<M> = M extends Model<any, any, infer TMethods> ? TMethods : {};
 
+  type IndexDefinition<T> = { [K in keyof T]: 1 | -1; };
+
   type PreMiddlewareFunction<T> = (this: T, next: (err?: CallbackError) => void) => void | Promise<void>;
   type PreSaveMiddlewareFunction<T> = (this: T, next: (err?: CallbackError) => void, opts: SaveOptions) => void | Promise<void>;
   type PostMiddlewareFunction<ThisType, ResType = any> = (this: ThisType, res: ResType, next: (err?: CallbackError) => void) => void | Promise<void>;
@@ -1277,13 +1279,13 @@ declare module 'mongoose' {
     eachPath(fn: (path: string, type: SchemaType) => void): this;
 
     /** Defines an index (most likely compound) for this schema. */
-    index(fields: mongodb.IndexSpecification, options?: IndexOptions): this;
+    index(fields: IndexDefinition<SchemaDefinition<DocumentDefinition<SchemaDefinitionType>>>, options?: IndexOptions): this;
 
     /**
      * Returns a list of indexes that this schema declares, via `schema.index()`
      * or by `index: true` in a path's options.
      */
-    indexes(): Array<any>;
+    indexes(): Array<IndexDefinition<SchemaDefinition<DocumentDefinition<SchemaDefinitionType>>>>;
 
     /** Gets a schema option. */
     get<K extends keyof SchemaOptions>(key: K): SchemaOptions[K];
dbf9b527f4263573e25e00e6d672c6f3973d7949
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2021-11-05 14:18:19 +0100

    apply review

--- index.d.ts
+++ index.d.ts
@@ -1258,7 +1258,7 @@ declare module 'mongoose' {
     childSchemas: { schema: Schema, model: any }[];
 
     /** Returns a copy of this schema */
-    clone<T extends DocType = DocType>(): Schema<T>;
+    clone<T extends Schema<DocType> = this>(): T;
 
     /** Object containing discriminators defined on this schema */
     discriminators?: { [name: string]: Schema };

cea80bcd907b2538ce2dc8a173b56b1b943b1a80
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2021-12-23 08:47:53 +0200

    fix(index): export ConnectionStates
    It is declared as an enum in ts definitions, but did not really work.

--- test/typescript/global.ts
+++ test/typescript/global.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const m: mongoose.Mongoose = new mongoose.Mongoose();
 
 m.STATES.connected;
+m.ConnectionStates.connected;
 
 m.connect('mongodb://localhost:27017/test').then(() => {
   console.log('Connected!');

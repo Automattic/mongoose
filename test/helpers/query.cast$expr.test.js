fc08d9212804cf01caec2fcc535014770965b5ea
Valeri Karpov <val@karpov.io>
2022-01-22 13:02:08 -0500

    feat(query): basic $expr casting for nested expressions and arithmetic operators
    Fix #10663

--- test/helpers/query.cast$expr.test.js
+++ test/helpers/query.cast$expr.test.js
@@ -11,8 +11,17 @@ describe('castexpr', function() {
     let res = cast$expr({ $eq: ['$date', '2021-06-01'] }, testSchema);
     assert.deepEqual(res, { $eq: ['$date', new Date('2021-06-01')] });
 
+    res = cast$expr({ $eq: [{ $year: '$date' }, 2021] }, testSchema);
+    assert.deepStrictEqual(res, { $eq: [{ $year: '$date' }, 2021] });
+
     res = cast$expr({ $eq: [{ $year: '$date' }, '2021'] }, testSchema);
     assert.deepStrictEqual(res, { $eq: [{ $year: '$date' }, 2021] });
+
+    res = cast$expr({ $eq: [{ $year: '$date' }, { $literal: '2021' }] }, testSchema);
+    assert.deepStrictEqual(res, { $eq: [{ $year: '$date' }, { $literal: 2021 }] });
+
+    res = cast$expr({ $eq: [{ $year: '$date' }, { $literal: '2021' }] }, testSchema);
+    assert.deepStrictEqual(res, { $eq: [{ $year: '$date' }, { $literal: 2021 }] });
 
     res = cast$expr({ $gt: ['$spent', '$budget'] }, testSchema);
     assert.deepStrictEqual(res, { $gt: ['$spent', '$budget'] });
@@ -21,19 +30,19 @@ describe('castexpr', function() {
   it('casts conditions', function() {
     const testSchema = new Schema({ price: Number, qty: Number });
 
-    const discountedPrice = {
+    let discountedPrice = {
       $cond: {
-        if: { $gte: ['$qty', '100'] },
+        if: { $gte: ['$qty', { $floor: '100' }] },
         then: { $multiply: ['$price', '0.5'] },
         else: { $multiply: ['$price', '0.75'] }
       }
     };
-    const res = cast$expr({ $lt: [discountedPrice, 5] }, testSchema);
+    let res = cast$expr({ $lt: [discountedPrice, 5] }, testSchema);
     assert.deepStrictEqual(res, {
       $lt: [
         {
           $cond: {
-            if: { $gte: ['$qty', 100] },
+            if: { $gte: ['$qty', { $floor: 100 }] },
             then: { $multiply: ['$price', 0.5] },
             else: { $multiply: ['$price', 0.75] }
           }
@@ -41,5 +50,41 @@ describe('castexpr', function() {
         5
       ]
     });
+
+    discountedPrice = {
+      $cond: {
+        if: { $and: [{ $gte: ['$qty', { $floor: '100' }] }] },
+        then: { $multiply: ['$price', '0.5'] },
+        else: { $multiply: ['$price', '0.75'] }
+      }
+    };
+    res = cast$expr({ $lt: [discountedPrice, 5] }, testSchema);
+    assert.deepStrictEqual(res, {
+      $lt: [
+        {
+          $cond: {
+            if: { $and: [{ $gte: ['$qty', { $floor: 100 }] }] },
+            then: { $multiply: ['$price', 0.5] },
+            else: { $multiply: ['$price', 0.75] }
+          }
+        },
+        5
+      ]
+    });
+  });
+
+  it('casts boolean expressions', function() {
+    const testSchema = new Schema({ date: Date, spent: Number, budget: Number });
+
+    const res = cast$expr({ $and: [{ $eq: [{ $year: '$date' }, '2021'] }] }, testSchema);
+    assert.deepStrictEqual(res, { $and: [{ $eq: [{ $year: '$date' }, 2021] }] });
+  });
+
+  it('cast errors', function() {
+    const testSchema = new Schema({ date: Date, spent: Number, budget: Number });
+
+    assert.throws(() => {
+      cast$expr({ $eq: [{ $year: '$date' }, 'not a number'] }, testSchema);
+    }, /Cast to Number failed/);
   });
 });

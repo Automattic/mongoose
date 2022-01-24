ed53d6cf2e838dd20ef4158d4080ca330b1f1f87
uzlopak <aras.abbasi@googlemail.com>
2022-01-24 11:11:20 +0100

    lint

--- test/helpers/parentPaths.test.js
+++ test/helpers/parentPaths.test.js
@@ -3,14 +3,14 @@
 const assert = require('assert');
 const parentPaths = require('../../lib/helpers/path/parentPaths');
 
-describe('parentPaths', function () {
-  it('first', function () {
+describe('parentPaths', function() {
+  it('first', function() {
     assert.deepEqual(parentPaths('first'), ['first']);
   });
-  it('first.second', function () {
+  it('first.second', function() {
     assert.deepEqual(parentPaths('first.second'), ['first', 'first.second']);
   });
-  it('first.', function () {
+  it('first.', function() {
     assert.deepEqual(parentPaths('first.'), ['first', 'first.']);
   });
 });

ae384693cbc22b058b70acea09ef8fd529d53940
uzlopak <aras.abbasi@googlemail.com>
2022-01-24 11:08:37 +0100

    add unit tests for parentPaths

---
+++ test/helpers/parentPaths.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+const parentPaths = require('../../lib/helpers/path/parentPaths');
+
+describe('parentPaths', function () {
+  it('first', function () {
+    assert.deepEqual(parentPaths('first'), ['first']);
+  });
+  it('first.second', function () {
+    assert.deepEqual(parentPaths('first.second'), ['first', 'first.second']);
+  });
+  it('first.', function () {
+    assert.deepEqual(parentPaths('first.'), ['first', 'first.']);
+  });
+});

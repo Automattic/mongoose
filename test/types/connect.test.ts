0e8b0a0ab123674f78b9186b0b79ad51087dbdf1
uzlopak <aras.abbasi@googlemail.com>
2022-02-08 02:36:50 +0100

    avoid empty fn error

--- test/types/connect.test.ts
+++ test/types/connect.test.ts
@@ -7,6 +7,6 @@ expectType<Promise<typeof mongoose>>(connect('mongodb://localhost:27017/test', {
 expectType<Promise<typeof mongoose>>(connect('mongodb://localhost:27017/test', { bufferCommands: true }));
 
 // Callback
-expectType<void>(connect('mongodb://localhost:27017/test', (err: Error | null) => {}));
-expectType<void>(connect('mongodb://localhost:27017/test', {}, (err: Error | null) => {}));
-expectType<void>(connect('mongodb://localhost:27017/test', { bufferCommands: true }, (err: Error | null) => {}));
+expectType<void>(connect('mongodb://localhost:27017/test', (err: Error | null) => { return; }));
+expectType<void>(connect('mongodb://localhost:27017/test', {}, (err: Error | null) => { return; }));
+expectType<void>(connect('mongodb://localhost:27017/test', { bufferCommands: true }, (err: Error | null) => { return; }));

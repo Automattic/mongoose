4b1e8e8e93fcc9887c3badc8f7c3a097fdf67c2b
uzlopak <aras.abbasi@googlemail.com>
2022-02-09 07:46:26 +0100

    fix linting issues

--- types/Expression.d.ts
+++ types/Expression.d.ts
@@ -17,7 +17,7 @@ declare module 'mongoose' {
         export interface Abs {
             /**
              * Returns the absolute value of a number.
-             * 
+             *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/abs/#mongodb-expression-exp.-abs
              */
             $abs: string | ArithmeticExpressionOperators;
@@ -25,7 +25,7 @@ declare module 'mongoose' {
         export interface Add {
             /**
              * Adds numbers to return the sum, or adds numbers and a date to return a new date. If adding numbers and a date, treats the numbers as milliseconds. Accepts any number of argument expressions, but at most, one expression can resolve to a date.
-             * 
+             *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/add/#mongodb-expression-exp.-add
              */
             $add: (string | ArithmeticExpressionOperators | DateExpressionOperators)[];
@@ -33,7 +33,7 @@ declare module 'mongoose' {
         export interface Subtract {
             /**
              * Returns the result of subtracting the second value from the first. If the two values are numbers, return the difference. If the two values are dates, return the difference in milliseconds. If the two values are a date and a number in milliseconds, return the resulting date. Accepts two argument expressions. If the two values are a date and a number, specify the date argument first as it is not meaningful to subtract a date from a number.
-             * 
+             *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/subtract/#mongodb-expression-exp.-subtract
              */
             $subtract: (string | ArithmeticExpressionOperators | DateExpressionOperators)[];
@@ -41,7 +41,7 @@ declare module 'mongoose' {
         export interface DateAdd {
             /**
              * Adds a number of time units to a date object.
-             * 
+             *
              * @version 5.0.0
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/dateAdd/#mongodb-expression-exp.-dateAdd
              */

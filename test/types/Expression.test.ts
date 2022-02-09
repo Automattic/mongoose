f0ab5c82a4baa5113dfa982d71fa32f6af1c6b07
uzlopak <aras.abbasi@googlemail.com>
2022-02-09 14:40:55 +0100

    Arithmetic Expression Operators

--- test/types/Expression.test.ts
+++ test/types/Expression.test.ts
@@ -7,6 +7,43 @@ const abs2: Expression = { $abs: { $add: ['$price', '$fee'] } };
 const add1: Expression = { $add: ['$date', 3 * 24 * 60 * 60000] };
 const add2: Expression = { $add: ['$price', '$fee'] };
 
+const ceil: Expression = { $ceil: '$value' };
+
+const divide: Expression = { $divide: ['$hours', 8] };
+
+const exp: Expression = { $exp: '$rate' };
+
+const floor: Expression = { $floor: '$value' };
+
+const ln: Expression = { $ln: '$sales' };
+
+const log: Expression = { $log: ['$positiveInt', 2] };
+
+const log10: Expression = { $log10: '$H3O' };
+
+const mod: Expression = { $mod: ['$hours', '$tasks'] };
+
+const multiply1: Expression = { $multiply: ['$price', '$quantity'] };
+const multiply2: Expression = { $multiply: ['$price', '$quantity', '$quantity'] };
+
+const pow: Expression = { $pow: [{ $stdDevPop: '$scores.score' }, 2] };
+
+const round1: Expression = { $round: ['$value'] };
+const round2: Expression = { $round: ['$value', 1] };
+const round3: Expression = { $round: ['$value', -1] };
+
+const sqrt: Expression = {
+  $sqrt: {
+    $add: [
+      { $pow: [{ $subtract: ['$p2.y', '$p1.y'] }, 2] },
+      { $pow: [{ $subtract: ['$p2.x', '$p1.x'] }, 2] }
+    ]
+  }
+};
+
 const subtract1: Expression = { $subtract: ['$date', 3 * 24 * 60 * 60000] };
 const subtract2: Expression = { $subtract: ['$price', '$fee'] };
+
+const trunc1: Expression = { $trunc: ['$value', 1] };
+const trunc2: Expression = { $trunc: ['$value'] };
 
--- types/Expression.d.ts
+++ types/Expression.d.ts
@@ -13,6 +13,10 @@ declare module 'mongoose' {
         '$$PRUNE' |
         '$$KEEP';
 
+    type Path = string;
+
+    type NumberExpression = Path | ArithmeticExpressionOperators | DateExpressionOperators;
+
     export namespace Expression {
         export interface Abs {
             /**
@@ -20,24 +24,143 @@ declare module 'mongoose' {
              *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/abs/#mongodb-expression-exp.-abs
              */
-            $abs: string | ArithmeticExpressionOperators;
+            $abs: Path | ArithmeticExpressionOperators;
         }
+
         export interface Add {
             /**
              * Adds numbers to return the sum, or adds numbers and a date to return a new date. If adding numbers and a date, treats the numbers as milliseconds. Accepts any number of argument expressions, but at most, one expression can resolve to a date.
              *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/add/#mongodb-expression-exp.-add
              */
-            $add: (string | ArithmeticExpressionOperators | DateExpressionOperators)[];
+            $add: NumberExpression[];
         }
+
+        export interface Ceil {
+            /**
+             * Returns the smallest integer greater than or equal to the specified number.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/ceil/#mongodb-expression-exp.-ceil
+             */
+            $ceil: NumberExpression;
+        }
+
+        export interface Divide {
+            /**
+             * Returns the result of dividing the first number by the second. Accepts two argument expressions.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/divide/#mongodb-expression-exp.-divide
+             */
+            $divide: NumberExpression[];
+        }
+
+        export interface Exp {
+            /**
+             * Raises e to the specified exponent.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/exp/#mongodb-expression-exp.-exp
+             */
+            $exp: NumberExpression;
+        }
+
+        export interface Floor {
+            /**
+             * Returns the largest integer less than or equal to the specified number.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/floor/#mongodb-expression-exp.-floor
+             */
+            $floor: NumberExpression;
+        }
+
+        export interface Ln {
+            /**
+             * Calculates the natural log of a number.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/ln/#mongodb-expression-exp.-ln
+             */
+            $ln: NumberExpression;
+        }
+
+        export interface Log {
+            /**
+             * Calculates the log of a number in the specified base.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/log/#mongodb-expression-exp.-log
+             */
+            $log: [NumberExpression, NumberExpression];
+        }
+
+        export interface Log10 {
+            /**
+             * Calculates the log base 10 of a number.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/log10/#mongodb-expression-exp.-log10
+             */
+            $log10: NumberExpression;
+        }
+
+        export interface Mod {
+            /**
+             * Returns the remainder of the first number divided by the second. Accepts two argument expressions.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/mod/#mongodb-expression-exp.-mod
+             */
+            $mod: [NumberExpression, NumberExpression];
+        }
+        export interface Multiply {
+            /**
+             * Multiplies numbers to return the product. Accepts any number of argument expressions.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/multiply/#mongodb-expression-exp.-multiply
+             */
+            $multiply: NumberExpression[];
+        }
+
+        export interface Pow {
+            /**
+             * Raises a number to the specified exponent.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/pow/#mongodb-expression-exp.-pow
+             */
+            $pow: [NumberExpression, NumberExpression];
+        }
+
+        export interface Round {
+            /**
+             * Rounds a number to to a whole integer or to a specified decimal place.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/round/#mongodb-expression-exp.-round
+             */
+            $round: [NumberExpression, NumberExpression?];
+        }
+
+        export interface Sqrt {
+            /**
+             * Calculates the square root.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/sqrt/#mongodb-expression-exp.-sqrt
+             */
+            $sqrt: NumberExpression;
+        }
+
         export interface Subtract {
             /**
              * Returns the result of subtracting the second value from the first. If the two values are numbers, return the difference. If the two values are dates, return the difference in milliseconds. If the two values are a date and a number in milliseconds, return the resulting date. Accepts two argument expressions. If the two values are a date and a number, specify the date argument first as it is not meaningful to subtract a date from a number.
              *
              * @see https://docs.mongodb.com/manual/reference/operator/aggregation/subtract/#mongodb-expression-exp.-subtract
              */
-            $subtract: (string | ArithmeticExpressionOperators | DateExpressionOperators)[];
+            $subtract: NumberExpression[];
         }
+
+        export interface Trunc {
+            /**
+             * Truncates a number to a whole integer or to a specified decimal place.
+             *
+             * @see https://docs.mongodb.com/manual/reference/operator/aggregation/trunc/#mongodb-expression-exp.-trunc
+             */
+            $trunc: [NumberExpression, NumberExpression?];
+        }
+
         export interface DateAdd {
             /**
              * Adds a number of time units to a date object.
@@ -49,7 +172,7 @@ declare module 'mongoose' {
                 startDate: Types.ObjectId | Date;
                 unit: DateUnit;
                 endDate: Types.ObjectId | Date;
-                timezone?: string;
+                timezone?: Path;
             };
         }
     }
@@ -59,10 +182,21 @@ declare module 'mongoose' {
         number |
         Expression.Abs |
         Expression.Add |
-        Expression.Subtract;
+        Expression.Ceil |
+        Expression.Divide |
+        Expression.Exp |
+        Expression.Floor |
+        Expression.Ln |
+        Expression.Log |
+        Expression.Log10 |
+        Expression.Mod |
+        Expression.Multiply |
+        Expression.Pow |
+        Expression.Round |
+        Expression.Sqrt |
+        Expression.Subtract |
+        Expression.Trunc;
 
     type DateUnit = 'year' | 'quarter' | 'week' | 'month' | 'day' | 'hour' | 'minute' | 'second' | 'millisecond';
     export type DateExpressionOperators = Expression.DateAdd;
-
-
 }

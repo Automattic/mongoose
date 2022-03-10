50029d4d324b8462e470d6599ba6369221bf6080
uzlopak <aras.abbasi@googlemail.com>
2022-03-10 23:34:37 +0100

    lint check-types-extensions.js

--- test/types/check-types-extensions.js
+++ test/types/check-types-extensions.js
@@ -1,20 +1,22 @@
+'use strict';
+
 const fs = require('fs');
 
 const readFolder = (folder) => {
-    const folderContent = fs.readdirSync(folder);
+  const folderContent = fs.readdirSync(folder);
 
-    for (const entry of folderContent) {
+  for (const entry of folderContent) {
 
-        if (fs.lstatSync(folder + "/" + entry).isDirectory()) {
-            readFolder(folder + "/" + entry);
-        } else {
-            if (entry === '.gitignore' || entry.endsWith('.d.ts')) {
-                continue;
-            } else {
-                throw new Error('File ' + entry + ' is not having a valid file-extension.');
-            }
-        }
+    if (fs.lstatSync(folder + '/' + entry).isDirectory()) {
+      readFolder(folder + '/' + entry);
+    } else {
+      if (entry === '.gitignore' || entry.endsWith('.d.ts')) {
+        continue;
+      } else {
+        throw new Error('File ' + entry + ' is not having a valid file-extension.');
+      }
     }
+  }
 };
 
-readFolder('./types')
+readFolder('./types');

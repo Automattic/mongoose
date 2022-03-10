65429fbab3841d340f68d978c68836dcbb3a05f1
Simon <info@shausdorf.de>
2022-03-10 13:24:06 +0700

    fix TypeScript declaration file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="./connection.d.ts" />
-/// <reference path="./cursor.ts" />
+/// <reference path="./cursor.d.ts" />
 /// <reference path="./error.d.ts" />
 /// <reference path="./pipelinestage.d.ts" />
 /// <reference path="./schemaoptions.d.ts" />

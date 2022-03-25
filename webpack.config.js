a535ffdb767a2d35f2440321af56c1afde388d63
Hafez <a.hafez852@gmail.com>
2022-03-25 06:18:10 +0200

    chroe: merge webpack.config.js and webpack.base.config.js in a single file

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@
 
 const paths = require('path');
 
-const base = require('./webpack.base.config.js');
-
-const webpackConfig = Object.assign({}, base, {
+const webpackConfig = {
   entry: require.resolve('./browser.js'),
   output: {
     filename: './dist/browser.umd.js',
@@ -17,8 +15,35 @@ const webpackConfig = Object.assign({}, base, {
   },
   externals: [
     /^node_modules\/.+$/
-  ]
-});
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          /\/mongoose\//i,
+          /\/kareem\//i
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  },
+  node: {
+    // Replace these Node.js native modules with empty objects, Mongoose's
+    // browser library does not use them.
+    // See https://webpack.js.org/configuration/node/
+    dns: 'empty',
+    fs: 'empty',
+    module: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  target: 'web',
+  mode: 'production'
+};
 
 module.exports = webpackConfig;
 

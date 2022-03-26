2cef3814900fa72aade0c5486911520810b7462c
Valeri Karpov <val@karpov.io>
2022-03-26 14:11:55 -0400

    test: add coverage for #1946

--- test/document.test.js
+++ test/document.test.js
@@ -11218,4 +11218,24 @@ describe('document', function() {
 
     assert.deepEqual(failure.modifiedPaths(), []);
   });
+
+  it('works when passing dot notation to mixed property (gh-1946)', async function() {
+    const schema = Schema({
+      name: String,
+      mix: { type: Schema.Types.Mixed },
+      nested: { prop: String }
+    });
+    const M = db.model('Test', schema);
+    const m1 = new M({ name: 'test', 'mix.val': 'foo', 'nested.prop': 'bar' });
+    assert.equal(m1.name, 'test');
+    assert.equal(m1.mix.val, 'foo');
+    assert.equal(m1.nested.prop, 'bar');
+    await m1.save();
+    assert.equal(m1.name, 'test');
+    assert.equal(m1.mix.val, 'foo');
+
+    const doc = await M.findById(m1);
+    assert.equal(doc.name, 'test');
+    assert.equal(doc.mix.val, 'foo');
+  });
 });

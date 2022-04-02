a416f6a95b449c65d7cc1ba68483a808c12908c3
Valeri Karpov <val@karpov.io>
2022-04-02 18:03:07 -0400

    types: remove unused type re: #10349

--- types/index.d.ts
+++ types/index.d.ts
@@ -755,8 +755,6 @@ declare module 'mongoose' {
   type SchemaPreOptions = { document?: boolean, query?: boolean };
   type SchemaPostOptions = { document?: boolean, query?: boolean };
 
-  type ExtractVirtuals<M> = M extends Model<any, any, any, infer TVirtuals> ? TVirtuals : {};
-
   type IndexDirection = 1 | -1 | '2d' | '2dsphere' | 'geoHaystack' | 'hashed' | 'text';
   type IndexDefinition = Record<string, IndexDirection>;
 

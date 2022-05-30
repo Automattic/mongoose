097ffa6f3dc90b18276332e9ff0d5dbcf4a5cb88
uzlopak <aras.abbasi@googlemail.com>
2022-05-30 02:40:47 +0200

    extract indizes typings into indizes.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,6 +6,7 @@
 /// <reference path="./document.d.ts" />
 /// <reference path="./error.d.ts" />
 /// <reference path="./helpers.d.ts" />
+/// <reference path="./indizes.d.ts" />
 /// <reference path="./mongooseoptions.d.ts" />
 /// <reference path="./pipelinestage.d.ts" />
 /// <reference path="./populate.d.ts" />
@@ -36,15 +37,6 @@ declare module 'mongoose' {
   export function connect(uri: string, options: ConnectOptions, callback: CallbackWithoutResult): void;
   export function connect(uri: string, callback: CallbackWithoutResult): void;
   export function connect(uri: string, options?: ConnectOptions): Promise<Mongoose>;
-
-  /**
-   * Makes the indexes in MongoDB match the indexes defined in every model's
-   * schema. This function will drop any indexes that are not defined in
-   * the model's schema except the `_id` index, and build any indexes that
-   * are in your schema but not in MongoDB.
-   */
-  export function syncIndexes(options?: SyncIndexesOptions): Promise<ConnectionSyncIndexesResult>;
-  export function syncIndexes(options: SyncIndexesOptions | null, callback: Callback<ConnectionSyncIndexesResult>): void;
 
   /** The Mongoose module's default connection. Equivalent to `mongoose.connections[0]`, see [`connections`](#mongoose_Mongoose-connections). */
   export const connection: Connection;
@@ -139,14 +131,6 @@ declare module 'mongoose' {
   interface MongooseBulkWriteOptions {
     skipValidation?: boolean;
   }
-
-  interface IndexesDiff {
-    /** Indexes that would be created in mongodb. */
-    toCreate: Array<any>
-    /** Indexes that would be dropped in mongodb. */
-    toDrop: Array<any>
-  }
-
   export interface ModifyResult<T> {
     value: Require_id<T> | null;
     /** see https://www.mongodb.com/docs/manual/reference/command/findAndModify/#lasterrorobject */
@@ -158,7 +142,10 @@ declare module 'mongoose' {
   }
 
   export const Model: Model<any>;
-  export interface Model<T, TQueryHelpers = {}, TMethodsAndOverrides = {}, TVirtuals = {}> extends NodeJS.EventEmitter, AcceptsDiscriminator {
+  export interface Model<T, TQueryHelpers = {}, TMethodsAndOverrides = {}, TVirtuals = {}> extends
+    NodeJS.EventEmitter,
+    AcceptsDiscriminator,
+    ModelIndexOperations {
     new <DocType = AnyKeys<T> & AnyObject>(doc?: DocType, fields?: any | null, options?: boolean | AnyObject): HydratedDocument<T, TMethodsAndOverrides, TVirtuals>;
 
     aggregate<R = any>(pipeline?: PipelineStage[], options?: mongodb.AggregateOptions, callback?: Callback<R[]>): Aggregate<Array<R>>;
@@ -220,13 +207,6 @@ declare module 'mongoose' {
     createCollection<T extends mongodb.Document>(options?: mongodb.CreateCollectionOptions & Pick<SchemaOptions, 'expires'>): Promise<mongodb.Collection<T>>;
     createCollection<T extends mongodb.Document>(options: mongodb.CreateCollectionOptions & Pick<SchemaOptions, 'expires'> | null, callback: Callback<mongodb.Collection<T>>): void;
 
-    /**
-     * Similar to `ensureIndexes()`, except for it uses the [`createIndex`](http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html#createIndex)
-     * function.
-     */
-    createIndexes(callback?: CallbackWithoutResult): Promise<void>;
-    createIndexes(options?: any, callback?: CallbackWithoutResult): Promise<void>;
-
     /** Connection the model uses. */
     db: Connection;
 
@@ -247,13 +227,6 @@ declare module 'mongoose' {
     deleteOne(filter?: FilterQuery<T>, options?: QueryOptions<T>, callback?: CallbackWithoutResult): QueryWithHelpers<mongodb.DeleteResult, HydratedDocument<T, TMethodsAndOverrides, TVirtuals>, TQueryHelpers, T>;
     deleteOne(filter: FilterQuery<T>, callback: CallbackWithoutResult): QueryWithHelpers<mongodb.DeleteResult, HydratedDocument<T, TMethodsAndOverrides, TVirtuals>, TQueryHelpers, T>;
     deleteOne(callback: CallbackWithoutResult): QueryWithHelpers<mongodb.DeleteResult, HydratedDocument<T, TMethodsAndOverrides, TVirtuals>, TQueryHelpers, T>;
-
-    /**
-     * Sends `createIndex` commands to mongo for each index declared in the schema.
-     * The `createIndex` commands are sent in series.
-     */
-    ensureIndexes(callback?: CallbackWithoutResult): Promise<void>;
-    ensureIndexes(options?: any, callback?: CallbackWithoutResult): Promise<void>;
 
     /**
      * Event emitter that reports any errors that occurred. Useful for global error
@@ -319,15 +292,6 @@ declare module 'mongoose' {
     insertMany(doc: AnyKeys<T> | AnyObject, options?: InsertManyOptions, callback?: Callback<HydratedDocument<T, TMethodsAndOverrides, TVirtuals>[] | InsertManyResult<T>>): void;
     insertMany(docs: Array<AnyKeys<T> | AnyObject>, options?: InsertManyOptions, callback?: Callback<Array<HydratedDocument<T, TMethodsAndOverrides, TVirtuals>> | InsertManyResult<T>>): void;
 
-    /**
-     * Lists the indexes currently defined in MongoDB. This may or may not be
-     * the same as the indexes defined in your schema depending on whether you
-     * use the [`autoIndex` option](/docs/guide.html#autoIndex) and if you
-     * build indexes manually.
-     */
-    listIndexes(callback: Callback<Array<any>>): void;
-    listIndexes(): Promise<Array<any>>;
-
     /** The name of the model */
     modelName: string;
 
@@ -336,23 +300,6 @@ declare module 'mongoose' {
       callback?: Callback<(HydratedDocument<T, TMethodsAndOverrides, TVirtuals>)[]>): Promise<Array<HydratedDocument<T, TMethodsAndOverrides, TVirtuals>>>;
     populate(doc: any, options: PopulateOptions | Array<PopulateOptions> | string,
       callback?: Callback<HydratedDocument<T, TMethodsAndOverrides, TVirtuals>>): Promise<HydratedDocument<T, TMethodsAndOverrides, TVirtuals>>;
-
-    /**
-     * Makes the indexes in MongoDB match the indexes defined in this model's
-     * schema. This function will drop any indexes that are not defined in
-     * the model's schema except the `_id` index, and build any indexes that
-     * are in your schema but not in MongoDB.
-     */
-    syncIndexes(options?: Record<string, unknown>): Promise<Array<string>>;
-    syncIndexes(options: Record<string, unknown> | null, callback: Callback<Array<string>>): void;
-
-    /**
-     * Does a dry-run of Model.syncIndexes(), meaning that
-     * the result of this function would be the result of
-     * Model.syncIndexes().
-     */
-    diffIndexes(options?: Record<string, unknown>): Promise<IndexesDiff>
-    diffIndexes(options: Record<string, unknown> | null, callback: (err: CallbackError, diff: IndexesDiff) => void): void
 
     /**
      * Starts a [MongoDB session](https://docs.mongodb.com/manual/release-notes/3.6/#client-sessions)
@@ -628,9 +575,6 @@ declare module 'mongoose' {
   export type SchemaPreOptions = { document?: boolean, query?: boolean };
   export type SchemaPostOptions = { document?: boolean, query?: boolean };
 
-  export type IndexDirection = 1 | -1 | '2d' | '2dsphere' | 'geoHaystack' | 'hashed' | 'text';
-  export type IndexDefinition = Record<string, IndexDirection>;
-
   export type PreMiddlewareFunction<ThisType = any> = (this: ThisType, next: CallbackWithoutResultAndOptionalError) => void | Promise<void>;
   export type PreSaveMiddlewareFunction<ThisType = any> = (this: ThisType, next: CallbackWithoutResultAndOptionalError, opts: SaveOptions) => void | Promise<void>;
   export type PostMiddlewareFunction<ThisType = any, ResType = any> = (this: ThisType, res: ResType, next: CallbackWithoutResultAndOptionalError) => void | Promise<void>;
@@ -835,31 +779,6 @@ declare module 'mongoose' {
     | typeof Schema.Types.String
     | typeof Schema.Types.Buffer
     | typeof Schema.Types.ObjectId;
-
-  export interface IndexOptions extends mongodb.CreateIndexesOptions {
-    /**
-     * `expires` utilizes the `ms` module from [guille](https://github.com/guille/) allowing us to use a friendlier syntax:
-     *
-     * @example
-     * ```js
-     * const schema = new Schema({ prop1: Date });
-     *
-     * // expire in 24 hours
-     * schema.index({ prop1: 1 }, { expires: 60*60*24 })
-     *
-     * // expire in 24 hours
-     * schema.index({ prop1: 1 }, { expires: '24h' })
-     *
-     * // expire in 1.5 hours
-     * schema.index({ prop1: 1 }, { expires: '1.5h' })
-     *
-     * // expire in 7 days
-     * schema.index({ prop1: 1 }, { expires: '7d' })
-     * ```
-     */
-    expires?: number | string;
-    weights?: AnyObject;
-  }
 
   export interface ValidatorProps {
     path: string;
@@ -1206,12 +1125,6 @@ declare module 'mongoose' {
     T extends unknown[] ? LeanDocument<RawDocType>[] :
       T extends Document ? LeanDocument<RawDocType> :
         T;
-
-  export interface SyncIndexesOptions extends mongodb.CreateIndexesOptions {
-    continueOnError?: boolean
-  }
-  export type ConnectionSyncIndexesResult = Record<string, OneCollectionSyncIndexesResult>;
-  export type OneCollectionSyncIndexesResult = Array<string> & mongodb.MongoServerError;
 
   /* for ts-mongoose */
   export class mquery { }

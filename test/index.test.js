100a9337666832f26d131c37a28ef058f1149895
Hafez <a.hafez852@gmail.com>
2022-06-02 14:51:42 +0200

    test(base): add test for immutableCreatedAt

--- test/index.test.js
+++ test/index.test.js
@@ -1019,5 +1019,29 @@ describe('mongoose module:', function() {
         assert.equal(optionsSentToMongo.allowDiskUse, false);
       });
     });
+    describe('global `immutableCreatedAt` (gh-10139)', () => {
+      it('is `true` by default', () => {
+        // Arrange
+        const m = new mongoose.Mongoose();
+
+        // Act
+        const userSchema = new m.Schema({ name: String }, { timestamps: true });
+
+        // Assert
+        assert.equal(userSchema.path('createdAt').options.immutable, true);
+      });
+
+      it('can be overridden to `false`', () => {
+        // Arrange
+        const m = new mongoose.Mongoose();
+        m.set('immutableCreatedAt', false);
+
+        // Act
+        const userSchema = new m.Schema({ name: String }, { timestamps: true });
+
+        // Assert
+        assert.equal(userSchema.path('createdAt').options.immutable, false);
+      });
+    });
   });
 });

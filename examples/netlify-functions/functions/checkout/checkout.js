200668b85d17fbefc9a3af4a9e83e205c55ee4dc
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-06-03 17:57:59 -0400

    not entirely sure how to redirect

--- examples/netlify-functions/functions/checkout/checkout.js
+++ examples/netlify-functions/functions/checkout/checkout.js
@@ -1,9 +1,37 @@
 'use strict';
+const stripe = require('stripe')('insert your stripe key here');
 
 const { Cart, Order } = require('../../models');
 
 const handler = async(event) => {
   try {
+    const stripeProducts = { line_items: [] };
+    for (let i = 0; i < event.body.products.length; i++) {
+      stripeProducts.line_items.push({
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: event.body.products[i].name
+          },
+          unit_amount: event.body.products[i].productPrice,
+        },
+        quantity: event.body.quantity
+      });
+    }
+    const session = await stripe.checkout.sessions.create({
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: event.body.products
+          }
+        }
+      }],
+      mode: 'payment',
+      success_url: 'insert a url here',
+      cancel_url: 'insert a url here'
+    });
+    
     const order = await Order.create({
       items: event.body.products,
       total: event.body.total,
@@ -22,7 +50,7 @@ const handler = async(event) => {
     const cart = await Cart.findOne({ _id: event.body.cartId });
     cart.orderId = order._id;
     await cart.save();
-    return { statusCode: 500, body: { order: order, cart: cart } };
+    return { statusCode: 500, body: { order: order, cart: cart }, headers: { Location: session.url } };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }

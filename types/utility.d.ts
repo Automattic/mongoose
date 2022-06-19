c41c486d30018d6e9c05030a506357a0ffc85c3d
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-06-19 14:45:03 +0530

    types: add `InferDocTypeFromModel`

--- types/utility.d.ts
+++ types/utility.d.ts
@@ -1,15 +1,24 @@
 declare module 'mongoose' {
+  type Unpacked<T> = T extends (infer U)[]
+    ? U
+    : T extends ReadonlyArray<infer U>
+    ? U
+    : T;
 
-  type Unpacked<T> = T extends (infer U)[] ?
-    U :
-    T extends ReadonlyArray<infer U> ? U : T;
-
-  type UnpackedIntersection<T, U> = T extends null ? null : T extends (infer A)[]
+  type UnpackedIntersection<T, U> = T extends null
+    ? null
+    : T extends (infer A)[]
     ? (Omit<A, keyof U> & U)[]
     : keyof U extends never
-      ? T
-      : Omit<T, keyof U> & U;
+    ? T
+    : Omit<T, keyof U> & U;
 
-  type MergeType<A extends Record<number | string, any>, B extends Record<number | string, any>> = Omit<A, keyof B> & B;
+  type MergeType<
+    A extends Record<number | string, any>,
+    B extends Record<number | string, any>
+  > = Omit<A, keyof B> & B;
 
+  type InferDocTypeFromModel<T extends Model<any>> = T extends Model<infer U>
+    ? U
+    : never;
 }

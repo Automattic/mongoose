e852249248bfebf5c7487fdb01e0e0287c142691
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-07-01 17:10:53 -0400

    fix getCart test

--- examples/ecommerce-netlify-functions/test/getCart.test.js
+++ examples/ecommerce-netlify-functions/test/getCart.test.js
@@ -3,7 +3,7 @@
 const { describe, it, before, after } = require('mocha');
 const assert = require('assert');
 const { handler: getCart } = require('../functions/getCart');
-const { handler: addToCart } = require('../functions/addToCart');
+const fixtures = require('./fixtures');
 const mongoose = require('mongoose');
 
 
@@ -16,23 +16,13 @@ describe('Get the cart given an id', function() {
   after(async() => {
     await mongoose.disconnect();
   });
-  it('Should create a cart and then find the cart.', async function() {
+  it('Should create a cart and then find the cart. 111', async function() {
+    const cart = await fixtures.createCart({ products: null });
     const params = {
       queryStringParameters: {
-        cartId: null
+        cartId: cart._id
       },
-      body: {
-        cartId: null,
-        product: [
-          { productId: '629e5f3686d82fc73b95b396', quantity: 2 },
-          { productId: '629e5f3686d82fc73b95b398', quantity: 1 }
-        ]
-      }
     };
-    const result = await addToCart(params);
-    assert(result.body);
-    assert(result.body.items.length);
-    params.queryStringParameters.cartId = result.body._id;
     const findCart = await getCart(params);
     assert.equal(findCart.statusCode, 200);
   });

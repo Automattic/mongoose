8eb0eb418ed9b7a3d612919d67e322d76c9f4cd8
Valeri Karpov <val@karpov.io>
2022-07-02 18:22:06 -0400

    fix(types): infer schematype type from schema paths when calling `SchemaType.path()`
    Fix #11987

--- test/types/schema.test.ts
+++ test/types/schema.test.ts
@@ -612,3 +612,21 @@ function gh12003() {
 
   expectType<{ name?: string }>({} as BaseSchemaType);
 }
+
+function gh11987() {
+  interface IUser {
+    name: string;
+    email: string;
+    organization: Types.ObjectId;
+  }
+
+  const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    organization: { type: Schema.Types.ObjectId, ref: 'Organization' }
+  });
+
+  expectType<SchemaType<string>>(userSchema.path<'name'>('name'));
+  expectError(userSchema.path<'foo'>('name'));
+  expectType<SchemaTypeOptions<string>>(userSchema.path<'name'>('name').OptionsConstructor);
+}

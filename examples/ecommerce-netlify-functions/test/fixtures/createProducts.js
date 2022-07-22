d2d0c01058b8cb22e03dbd6b04d2e975a0ee0d0e
Valeri Karpov <val@karpov.io>
2022-07-21 20:11:55 -0400

    chore: remove ecommerce-netlify-functions example, moving to mongoosejs/sample-apps repo

--- examples/ecommerce-netlify-functions/test/fixtures/createProducts.js
+++
@@ -1,7 +0,0 @@
-const { Product } = require('../../models');
-
-module.exports = async function createProducts(params) {
-    const products = await Product.create(params.product);
-
-    return { products };
-};

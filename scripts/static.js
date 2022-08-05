ecab6db70710ea9afa13ef729da23fcc7d5b923f
hasezoey <hasezoey@gmail.com>
2022-08-05 14:51:09 +0200

    chore(static.js): change to import "website.js" directly and use watching ability

--- scripts/static.js
+++ scripts/static.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const handler = require('serve-handler');
+const website = require('./website');
 
 const port = process.env.PORT
   ? parseInt(process.env.PORT, 10)
   : 8089;
 
-require('http').createServer(function(req, res) {
-  // using "rewrites", because the current script file is in a different directly that what should be served
-  handler(req, res, { rewrites: [{ source: '.', destination: '..' }] }).catch(err => res.statusCode(500).send(err.message));
-}).listen(port);
+async function main() {
+  await website.pugifyAllFiles();
+  // start watching for file changes and re-compile them, so that they can be served directly
+  website.startWatch();
 
-console.log(`now listening on http://localhost:${port}`);
+  require('http').createServer(function(req, res) {
+  // using "rewrites", because the current script file is in a different directly that what should be served
+    handler(req, res, { rewrites: [{ source: '.', destination: '..' }] }).catch(err => res.statusCode(500).send(err.message));
+  }).listen(port);
+
+  console.log(`now listening on http://localhost:${port}`);
+
+}
+
+main();

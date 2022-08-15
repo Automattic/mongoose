a4f7259bf731cc0447da7328e5da48674f0feeb3
Hafez <a.hafez852@gmail.com>
2022-08-16 00:34:31 +0200

    test(model): assert with syncIndexes calling listIndexes twice

--- test/model.test.js
+++ test/model.test.js
@@ -6765,18 +6765,20 @@ describe('Model', function() {
 
         const User = db.model('User', userSchema);
 
+        await User.init();
+
         const createIndexSpy = sinon.spy(User.collection, 'createIndex');
         const listIndexesSpy = sinon.spy(User.collection, 'listIndexes');
 
         // Act
         await User.syncIndexes();
         assert.equal(createIndexSpy.callCount, 1);
-        assert.equal(listIndexesSpy.callCount, 1);
+        assert.equal(listIndexesSpy.callCount, 2);
 
         await User.syncIndexes();
 
         // Assert
-        assert.equal(listIndexesSpy.callCount, 2);
+        assert.equal(listIndexesSpy.callCount, 4);
         assert.equal(createIndexSpy.callCount, 1);
       });
     });

Automattic/mongoose

commit 6865fea216220a88d2e7128d3a8eb8228f29155e

BREAKING CHANGE: if model is registered on a non-default connection, don't register it on mongoose global
Re: #5758

Tests still failing, pushing to help debug using travis

lib/connection.js
@@ -1088,9 +1088,6 @@ Connection.prototype.model = function(name, schema, collection) {
     return sub;
   }
 
-  // lookup model in mongoose module
-  model = this.base.models[name];
-
   if (!model) {
     throw new MongooseError.MissingSchemaError(name);
   }
@@ -1142,7 +1139,6 @@ Connection.prototype.deleteModel = function(name) {
     const collectionName = model.collection.name;
     delete this.models[name];
     delete this.collections[collectionName];
-    delete this.base.modelSchemas[name];
   } else if (name instanceof RegExp) {
     const pattern = name;
     const names = this.modelNames();

lib/index.js
@@ -63,7 +63,6 @@ require('./helpers/printJestWarning');
 function Mongoose(options) {
   this.connections = [];
   this.models = {};
-  this.modelSchemas = {};
   // default global options
   this.options = Object.assign({
     pluralization: true
@@ -493,53 +492,20 @@ Mongoose.prototype.model = function(name, schema, collection, skipInit) {
     options = {};
   }
 
-  // look up schema for the collection.
-  if (!_mongoose.modelSchemas[name]) {
-    if (schema) {
-      // cache it so we only apply plugins once
-      _mongoose.modelSchemas[name] = schema;
-    } else {
-      throw new mongoose.Error.MissingSchemaError(name);
+  if (_mongoose.models.hasOwnProperty(name)) {
+    if (schema != null &&
+        schema.instanceOfSchema &&
+        schema !== _mongoose.models[name].schema) {
+      throw new _mongoose.Error.OverwriteModelError(name);
     }
+    return _mongoose.models[name];
   }
 
-  const originalSchema = schema;
   if (schema) {
     if (_mongoose.get('cloneSchemas')) {
       schema = schema.clone();
     }
     _mongoose._applyPlugins(schema);
-  }
-
-  let sub;
-
-  // connection.model() may be passing a different schema for
-  // an existing model name. in this case don't read from cache.
-  if (_mongoose.models[name] && options.cache !== false) {
-    if (originalSchema &&
-        originalSchema.instanceOfSchema &&
-        originalSchema !== _mongoose.models[name].schema) {
-      throw new _mongoose.Error.OverwriteModelError(name);
-    }
-
-    if (collection && collection !== _mongoose.models[name].collection.name) {
-      // subclass current model with alternate collection
-      model = _mongoose.models[name];
-      schema = model.prototype.schema;
-      sub = model.__subclass(_mongoose.connection, schema, collection);
-      // do not cache the sub model
-      return sub;
-    }
-
-    return _mongoose.models[name];
-  }
-
-  // ensure a schema exists
-  if (!schema) {
-    schema = this.modelSchemas[name];
-    if (!schema) {
-      throw new mongoose.Error.MissingSchemaError(name);
-    }
   }
 
   // Apply relevant "global" options to the schema
@@ -554,18 +520,17 @@ Mongoose.prototype.model = function(name, schema, collection, skipInit) {
 
   const connection = options.connection || _mongoose.connection;
   model = _mongoose.Model.compile(model || name, schema, collection, connection, _mongoose);
+  if (connection === _mongoose.connection) {
+    connection.models[name] = model;
+    _mongoose.models[name] = model;
+  }
 
   if (!skipInit) {
     // Errors handled internally, so safe to ignore error
     model.init(function $modelInitNoop() {});
   }
 
-  if (options.cache === false) {
-    return model;
-  }
-
-  _mongoose.models[name] = model;
-  return _mongoose.models[name];
+  return model;
 };
 
 /**
@@ -596,6 +561,7 @@ Mongoose.prototype.deleteModel = function(name) {
   const _mongoose = this instanceof Mongoose ? this : mongoose;
 
   _mongoose.connection.deleteModel(name);
+  delete _mongoose.models[name];
   return _mongoose;
 };
 

commit c4935c75ed80ecc93f15a6dff9b9970249ab2d9c

Update connection.js
Below code throws `MissingSchemaError` in the `populate` statement:
```
const mongoose = require("mongoose");

const { Schema } = mongoose;

async function run() {
  try {
    await mongoose.connect(process.env.MONGODB_URI);

    mongoose.set("overwriteModels", true);

    const personSchema = Schema({
      name: String,
    });

    const storySchema = Schema({
      author: { type: Schema.Types.ObjectId, ref: "Person" },
      title: String,
    });

    const Story = mongoose.model("Story", storySchema);
    const Person = mongoose.model("Person", personSchema);

    const person = await Person.create({ name: "David" });
    await Story.create({
      title: "My Story",
      author: person._id,
    });

    await Story.findOne({ title: "My Story" })
      .populate("author")
      .exec();
  } catch (error) {
    console.error(error);
  }
}

run();
```
It will works if I do not set `overwriteModels` as `true`.

I find that it is caused by 6865fea216220a88d2e7128d3a8eb8228f29155e. Looks like that commit prevents mixing models registered in non-default connection and global mongoose instance. 

How about adding back the line for model lookup but lookup from the connection itself (lookup from `this.models` instead of `this.base.models`)?

lib/connection.js
@@ -1229,6 +1229,8 @@ Connection.prototype.model = function(name, schema, collection, options) {
     // do not cache the sub model
     return sub;
   }
+  
+  model = this.models[name];
 
   if (!model) {
     throw new MongooseError.MissingSchemaError(name);
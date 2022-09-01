9aabf6256714b095a1daf7e5ed6460afd5c587dd
hasezoey <hasezoey@gmail.com>
2022-09-01 10:49:33 +0200

    test(mocha-fixtures): set some MMS environment options depending on context

--- test/mocha-fixtures.js
+++ test/mocha-fixtures.js
@@ -19,6 +19,14 @@ const startMemoryReplset = !process.env.MONGOOSE_REPLSET_URI;
 module.exports.mochaGlobalSetup = async function mochaGlobalSetup() {
   let instanceuri;
   let replseturi;
+
+  process.env.RUNTIME_DOWNLOAD = '1'; // ensure MMS is able to download binaries in this context
+
+  // set some options when running in a CI
+  if (process.env.CI) {
+    process.env.MONGOMS_PREFER_GLOBAL_PATH = '1'; // set MMS to use "~/.cache/mongodb-binaries" even when the path does not yet exist
+  }
+
   if (startMemoryInstance) { // Config to decided if an mongodb-memory-server instance should be used
     // it's needed in global space, because we don't want to create a new instance every test-suite
     mongoinstance = await mms.MongoMemoryServer.create({ instance: { args: ['--setParameter', 'ttlMonitorSleepSecs=1'] } });

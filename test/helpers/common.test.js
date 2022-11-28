f60b59a9be101400eb09ee5b1bd182cdeb77f336
Valeri Karpov <val@karpov.io>
2022-11-28 13:35:56 -0500

    test: remove unnecessary test

--- test/helpers/common.test.js
+++ test/helpers/common.test.js
@@ -9,39 +9,14 @@ const { Schema } = mongoose;
 
 
 describe('modifiedPaths, bad update value which has circular reference field', () => {
-
   it('update value can be null', function() {
     modifiedPaths(null, 'path', null);
   });
 
-  it('values with obvious error', function() {
+  it('values with obvious error on circular reference', function() {
     const objA = {};
     objA.a = objA;
 
     assert.throws(() => modifiedPaths(objA, 'path', null), /circular reference/);
-  });
-
-  it('original error i made', async function() {
-    await mongoose.connect(start.uri);
-
-    const test1Schema = new Schema({
-      v: Number,
-      n: String
-    });
-
-    const Test1Model = mongoose.model('Test1', test1Schema);
-
-    const test2Schema = new Schema({
-      v: Number
-    });
-
-    const Test2Model = mongoose.model('Test2', test2Schema);
-
-    for (let i = 0; i < 5; i++) {
-      const doc = new Test2Model({ v: i });
-      await doc.save();
-    }
-    
-    assert.rejects(() => Test1Model.updateOne({ n: 'x' }, { v: Test2Model.countDocuments() }, { upsert: true }), /circular reference/);
   });
 });

Automattic/mongoose

commit c3384bcd1dc60fa15b57f41931bac9d9c99fc6e2

docs: fix search re: #12830

docs/js/navbar-search.js
@@ -1,11 +1,18 @@
-document.getElementById('search-button-nav').onclick = function() {
-  var q = document.getElementById('search-input-nav').value;
-  window.location.href = 'search.html?q=' + encodeURIComponent(q);
-};
+(function() {
+  var versionFromUrl = window.location.pathname.match(/^\/docs\/(\d+\.x)/);
+  var version = versionFromUrl ? versionFromUrl[1] : defaultVersion;
 
-var q = document.getElementById('search-input-nav').onkeyup = function(ev) {
-  if (ev.keyCode === 13) {
+  var searchPrefix = versionFromUrl ? '/docs/' + version + '/docs/' : '/docs/';
+
+  document.getElementById('search-button-nav').onclick = function() {
     var q = document.getElementById('search-input-nav').value;
-    window.location.href = '/docs/search.html?q=' + encodeURIComponent(q);
-  }
-};
+    window.location.href = searchPrefix + 'search.html?q=' + encodeURIComponent(q);
+  };
+
+  document.getElementById('search-input-nav').onkeyup = function(ev) {
+    if (ev.keyCode === 13) {
+      var q = document.getElementById('search-input-nav').value;
+      window.location.href = searchPrefix + '/search.html?q=' + encodeURIComponent(q);
+    }
+  };
+})();

docs/js/search.js
@@ -9,8 +9,9 @@ for (var i = 0; i < pairs.length; ++i) {
   }
 }
 
+var defaultVersion = '6.x';
 var versionFromUrl = window.location.pathname.match(/^\/docs\/(\d+\.x)/);
-var version = versionFromUrl ? versionFromUrl[1] : '6.x';
+var version = versionFromUrl ? versionFromUrl[1] : defaultVersion;
 
 if (q != null) {
   document.getElementById('search-input').value = decodeURIComponent(q);

commit a46ee187dcc1459a8212917396e6b9a631a29f84

docs(navbar-search): add missing variables
was broken by c3384bcd1dc60fa15b57f41931bac9d9c99fc6e2

re #12830

docs/js/navbar-search.js
@@ -1,3 +1,5 @@
+var defaultVersion = '6.x';
+
 (function() {
   var versionFromUrl = window.location.pathname.match(/^\/docs\/(\d+\.x)/);
   var version = versionFromUrl ? versionFromUrl[1] : defaultVersion;
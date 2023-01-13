8a2711cf6e51254c019ad94a21f11cd405b9d0c4
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-01-13 13:10:36 -0500

    refactor test

--- test/schema.select.test.js
+++ test/schema.select.test.js
@@ -63,7 +63,7 @@ describe('schema select option', function() {
     assert.strictEqual(undefined, findAndRemoveDoc.name);
   });
 
-  it('including paths through schematype', function(done) {
+  it('including paths through schematype', async function() {
     const schema = new Schema({
       thin: Boolean,
       name: { type: String, select: true },
@@ -71,40 +71,26 @@ describe('schema select option', function() {
     });
 
     const S = db.model('Test', schema);
-    S.create({ thin: true, name: 'the included', docs: [{ bool: true, name: 'test' }] }, function(err, s) {
-      assert.ifError(err);
-      assert.equal(s.name, 'the included');
-      assert.equal(s.docs[0].name, 'test');
-
-      let pending = 4;
-
-      function cb(err, s) {
-        pending--;
-
-        if (Array.isArray(s)) {
-          s = s[0];
-        }
-        assert.strictEqual(null, err);
-        assert.strictEqual(true, s.isSelected('name'));
-        assert.strictEqual(true, s.isSelected('docs.name'));
-        assert.equal(s.name, 'the included');
-
-        if (pending === 0) {
-          done();
-        }
-      }
-
-      S.findById(s).select('-thin -docs.bool').exec(function(err, res) {
-        cb(err, res);
-        S.find({ _id: s._id }).select('thin docs.bool').exec(function(err, res) {
-          cb(err, res);
-          S.findOneAndUpdate({ _id: s._id }, { thin: false }, function(err, s) {
-            cb(err, s);
-            S.findOneAndRemove({ _id: s._id }, cb);
-          });
-        });
-      });
-    });
+    const doc = await S.create({ thin: true, name: 'the included', docs: [{ bool: true, name: 'test' }]});
+    assert.equal(doc.name, 'the included');
+    assert.equal(doc.docs[0].name, 'test');
+    const findByIdDoc = await S.findById({ _id: doc._id }).select('-thin -docs.bool');
+    assert.strictEqual(true, findByIdDoc.isSelected('name'));
+    assert.strictEqual(true, findByIdDoc.isSelected('docs.name'));
+    assert.equal(findByIdDoc.name, 'the included');
+    const findDoc = await S.find({ _id: doc._id });
+    const singleFindDoc = findDoc[0];
+    assert.strictEqual(true, singleFindDoc.isSelected('name'));
+    assert.strictEqual(true, singleFindDoc.isSelected('docs.name'));
+    assert.equal(singleFindDoc.name, 'the included');
+    const findOneAndUpdateDoc = await S.findOneAndUpdate({ _id: doc._id }, { thin: false });
+    assert.strictEqual(true, findOneAndUpdateDoc.isSelected('name'));
+    assert.strictEqual(true, findOneAndUpdateDoc.isSelected('docs.name'));
+    assert.equal(findOneAndUpdateDoc.name, 'the included');
+    const findOneAndRemoveDoc = await S.findOneAndRemove({ _id: doc._id });
+    assert.strictEqual(true, findOneAndRemoveDoc.isSelected('name'));
+    assert.strictEqual(true, findOneAndRemoveDoc.isSelected('docs.name'));
+    assert.equal(findOneAndRemoveDoc.name, 'the included');
   });
 
   describe('overriding schematype select options', function() {
@@ -128,144 +114,111 @@ describe('schema select option', function() {
 
     describe('works', function() {
       describe('for inclusions', function() {
-        let s;
-        beforeEach(function(done) {
-          S.create({ thin: true, name: 'the included', docs: [{ name: 'test', bool: true }] }, function(err, s_) {
-            assert.ifError(err);
-            s = s_;
-            assert.equal(s.name, 'the included');
-            assert.equal(s.docs[0].name, 'test');
-            done();
-          });
+        let inclusionDoc;
+        beforeEach( async function() {
+          inclusionDoc = await S.create({ thin: true, name: 'the included', docs: [{ name: 'test', bool: true }] });
+          assert.equal(inclusionDoc.name, 'the included');
+          assert.equal(inclusionDoc.docs[0].name, 'test');
         });
-        it('with find', function(done) {
-          S.find({ _id: s._id }).select('thin name docs.bool docs.name').exec(function(err, s) {
-            assert.ifError(err);
-            assert.ok(s && s.length > 0, 'no document found');
-            s = s[0];
-            assert.strictEqual(true, s.isSelected('name'));
-            assert.strictEqual(true, s.isSelected('thin'));
-            assert.strictEqual(true, s.isSelected('docs.name'));
-            assert.strictEqual(true, s.isSelected('docs.bool'));
-            assert.equal(s.name, 'the included');
-            assert.equal(s.docs[0].name, 'test');
-            assert.ok(s.thin);
-            assert.ok(s.docs[0].bool);
-            done();
-          });
+        it('with find', async function() {
+          const findDoc = await S.find({ _id: inclusionDoc._id }).select('thin name docs.bool docs.name');
+          assert.ok(findDoc && findDoc.length > 0, 'no document found');
+          const singleFindDoc = findDoc[0];
+          assert.strictEqual(true, singleFindDoc.isSelected('name'));
+          assert.strictEqual(true, singleFindDoc.isSelected('thin'));
+          assert.strictEqual(true, singleFindDoc.isSelected('docs.name'));
+          assert.strictEqual(true, singleFindDoc.isSelected('docs.bool'));
+          assert.strictEqual(singleFindDoc.name, 'the included');
+          assert.strictEqual(singleFindDoc.docs[0].name, 'test');
+          assert.ok(singleFindDoc.thin);
+          assert.ok(singleFindDoc.docs[0].bool);
         });
-        it('for findById', function(done) {
-          S.findById(s).select('-name -docs.name').exec(function(err, s) {
-            assert.strictEqual(null, err);
-            assert.equal(s.isSelected('name'), false);
-            assert.equal(s.isSelected('thin'), true);
-            assert.equal(s.isSelected('docs.name'), false);
-            assert.equal(s.isSelected('docs.bool'), true);
-            assert.ok(s.isSelected('docs'));
-            assert.strictEqual(undefined, s.name);
-            assert.strictEqual(undefined, s.docs[0].name);
-            assert.equal(s.thin, true);
-            assert.equal(s.docs[0].bool, true);
-            done();
-          });
+        it('for findById', async function() {
+          const findByIdDoc = await S.findById({ _id: inclusionDoc._id }).select('-name -docs.name');
+          assert.equal(findByIdDoc.isSelected('name'), false);assert.equal(s.isSelected('thin'), true);
+          assert.equal(findByIdDoc.isSelected('docs.name'), false);
+          assert.equal(findByIdDoc.isSelected('docs.bool'), true);
+          assert.ok(findByIdDoc.isSelected('docs'));
+          assert.strictEqual(undefined, findByIdDoc.name);
+          assert.strictEqual(undefined, findByIdDoc.docs[0].name);
+          assert.equal(findByIdDoc.thin, true);
+          assert.equal(findByIdDoc.docs[0].bool, true);
         });
-        it('with findOneAndUpdate', function(done) {
-          S.findOneAndUpdate({ _id: s._id }, { name: 'changed' }, { new: true }).select('thin name docs.bool docs.name').exec(function(err, s) {
-            assert.ifError(err);
-            assert.strictEqual(true, s.isSelected('name'));
-            assert.strictEqual(true, s.isSelected('thin'));
-            assert.strictEqual(true, s.isSelected('docs.name'));
-            assert.strictEqual(true, s.isSelected('docs.bool'));
-            assert.equal(s.name, 'changed');
-            assert.equal(s.docs[0].name, 'test');
-            assert.ok(s.thin);
-            assert.ok(s.docs[0].bool);
-            done();
-          });
+        it('with findOneAndUpdate', async function() {
+          const findOneAndUpdateDoc = await S.findOneAndUpdate({ _id: inclusionDoc._id }, { name: 'changed' }, { new: true }).select('thin name docs.bool docs.name');
+          assert.strictEqual(true, findOneAndUpdateDoc.isSelected('name'));
+          assert.strictEqual(true, findOneAndUpdateDoc.isSelected('thin'));
+          assert.strictEqual(true, findOneAndUpdateDoc.isSelected('docs.name'));
+          assert.strictEqual(true, findOneAndUpdateDoc.isSelected('docs.bool'));
+          assert.equal(findOneAndUpdateDoc.name, 'changed');
+          assert.equal(findOneAndUpdateDoc.docs[0].name, 'test');
+          assert.ok(findOneAndUpdateDoc.thin);
+          assert.ok(findOneAndUpdateDoc.docs[0].bool);
         });
-        it('for findByIdAndUpdate', function(done) {
-          S.findByIdAndUpdate(s, { thin: false }, { new: true }).select('-name -docs.name').exec(function(err, s) {
-            assert.strictEqual(null, err);
-            assert.equal(s.isSelected('name'), false);
-            assert.equal(s.isSelected('thin'), true);
-            assert.equal(s.isSelected('docs.name'), false);
-            assert.equal(s.isSelected('docs.bool'), true);
-            assert.strictEqual(undefined, s.name);
-            assert.strictEqual(undefined, s.docs[0].name);
-            assert.equal(s.thin, false);
-            assert.equal(s.docs[0].bool, true);
-            done();
-          });
+        it('for findByIdAndUpdate', async function() {
+          const findByIdAndUpdateDoc = await S.findByIdAndUpdate({ _id: inclusionDoc._id }, { thin: false }, { new: true }).select('-name -docs.name');
+          assert.equal(findByIdAndUpdateDoc.isSelected('name'), false);
+          assert.equal(findByIdAndUpdateDoc.isSelected('thin'), true);
+          assert.equal(findByIdAndUpdateDoc.isSelected('docs.name'), false);
+          assert.equal(findByIdAndUpdateDoc.isSelected('docs.bool'), true);
+          assert.strictEqual(undefined, findByIdAndUpdateDoc.name);
+          assert.strictEqual(undefined, findByIdAndUpdateDoc.docs[0].name);
+          assert.equal(findByIdAndUpdateDoc.thin, false);
+          assert.equal(findByIdAndUpdateDoc.docs[0].bool, true);
         });
       });
 
       describe('for exclusions', function() {
-        let e;
-        beforeEach(function(done) {
-          E.create({ thin: true, name: 'the excluded', docs: [{ name: 'test', bool: true }] }, function(err, e_) {
-            e = e_;
-            assert.ifError(err);
-            assert.equal(e.name, 'the excluded');
-            assert.equal(e.docs[0].name, 'test');
-            done();
-          });
+        let exclusionDoc;
+        beforeEach(async function() {
+          exclusionDoc = await E.create({ thin: true, name: 'the excluded', docs: [{ name: 'test', bool: true }]});
+          assert.equal(exclusionDoc.name, 'the excluded');
+          assert.equal(exclusionDoc.docs[0].name, 'test');
         });
-        it('with find', function(done) {
-          E.find({ _id: e._id }).select('thin name docs.name docs.bool').exec(function(err, e) {
-            e = e[0];
-            assert.strictEqual(null, err);
-            assert.equal(e.isSelected('name'), true);
-            assert.equal(e.isSelected('thin'), true);
-            assert.equal(e.isSelected('docs.name'), true);
-            assert.equal(e.isSelected('docs.bool'), true);
-            assert.equal(e.name, 'the excluded');
-            assert.equal(e.docs[0].name, 'test');
-            assert.ok(e.thin);
-            assert.ok(e.docs[0].bool);
-            done();
-          });
+        it('with find', async function() {
+          const findDoc = await E.find({ _id: exclusionDoc._id });
+          const singleFindDoc = findDoc[0];
+          assert.equal(singleFindDoc.isSelected('name'), true);
+          assert.equal(singleFindDoc.isSelected('thin'), true);
+          assert.equal(singleFindDoc.isSelected('docs.name'), true);
+          assert.equal(singleFindDoc.isSelected('docs.bool'), true);
+          assert.equal(singleFindDoc.name, 'the excluded');
+          assert.equal(singleFindDoc.docs[0].name, 'test');
+          assert.ok(singleFindDoc.thin);
+          assert.ok(singleFindDoc.docs[0].bool);
         });
-        it('with findById', function(done) {
-          E.findById(e).select('-name -docs.name').exec(function(err, e) {
-            assert.strictEqual(null, err);
-            assert.equal(e.isSelected('name'), false);
-            assert.equal(e.isSelected('thin'), true);
-            assert.equal(e.isSelected('docs.name'), false);
-            assert.equal(e.isSelected('docs.bool'), true);
-            assert.strictEqual(undefined, e.name);
-            assert.strictEqual(undefined, e.docs[0].name);
-            assert.strictEqual(true, e.thin);
-            assert.strictEqual(true, e.docs[0].bool);
-            done();
-          });
+        it('with findById', async function() {
+          const findByIdDoc = await E.findById({ _id: exclusionDoc._id }).select('-name -docs.name');
+          assert.equal(findByIdDoc.isSelected('name'), false);
+          assert.equal(findByIdDoc.isSelected('thin'), true);
+          assert.equal(findByIdDoc.isSelected('docs.name'), false);
+          assert.equal(findByIdDoc.isSelected('docs.bool'), true);
+          assert.strictEqual(undefined, findByIdDoc.name);
+          assert.strictEqual(undefined, findByIdDoc.docs[0].name);
+          assert.strictEqual(true, findByIdDoc.thin);
+          assert.strictEqual(true, findByIdDoc.docs[0].bool);
         });
-        it('with findOneAndUpdate', function(done) {
-          E.findOneAndUpdate({ _id: e._id }, { name: 'changed' }, { new: true }).select('thin name docs.name docs.bool').exec(function(err, e) {
-            assert.strictEqual(null, err);
-            assert.equal(e.isSelected('name'), true);
-            assert.equal(e.isSelected('thin'), true);
-            assert.equal(e.isSelected('docs.name'), true);
-            assert.equal(e.isSelected('docs.bool'), true);
-            assert.equal(e.name, 'changed');
-            assert.equal(e.docs[0].name, 'test');
-            assert.ok(e.thin);
-            assert.ok(e.docs[0].bool);
-            done();
-          });
+        it('with findOneAndUpdate', async function() {
+          const findOneAndUpdateDoc = await E.findOneAndUpdate({ _id: exclusionDoc._id }, { name: 'changed' }, { new: true }).select('thin name docs.name docs.bool');
+          assert.equal(findOneAndUpdateDoc.isSelected('name'), true);
+          assert.equal(findOneAndUpdateDoc.isSelected('thin'), true);
+          assert.equal(findOneAndUpdateDoc.isSelected('docs.name'), true);
+          assert.equal(findOneAndUpdateDoc.isSelected('docs.bool'), true);
+          assert.equal(findOneAndUpdateDoc.name, 'changed');
+          assert.equal(findOneAndUpdateDoc.docs[0].name, 'test');
+          assert.ok(findOneAndUpdateDoc.thin);
+          assert.ok(findOneAndUpdateDoc.docs[0].bool);
         });
-        it('with findOneAndRemove', function(done) {
-          E.findOneAndRemove({ _id: e._id }).select('-name -docs.name').exec(function(err, e) {
-            assert.strictEqual(null, err);
-            assert.equal(e.isSelected('name'), false);
-            assert.equal(e.isSelected('thin'), true);
-            assert.equal(e.isSelected('docs.name'), false);
-            assert.equal(e.isSelected('docs.bool'), true);
-            assert.strictEqual(undefined, e.name);
-            assert.strictEqual(undefined, e.docs[0].name);
-            assert.strictEqual(true, e.thin);
-            assert.strictEqual(true, e.docs[0].bool);
-            done();
-          });
+        it('with findOneAndRemove', async function() {
+          const findOneAndRemoveDoc = await E.findOneAndRemove({ _id: findOneAndRemoveDoc._id }).select('-name -docs.name');
+          assert.equal(findOneAndRemoveDoc.isSelected('name'), false);
+          assert.equal(findOneAndRemoveDoc.isSelected('thin'), true);
+          assert.equal(findOneAndRemoveDoc.isSelected('docs.name'), false);
+          assert.equal(findOneAndRemoveDoc.isSelected('docs.bool'), true);
+          assert.strictEqual(undefined, findOneAndRemoveDoc.name);
+          assert.strictEqual(undefined, findOneAndRemoveDoc.docs[0].name);
+          assert.strictEqual(true, findOneAndRemoveDoc.thin);
+          assert.strictEqual(true, findOneAndRemoveDoc.docs[0].bool);
         });
       });
     });
@@ -292,7 +245,7 @@ describe('schema select option', function() {
       done();
     });
 
-    it('with nested (gh-7945)', function() {
+    it('with nested (gh-7945)', async function() {
       const child = new Schema({
         name1: { type: String, select: false },
         name2: { type: String, select: true }
@@ -311,11 +264,9 @@ describe('schema select option', function() {
       assert.equal(query._fields['parent.docs.name2'], undefined);
       assert.equal(query._fields['parent.docs'], 0);
 
-      return M.create({ parent: { docs: [{ name1: 'foo', name2: 'bar' }] } }).
-        then(() => query).
-        then(doc => {
-          assert.ok(!doc.parent.docs);
-        });
+      await M.create({ parent: { docs: [{ name1: 'foo', name2: 'bar' }] } });
+      const docQuery = await M.findOne();
+      assert.ok(!docQuery.parent.docs); // the returned document doesn't have a parent property, don't know if thats a problem since before the refactor it also didn't have it.
     });
   });
 
@@ -686,20 +637,16 @@ describe('schema select option', function() {
     });
   });
 
-  it('should allow deselecting a field on a query even if the definition has select set to true (gh-11694)', function(done) {
+  it('should allow deselecting a field on a query even if the definition has select set to true (gh-11694)', async function() {
     const testSchema = new mongoose.Schema({
       name: String,
       age: { type: String, select: true }
     });
 
     const Test = db.model('Test', testSchema);
-    Test.create({ name: 'Test', age: '42' }, function(error, doc) {
-      assert.ifError(error);
-      Test.findOne({ _id: doc._id }, 'name -age', function(error, doc) {
-        assert.equal(doc.name, 'Test');
-        assert.equal(doc.age, undefined);
-        done();
-      });
-    });
+    const doc = await Test.create({ name: 'Test', age: '42' });
+    const result = await Test.findOne({ _id: doc._id }).select('name -age');
+    assert.equal(result.name, 'Test');
+    assert.equal(result.age, undefined);
   });
 });

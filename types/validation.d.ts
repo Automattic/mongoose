71552828743b871b193e25aa616967e3f526d380
Nico <freezystem@gmail.com>
2023-01-24 02:22:36 +0100

    Allow this typing in validator functions
    Improve ability to type validator functions by allowing to define the `this` keyword.
    This avoid type errors when using `this` in the function body.

--- types/validation.d.ts
+++ types/validation.d.ts
@@ -2,32 +2,32 @@ declare module 'mongoose' {
 
   type SchemaValidator<T> = RegExp | [RegExp, string] | Function | [Function, string] | ValidateOpts<T> | ValidateOpts<T>[];
 
-  interface ValidatorProps {
+  interface ValidatorProps<T> {
     path: string;
-    value: any;
+    value: T;
   }
 
-  interface ValidatorMessageFn {
-    (props: ValidatorProps): string;
+  interface ValidatorMessageFn<T> {
+    (props: ValidatorProps<T>): string;
   }
 
-  interface ValidateFn<T> {
-    (value: T, props?: ValidatorProps & Record<string, any>): boolean;
+  interface ValidateFn<T, M = Model<unknown>> {
+    (this: M, value: T, props?: ValidatorProps<T> & Record<string, any>): boolean;
   }
 
-  interface LegacyAsyncValidateFn<T> {
-    (value: T, done: (result: boolean) => void): void;
+  interface LegacyAsyncValidateFn<T, M = Model<unknown>> {
+    (this: M, value: T, done: (result: boolean) => void): void;
   }
 
-  interface AsyncValidateFn<T> {
-    (value: T, props?: ValidatorProps & Record<string, any>): Promise<boolean>;
+  interface AsyncValidateFn<T, M = Model<unknown>> {
+    (this: M, value: T, props?: ValidatorProps<T> & Record<string, any>): Promise<boolean>;
   }
 
-  interface ValidateOpts<T> {
+  interface ValidateOpts<T, M = Model<unknown>> {
     msg?: string;
-    message?: string | ValidatorMessageFn;
+    message?: string | ValidatorMessageFn<T>;
     type?: string;
-    validator: ValidateFn<T> | LegacyAsyncValidateFn<T> | AsyncValidateFn<T>;
+    validator: ValidateFn<T, M> | LegacyAsyncValidateFn<T, M> | AsyncValidateFn<T, M>;
     propsParameter?: boolean;
   }
 }

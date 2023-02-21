4397d86ebc24e48dc9061c14b43d36a69e175c8b
Valeri Karpov <val@karpov.io>
2023-02-21 10:03:34 -0500

    test: print doc value for debugging

--- test/model.test.js
+++ test/model.test.js
@@ -109,6 +109,7 @@ describe('Model', function() {
 
     const doc = await Test.findOne();
 
+    console.log('Found', doc);
     assert.ok('last_name' in doc);
     assert.ok('_id' in doc);
     assert.ok('first_name' in doc._id);

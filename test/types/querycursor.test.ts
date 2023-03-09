b5bea04d99a4286ee3361e3295088670572fa46c
Katherine <krosenk729@gmail.com>
2023-03-09 10:43:40 -0500

    fix(document): adding eachAsync type test

--- test/types/querycursor.test.ts
+++ test/types/querycursor.test.ts
@@ -10,3 +10,10 @@ const Test = model<ITest>('Test', schema);
 
 Test.find().cursor().eachAsync(async(doc: ITest) => console.log(doc.name)).
   then(() => console.log('Done!'));
+
+Test.find().cursor().
+  eachAsync(async(doc: ITest, i) => {
+    expectType<Types.ObjectId>(doc._id);
+    expectType<number>(i);
+  }).
+  then(() => console.log('Done!'));

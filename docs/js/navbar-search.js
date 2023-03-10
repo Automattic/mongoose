14b8e6182439cabeb375c3a16b669432ad7534f6
hasezoey <hasezoey@gmail.com>
2023-03-10 12:39:32 +0100

    style: apply eslint config to "docs" and "docs/js" files

--- docs/js/navbar-search.js
+++ docs/js/navbar-search.js
@@ -1,19 +1,20 @@
-var defaultVersion = '6.x';
+'use strict';
+const defaultVersion = '6.x';
 
 (function() {
-  var versionFromUrl = window.location.pathname.match(/^\/docs\/(\d+\.x)/);
-  var version = versionFromUrl ? versionFromUrl[1] : defaultVersion;
+  const versionFromUrl = window.location.pathname.match(/^\/docs\/(\d+\.x)/);
+  const version = versionFromUrl ? versionFromUrl[1] : defaultVersion;
 
-  var searchPrefix = versionFromUrl ? '/docs/' + version + '/docs/' : '/docs/';
+  const searchPrefix = versionFromUrl ? '/docs/' + version + '/docs/' : '/docs/';
 
   document.getElementById('search-button-nav').onclick = function() {
-    var q = document.getElementById('search-input-nav').value;
+    const q = document.getElementById('search-input-nav').value;
     window.location.href = searchPrefix + 'search.html?q=' + encodeURIComponent(q);
   };
 
   document.getElementById('search-input-nav').onkeyup = function(ev) {
     if (ev.keyCode === 13) {
-      var q = document.getElementById('search-input-nav').value;
+      const q = document.getElementById('search-input-nav').value;
       window.location.href = searchPrefix + 'search.html?q=' + encodeURIComponent(q);
     }
   };

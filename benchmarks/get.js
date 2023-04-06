5b20ecd987fc5d6a6e51a6d0156184ace003c928
Valeri Karpov <val@karpov.io>
2023-04-06 11:12:55 -0400

    chore(benchmarks): add benchmark for #12953

--- benchmarks/get.js
+++ benchmarks/get.js
@@ -1,40 +1,72 @@
 'use strict';
 
-const get = require('../lib/helpers/get');
+const mongoose = require('../');
 
-let obj;
+run().catch(err => {
+  console.error(err);
+  process.exit(-1);
+});
 
-// Single string
-obj = {};
+async function run() {
+  const schema = new mongoose.Schema({
+    field1: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field2: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field3: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field4: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field5: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field6: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field7: {type: String, default: Math.random().toString(36).slice(2, 16)},
+    field8: {type: String, default: Math.random().toString(36).slice(2, 16)},
+  }, { versionKey: false });
+  const TestModel = mongoose.model('Test', schema);
+  
+  let tests = [];
+  for (let i = 0; i < 10000; i++) {
+    tests.push(new TestModel());
+  }
 
-let start = Date.now();
-for (let i = 0; i < 10000000; ++i) {
-  get(obj, 'test', null);
+  let loopStart = Date.now();
+
+  // run loop with mongoose objects
+  for (let k = 0; k < 100; k++) {
+    for (let test of tests) {
+      test.field1;
+      test.field2;
+      test.field3;
+      test.field4;
+      test.field5;
+      test.field6;
+      test.field7;
+      test.field8;
+    }
+  }
+
+  const results = {
+    'Model loop ms': Date.now() - loopStart
+  };
+
+  const plainTests = [];
+  for (let test of tests) {
+    plainTests.push(test.toObject());
+  }
+
+  loopStart = Date.now();
+
+  // run loop with plain objects
+  for (let k = 0; k < 100; k++) {
+    for (let test of plainTests) {
+      test.field1;
+      test.field2;
+      test.field3;
+      test.field4;
+      test.field5;
+      test.field6;
+      test.field7;
+      test.field8;
+    }
+  }
+
+  results['POJO loop ms'] = Date.now() - loopStart;
+
+  console.log(JSON.stringify(results, null, '  '));
 }
-console.log('Single string', Date.now() - start);
-
-// Array of length 1
-obj = {};
-start = Date.now();
-let arr = ['test'];
-for (let i = 0; i < 10000000; ++i) {
-  get(obj, arr, null);
-}
-console.log('Array of length 1', Date.now() - start);
-
-// String with dots
-obj = { a: { b: 1 } };
-start = Date.now();
-for (let i = 0; i < 10000000; ++i) {
-  get(obj, 'a.b', null);
-}
-console.log('String with dots', Date.now() - start);
-
-// Multi element array
-obj = { a: { b: 1 } };
-start = Date.now();
-arr = ['a', 'b'];
-for (let i = 0; i < 10000000; ++i) {
-  get(obj, arr, null);
-}
-console.log('Multi element array', Date.now() - start);

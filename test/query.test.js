b7b2e42f64a80f60463b6d6cc58cfd3d37c4b528
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-05-18 13:50:39 -0400

    remove console log

--- test/query.test.js
+++ test/query.test.js
@@ -3985,7 +3985,6 @@ describe('Query', function() {
     let lastOptions = {};
     m.set('debug', function(_coll, _method, ...args) {
       lastOptions = args[args.length - 2];
-      console.log('what is lastOptions', lastOptions)
     });
 
     const connDebug = m.createConnection(start.uri);

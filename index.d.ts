443092aa190f1a16b72e13ea0a0d6b2ee8239992
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-07-28 11:54:03 -0400

    add back `uniqueDocs`

--- index.d.ts
+++ index.d.ts
@@ -1225,6 +1225,7 @@ declare module 'mongoose' {
       minDistance?: number;
       query?: AnyObject;
       spherical?: boolean;
+      uniqueDocs?: boolean;
       /**
        * Deprecated. Use only with MondoDB below 4.2 (removed in 4.2)
        * @deprecated

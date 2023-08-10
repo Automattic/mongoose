d5cf4ca2c8c053e668ecbd88b080abd77b091eb5
hajjiri <hajjiri.hasan@gmail.com>
2023-08-10 16:00:01 +0200

    test: testing different cases with discriminators missing

--- test/helpers/query.castUpdate.test.js
+++ test/helpers/query.castUpdate.test.js
@@ -5,11 +5,37 @@ const assert = require('assert');
 const castUpdate = require('../../lib/helpers/query/castUpdate');
 
 describe('castUpdate', function() {
-  it('avoids adding `$each` if `$addToSet` on mixed array (gh-11284)', function() {
+  it('avoids adding `$each` if `$addToSet` on mixed array (gh-11284)', function () {
     const schema = new Schema({ test: [] });
     const obj = { $addToSet: { test: [1, 2, 3] } };
 
     const res = castUpdate(schema, obj);
     assert.deepEqual(res, { $addToSet: { test: [1, 2, 3] } });
+  });
+
+  it('casts the update correctly when target discriminator type is missing', function() {
+    const shapeSchema = Schema({ name: String }, { discriminatorKey: 'kind' });
+    const schema = Schema({ shape: shapeSchema });
+
+    schema
+      .path('shape')
+      .discriminator(
+        'gh8378_Circle',
+        Schema({ radius: String, color: String })
+      );
+
+    // schema
+    //   .path('shape')
+    //   .discriminator('gh8378_Square', Schema({ side: Number, color: String }));
+
+    const toBeUpdated = {
+      'shape.name': 'aName',
+      'shape.kind': 'gh8378_Square',
+      'shape.side': 4,
+      'shape.color': 'white'
+    };
+
+    const res = castUpdate(schema, { $set: toBeUpdated });
+    assert.deepEqual(res, { $set: toBeUpdated });
   });
 });
---
+++ test/model.findByIdAndUpdate.test.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/**
+ * Test dependencies.
+ */
+
+const start = require('./common');
+
+const assert = require('assert');
+const mongoose = start.mongoose;
+const Schema = mongoose.Schema;
+const util = require('./util');
+
+describe('model: findByIdAndUpdate:', function() {
+  let db;
+
+  before(function() {
+    db = start();
+  });
+
+  after(async function() {
+    await db.close();
+  });
+
+  beforeEach(() => db.deleteModel(/.*/));
+  afterEach(() => util.clearTestData(db));
+  afterEach(() => require('./util').stopRemainingOps(db));
+
+  it('returns the edited document with previous and target discriminators types defined', async function() {
+    const shapeSchema = Schema({ name: String }, { discriminatorKey: 'kind' });
+    const schema = Schema({ shape: shapeSchema });
+
+    schema.path('shape').discriminator('gh8378_Circle',
+      Schema({ radius: String, color: String }));
+    schema.path('shape').discriminator('gh8378_Square',
+      Schema({ side: Number, color: String }));
+
+    const MyModel = db.model('Test', schema);
+
+
+    let doc = await MyModel.create({
+      shape: {
+        kind: 'gh8378_Circle',
+        name: 'before',
+        radius: 5,
+        color: 'red'
+      }
+    });
+
+    doc = await MyModel.findByIdAndUpdate(doc._id, {
+      'shape.kind': 'gh8378_Square',
+      'shape.name': 'after',
+      'shape.side': 4,
+      'shape.color': 'white'
+    }, { new: true });
+    console.log('doc');
+    console.log(doc);
+    console.log('doc');
+
+    assert.equal(doc.shape.kind, 'gh8378_Square');
+    assert.equal(doc.shape.name, 'after');
+    assert.equal(doc.shape.side, 4);
+    assert.equal(doc.shape.color, 'white');
+  });
+
+  it('returns the edited document with only previous discriminator type defined', async function() {
+    const shapeSchema = Schema({ name: String }, { discriminatorKey: 'kind' });
+    const schema = Schema({ shape: shapeSchema });
+
+    schema.path('shape').discriminator('gh8378_Circle',
+      Schema({ radius: String, color: String }));
+    // schema.path('shape').discriminator('gh8378_Square',
+    //   Schema({ side: Number, color: String }));
+
+    const MyModel = db.model('Test', schema);
+
+
+    let doc = await MyModel.create({
+      shape: {
+        kind: 'gh8378_Circle',
+        name: 'before',
+        radius: 5,
+        color: 'red'
+      }
+    });
+
+    doc = await MyModel.findByIdAndUpdate(doc._id, {
+      'shape.kind': 'gh8378_Square',
+      'shape.name': 'after',
+      'shape.side': 4,
+      'shape.color': 'white'
+    }, { new: true });
+
+    assert.equal(doc.shape.kind, 'gh8378_Square');
+    assert.equal(doc.shape.name, 'after');
+    assert.equal(doc.shape.side, 4); // this assertion is failing, this property is not updated
+    assert.equal(doc.shape.color, 'white');
+  });
+});

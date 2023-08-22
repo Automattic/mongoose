17c31b7e2874fb1b425ffcbd4ab7d665608d1eb8
Valeri Karpov <val@karpov.io>
2023-08-22 18:00:54 -0400

    fix(model): make Model.bulkWrite() with empty array and ordered false not throw an error
    Fix #13664

--- test/model.test.js
+++ test/model.test.js
@@ -7856,9 +7856,32 @@ describe('Model', function() {
     const userSchema = new Schema({ name: String });
     const User = db.model('User', userSchema);
 
-    const err = await User.bulkWrite([], { ordered: false }).then(() => null, err => err);
-    assert.ok(err);
-    assert.equal(err.name, 'MongoInvalidArgumentError');
+    const res = await User.bulkWrite([], { ordered: false });
+    assert.deepEqual(
+      res,
+      {
+        result: {
+          ok: 1,
+          writeErrors: [],
+          writeConcernErrors: [],
+          insertedIds: [],
+          nInserted: 0,
+          nUpserted: 0,
+          nMatched: 0,
+          nModified: 0,
+          nRemoved: 0,
+          upserted: []
+        },
+        insertedCount: 0,
+        matchedCount: 0,
+        modifiedCount: 0,
+        deletedCount: 0,
+        upsertedCount: 0,
+        upsertedIds: {},
+        insertedIds: {},
+        n: 0
+      }
+    );
   });
 
   it('allows calling `create()` after `bulkWrite()` (gh-9350)', async function() {

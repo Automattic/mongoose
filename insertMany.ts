4339e3ef8f74ddebd53c63c7985fdb670ac83d11
donoftime2018 <donoftime2018@gmail.com>
2023-11-03 23:48:54 -0400

    fix to issue #13999 posted by ksyd9821

---
+++ insertMany.ts
@@ -0,0 +1,14 @@
+import * as mongoose from "mongoose";
+
+export class RepositoryBase<T> {
+    protected model: mongoose.Model<T & mongoose.Document>;
+
+    constructor(schemaModel: mongoose.Model<T & mongoose.Document>) {
+        this.model = schemaModel;
+    }
+
+    async insertMany(elems: T[]): Promise<T[]> {
+        elems = await this.model.insertMany(elems);
+        return elems;
+    }
+}

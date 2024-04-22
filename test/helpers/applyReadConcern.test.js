5efd3d7f79a0d089d13453b9d8937df7a890f6e7
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-04-22 14:51:47 -0400

    readConcern

---
+++ test/helpers/applyReadConcern.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('assert');
+const applyReadConcern = require('../../lib/helpers/schema/applyReadConcern');
+const start = require('../common');
+const { readConcern } = require('mquery/lib/utils');
+const mongoose = start.mongoose;
+
+describe('applyReadConcern', function() {
+  let db;
+  before(function() {
+    db = start();
+  });
+  after(async function() {
+    await db.close();
+  });
+  it('should not overwrite user specified read options (gh-14511)', async function() {
+    const options = { readConcern: { level: 'majority' } };
+    const testSchema = new mongoose.Schema({ name: String }, { readConcern: { level: 'majority '} });
+    applyReadConcern(testSchema, options);
+    assert.deepStrictEqual({ readConcern: { level: 'majority' } }, options);
+  });
+});

40ec8139fc0c3ee06e742b95c2ecc802e9bdc4ac
Valeri Karpov <val@karpov.io>
2024-05-24 10:03:43 -0400

    types: pass DocType down to subdocuments so `HydratedSingleSubdocument` and `HydratedArraySubdocument` `toObject()` returns correct type
    Fix #14601

--- test/types/subdocuments.test.ts
+++ test/types/subdocuments.test.ts
@@ -1,4 +1,13 @@
-import { Schema, model, Model, Document, Types } from 'mongoose';
+import {
+  Schema,
+  model,
+  Model,
+  Document,
+  Types,
+  HydratedArraySubdocument,
+  HydratedSingleSubdocument
+} from 'mongoose';
+import { expectAssignable } from 'tsd';
 
 const childSchema: Schema = new Schema({ name: String });
 
@@ -107,4 +116,36 @@ function gh13040(): void {
   user.products.forEach(product => {
     product.ownerDocument();
   });
+}
+
+function gh14601() {
+  interface ISub {
+    field1: string;
+  }
+  interface IMain {
+    f1: string;
+    f2: HydratedSingleSubdocument<ISub>;
+    f3: HydratedArraySubdocument<ISub>[];
+  }
+
+  const subSchema = new Schema({ field1: String }, { _id: false });
+
+  const mainSchema = new Schema({
+    f1: String,
+    f2: { type: subSchema },
+    f3: { type: [subSchema] }
+  });
+  const MainModel = model<IMain>('Main', mainSchema);
+
+  const item = new MainModel({
+    f1: 'test',
+    f2: { field1: 'test' },
+    f3: [{ field1: 'test' }]
+  });
+
+  const obj = item.toObject();
+
+  const obj2 = item.f2.toObject();
+
+  expectAssignable<{ _id: Types.ObjectId, field1: string }>(obj2);
 }

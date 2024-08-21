1b5fecc64fa51750ec69c34d32f8c7f0b140f93c
Valeri Karpov <val@karpov.io>
2024-08-21 07:30:14 -0400

    alternative fix for change stream tests re: #14177

--- test/model.test.js
+++ test/model.test.js
@@ -3680,6 +3680,9 @@ describe('Model', function() {
 
           await close;
           await db.close();
+          // Change stream may still emit "MongoAPIError: ChangeStream is closed" because change stream
+          // may still poll after close.
+          changeStream.on('error', () => {});
         });
 
         it('watch() close() closes the stream (gh-7022)', async function() {
@@ -3700,6 +3703,10 @@ describe('Model', function() {
           assert.strictEqual(closedData, true);
 
           await db.close();
+
+          // Change stream may still emit "MongoAPIError: ChangeStream is closed" because change stream
+          // may still poll after close.
+          changeStream.on('error', () => {});
         });
 
         it('bubbles up resumeTokenChanged events (gh-14349)', async function() {

60541e7480ac4ce2b3d69cc7a5f41e1d3322385e
Valeri Karpov <val@karpov.io>
2024-09-11 11:45:38 -0400

    types: add function args to setters property

--- types/schematypes.d.ts
+++ types/schematypes.d.ts
@@ -217,7 +217,7 @@ declare module 'mongoose' {
     static get(getter: (value: any) => any): void;
 
     /** Array containing default setters for all instances of this SchemaType */
-    static setters: Function[];
+    static setters: ((val?: unknown, priorVal?: unknown, doc?: Document<unknown>, options?: Record<string, any> | null) => unknown)[];
 
     /** The class that Mongoose uses internally to instantiate this SchemaType's `options` property. */
     OptionsConstructor: SchemaTypeOptions<T>;

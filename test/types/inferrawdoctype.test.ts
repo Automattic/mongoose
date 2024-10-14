ad7824f473e7423d74356c8d74d5b0163fce2222
Valeri Karpov <val@karpov.io>
2024-10-14 12:50:45 -0400

    test cleanup

--- test/types/inferrawdoctype.test.ts
+++ test/types/inferrawdoctype.test.ts
@@ -19,6 +19,39 @@ function gh14839() {
       required: true
     },
     subdoc: new AutoInferredSchema({
+      name: { type: String, required: true }
+    }),
+    docArr: [new AutoInferredSchema({ test: { type: String, required: true } })]
+  };
+
+  type UserType = InferRawDocType<typeof schemaDefinition>;
+  expectType<{
+    email: string,
+    password: string,
+    dateOfBirth: Date,
+    subdoc?: { name: string } | null,
+    docArr: { test: string }[]
+  }>({} as UserType);
+}
+
+function gh14954() {
+  const schemaDefinition = {
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true
+    },
+    subdoc: new AutoInferredSchema({
       name: { type: String, required: true },
       l2: new AutoInferredSchema({
         myProp: { type: Number, required: true }

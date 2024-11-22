a135e79c93a1836acc0639c7dfd3304565c11eda
Aditi Khare <aditi.khare@mongodb.com>
2024-11-22 15:02:53 -0500

    fix tests

--- test/encryption/encryption.test.js
+++ test/encryption/encryption.test.js
@@ -3,9 +3,14 @@
 const assert = require('assert');
 const EJSON = require('bson').EJSON;
 
-describe('setup check', () => {
-  it('environment variables are set', async function() {
-    const { local } = EJSON.parse(process.env.CSFLE_KMS_PROVIDERS || '{}');
-    assert.ok(local);
+describe('environmental variables', () => {
+  it('MONGODB_URI is set', async function() {
+    const uri = process.env.MONGODB_URI;
+    assert.ok(uri);
+  });
+
+  it('CRYPT_SHARED_LIB_PATH is set', async function() {
+    const shared_library_path = process.env.CRYPT_SHARED_LIB_PATH;
+    assert.ok(shared_library_path);
   });
 });

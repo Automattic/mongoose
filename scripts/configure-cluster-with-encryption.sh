e0bb056863cbac477d76fb634be99eb423a2cf77
Valeri Karpov <val@karpov.io>
2025-02-04 10:43:08 -0500

    chore: pin exact commit from Jan 15 and remove --depth=1 so we get all commits

--- scripts/configure-cluster-with-encryption.sh
+++ scripts/configure-cluster-with-encryption.sh
@@ -5,9 +5,9 @@
 # this script downloads all tools required to use FLE with mongodb, then starts a cluster of the provided configuration (sharded on 8.0 server)
 
 export CWD=$(pwd);
-export DRIVERS_TOOLS_PINNED_COMMIT=d8098d27d0a94afe6ed20b01d653404ba6dd3910;
+export DRIVERS_TOOLS_PINNED_COMMIT=35d0592c76f4f3d25a5607895eb21b491dd52543;
 
-# install extra dependency 
+# install extra dependency
 npm install mongodb-client-encryption
 
 # set up mongodb cluster and encryption configuration if the data/ folder does not exist
@@ -16,15 +16,15 @@ if [ ! -d "data" ]; then
   mkdir data
   cd data
 
-  # note: 
-    # we're using drivers-evergreen-tools which is a repo used by MongoDB drivers to start clusters for testing.  
+  # note:
+    # we're using drivers-evergreen-tools which is a repo used by MongoDB drivers to start clusters for testing.
     # if you'd like to make changes to the cluster settings, edit the exported variables below.
     # for configuration options for the exported variables, see here: https://github.com/mongodb-labs/drivers-evergreen-tools/blob/master/.evergreen/run-orchestration.sh
     # after this script is run, the data/ folder will notably contain the following:
       # 'mo-expansion.yml' file which contains for your cluster URI and crypt shared library path
       # 'drivers-evergreen-tools/mongodb/bin' which contain executables for other mongodb libraries such as mongocryptd, mongosh, and mongod
   if [ ! -d "drivers-evergreen-tools/" ]; then
-    git clone --depth=1 "https://github.com/mongodb-labs/drivers-evergreen-tools.git"
+    git clone "https://github.com/mongodb-labs/drivers-evergreen-tools.git"
     # pin stable commit
     git checkout $DRIVERS_TOOLS_PINNED_COMMIT
   fi
@@ -50,7 +50,7 @@ if [ ! -d "data" ]; then
 
   # start cluster
   (bash $DRIVERS_TOOLS/.evergreen/run-orchestration.sh) 1> /dev/null 2> /dev/null
- 
+
   echo 'Cluster Configuration Finished!'
 
   cd ..

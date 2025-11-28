feba6dcb1f0265c64df204bbab0fc6444e373927
Himanshu Mishra <himanshumishra4926@gmail.com>
2025-11-28 12:04:40 +0530

    fix: remove conflicting child projections when parent path excluded

---
+++ test/projection.pathCollision.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('../');
+const { Schema, model } = mongoose;
+const assert = require('assert');
+
+describe('Fix projection collision', function() {
+  it('removes children when parent is excluded', async function() {
+    const BarSchema = new Schema({
+      name: String,
+      subd: {
+        raw: { type: String, select: false },
+        clean: String
+      }
+    });
+
+    const Bar = model('Bar', BarSchema, 'bars');
+
+    const q = Bar.find({}).select('-subd');
+    const proj = q._fields;
+
+    assert.deepStrictEqual(proj, { subd: 0 });
+  });
+});

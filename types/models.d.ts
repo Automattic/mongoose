a5e89da341b612f1f0c110a7595be77e1b1d4b48
Hafez <a.hafez852@gmail.com>
2025-12-02 01:14:10 +0100

    fix: address PR feedback

--- types/models.d.ts
+++ types/models.d.ts
@@ -142,104 +142,45 @@ declare module 'mongoose' {
 
   interface RemoveOptions extends SessionOption, Omit<mongodb.DeleteOptions, 'session'> {}
 
-  export type AnyBulkWriteOperation<TSchema = AnyObject> = {
-    insertOne: InsertOneModel<TSchema>;
-  } | {
-    replaceOne: ReplaceOneModel<TSchema>;
-  } | {
-    updateOne: UpdateOneModel<TSchema>;
-  } | {
-    updateMany: UpdateManyModel<TSchema>;
-  } | {
-    deleteOne: DeleteOneModel<TSchema>;
-  } | {
-    deleteMany: DeleteManyModel<TSchema>;
-  };
-
-  export interface InsertOneModel<TSchema> {
-    document: mongodb.OptionalId<TSchema>;
+  interface MongooseBulkWritePerOperationOptions {
     /** Skip validation for this operation. */
     skipValidation?: boolean;
     /** When false, do not add timestamps. When true, overrides the `timestamps` option set in the `bulkWrite` options. */
     timestamps?: boolean;
   }
 
-  export interface ReplaceOneModel<TSchema = AnyObject> {
-    /** The filter to limit the replaced document. */
-    filter: FilterQuery<TSchema>;
-    /** The document with which to replace the matched document. */
-    replacement: mongodb.WithoutId<TSchema>;
-    /** Specifies a collation. */
-    collation?: mongodb.CollationOptions;
-    /** The index to use. If specified, then the query system will only consider plans using the hinted index. */
-    hint?: mongodb.Hint;
-    /** When true, creates a new document if no document matches the query. */
-    upsert?: boolean;
-    /** Skip validation for this operation. */
-    skipValidation?: boolean;
-    /** When false, do not add timestamps. When true, overrides the `timestamps` option set in the `bulkWrite` options. */
-    timestamps?: boolean;
-  }
-
-  export interface UpdateOneModel<TSchema = AnyObject> {
-    /** The filter to limit the updated documents. */
-    filter: FilterQuery<TSchema>;
-    /** A document or pipeline containing update operators. */
-    update: UpdateQuery<TSchema> | UpdateQuery<TSchema>[];
-    /** A set of filters specifying to which array elements an update should apply. */
-    arrayFilters?: AnyObject[];
-    /** Specifies a collation. */
-    collation?: mongodb.CollationOptions;
-    /** The index to use. If specified, then the query system will only consider plans using the hinted index. */
-    hint?: mongodb.Hint;
-    /** When true, creates a new document if no document matches the query. */
-    upsert?: boolean;
-    /** When false, do not add timestamps. When true, overrides the `timestamps` option set in the `bulkWrite` options. */
-    timestamps?: boolean;
+  interface MongooseBulkUpdatePerOperationOptions extends MongooseBulkWritePerOperationOptions {
     /** When true, allows updating fields that are marked as `immutable` in the schema. */
     overwriteImmutable?: boolean;
     /** When false, do not set default values on insert. */
     setDefaultsOnInsert?: boolean;
   }
 
-  export interface UpdateManyModel<TSchema = AnyObject> {
-    /** The filter to limit the updated documents. */
-    filter: FilterQuery<TSchema>;
-    /** A document or pipeline containing update operators. */
-    update: UpdateQuery<TSchema> | UpdateQuery<TSchema>[];
-    /** A set of filters specifying to which array elements an update should apply. */
-    arrayFilters?: AnyObject[];
-    /** Specifies a collation. */
-    collation?: mongodb.CollationOptions;
-    /** The index to use. If specified, then the query system will only consider plans using the hinted index. */
-    hint?: mongodb.Hint;
-    /** When true, creates a new document if no document matches the query. */
-    upsert?: boolean;
-    /** When false, do not add timestamps. When true, overrides the `timestamps` option set in the `bulkWrite` options. */
-    timestamps?: boolean;
-    /** When true, allows updating fields that are marked as `immutable` in the schema. */
-    overwriteImmutable?: boolean;
-    /** When false, do not set default values on insert. */
-    setDefaultsOnInsert?: boolean;
-  }
+  export type InsertOneModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.InsertOneModel<TSchema> & MongooseBulkWritePerOperationOptions;
 
-  export interface DeleteOneModel<TSchema = AnyObject> {
-    /** The filter to limit the deleted documents. */
-    filter: FilterQuery<TSchema>;
-    /** Specifies a collation. */
-    collation?: mongodb.CollationOptions;
-    /** The index to use. If specified, then the query system will only consider plans using the hinted index. */
-    hint?: mongodb.Hint;
-  }
+  export type ReplaceOneModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.ReplaceOneModel<TSchema> & MongooseBulkWritePerOperationOptions;
 
-  export interface DeleteManyModel<TSchema = AnyObject> {
-    /** The filter to limit the deleted documents. */
-    filter: FilterQuery<TSchema>;
-    /** Specifies a collation. */
-    collation?: mongodb.CollationOptions;
-    /** The index to use. If specified, then the query system will only consider plans using the hinted index. */
-    hint?: mongodb.Hint;
-  }
+  export type UpdateOneModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.UpdateOneModel<TSchema> & MongooseBulkUpdatePerOperationOptions;
+
+  export type UpdateManyModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.UpdateManyModel<TSchema> & MongooseBulkUpdatePerOperationOptions;
+
+  export type DeleteOneModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.DeleteOneModel<TSchema>;
+
+  export type DeleteManyModel<TSchema extends mongodb.Document = mongodb.Document> =
+    mongodb.DeleteManyModel<TSchema>;
+
+  export type AnyBulkWriteOperation<TSchema extends mongodb.Document = mongodb.Document> =
+    | { insertOne: InsertOneModel<TSchema> }
+    | { replaceOne: ReplaceOneModel<TSchema> }
+    | { updateOne: UpdateOneModel<TSchema> }
+    | { updateMany: UpdateManyModel<TSchema> }
+    | { deleteOne: DeleteOneModel<TSchema> }
+    | { deleteMany: DeleteManyModel<TSchema> };
 
   const Model: Model<any>;
 

a8cc3c52a55f96cbd1889dacc74d23b156d5554b
Hafez <a.hafez852@gmail.com>
2026-01-20 17:18:24 +0100

    fix(types): move `this` typing onto SchemaOptions.Virtuals via ThisType

--- types/virtuals.d.ts
+++ types/virtuals.d.ts
@@ -6,9 +6,9 @@ declare module 'mongoose' {
     };
 
   type TVirtualPathFN<DocType = {}, PathType = unknown, TInstanceMethods = {}, TReturn = unknown> =
-    <T = HydratedDocument<DocType, TInstanceMethods>>(this: Document<any, any, DocType> & DocType, value: PathType, virtual: VirtualType<T>, doc: Document<any, any, DocType> & DocType) => TReturn;
+    <T = HydratedDocument<DocType, TInstanceMethods>>(value: PathType, virtual: VirtualType<T>, doc: Document<any, any, DocType> & DocType) => TReturn;
 
   type SchemaOptionsVirtualsPropertyType<DocType = any, VirtualPaths = Record<any, unknown>, TInstanceMethods = {}> = {
     [K in keyof VirtualPaths]: VirtualPathFunctions<IsItRecordAndNotAny<DocType> extends true ? DocType : any, VirtualPaths[K], TInstanceMethods>
-  };
+  } & ThisType<Document<any, any, DocType> & DocType>;
 }

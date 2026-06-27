ff58e0bbeefaf6ce7a1d331aebb36454e71af734
Valeri Karpov <val@karpov.io>
2026-06-27 15:51:18 -0400

    perf(model): improved insertMany benchmark and improve performance of $validate()
    _getPathsToValidate() unnecessarily creates a new Set() when it doesn't
    need to (state machine already enforces uniqueness), which is a low
    hanging fruit for documents with only top level properties

--- benchmarks/insertManySimple.js
+++ benchmarks/insertManySimple.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('../');
+const { MongoClient } = require('mongodb');
 
 run().catch(err => {
   console.error(err);
@@ -8,31 +9,59 @@ run().catch(err => {
 });
 
 async function run() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/mongoose_benchmark');
+  const uri = 'mongodb://127.0.0.1:27017/mongoose_benchmark';
+
+  await mongoose.connect(uri);
   const FooSchema = new mongoose.Schema({ foo: String });
-  const FooModel = mongoose.model('Foo', FooSchema);
+  const FooModel = mongoose.model('Foo', FooSchema, 'foos');
+
+  const client = new MongoClient(uri);
+  await client.connect();
+  const db = client.db();
+  const fooCollection = db.collection('foos');
 
   if (!process.env.MONGOOSE_BENCHMARK_SKIP_SETUP) {
     await FooModel.deleteMany({});
+    await fooCollection.deleteMany({});
   }
 
   const numDocs = 1500;
-  const docs = [];
-  for (let i = 0; i < numDocs; ++i) {
-    docs.push({ foo: 'test foo ' + i });
+  const createDocs = iteration => {
+    const docs = [];
+    for (let i = 0; i < numDocs; ++i) {
+      docs.push({ foo: 'test foo ' + iteration + ' ' + i });
+    }
+    return docs;
+  };
+
+  for (let i = 0; i < 10; ++i) {
+    // Warm up
+    await FooModel.insertMany(createDocs(i));
   }
 
   const numIterations = 200;
-  const insertStart = Date.now();
-  for (let i = 0; i < numIterations; ++i) {
-    await FooModel.insertMany(docs);
+
+  for (let i = 0; i < 3; ++i) {
+    const driverInsertManyStart = Date.now();
+    for (let i = 0; i < numIterations; ++i) {
+      await fooCollection.insertMany(createDocs(i));
+    }
+    const driverInsertManyEnd = Date.now();
+
+    const mongooseInsertManyStart = Date.now();
+    for (let i = 0; i < numIterations; ++i) {
+      await FooModel.insertMany(createDocs(i), { lean: false });
+    }
+    const mongooseInsertManyEnd = Date.now();
+
+    const results = {
+      'Average mongoose insertMany time ms': +((mongooseInsertManyEnd - mongooseInsertManyStart) / numIterations).toFixed(2),
+      'Average driver insertMany time ms': +((driverInsertManyEnd - driverInsertManyStart) / numIterations).toFixed(2)
+    };
+
+    console.log(JSON.stringify(results, null, '  '));
   }
-  const insertEnd = Date.now();
 
-  const results = {
-    'Average insertMany time ms': +((insertEnd - insertStart) / numIterations).toFixed(2)
-  };
-
-  console.log(JSON.stringify(results, null, '  '));
+  await client.close();
   process.exit(0);
 }

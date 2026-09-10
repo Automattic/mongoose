ca1470a3145311763dcab567e1b651eb50ed1b33
Valeri Karpov <val@karpov.io>
2026-09-10 15:27:05 -0400

    remove a couple more unnecessary types

--- types/callback.d.ts
+++ types/callback.d.ts
@@ -1,8 +1,3 @@
 declare module 'mongoose' {
-  type CallbackError = NativeError | null;
-
-  type Callback<T = any> = (error: CallbackError, result: T) => void;
-
-  type CallbackWithoutResult = (error: CallbackError) => void;
-  type CallbackWithoutResultAndOptionalError = (error?: CallbackError) => void;
+  type CallbackWithoutResultAndOptionalError = (error?: NativeError | null) => void;
 }

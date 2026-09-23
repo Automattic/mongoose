53a4e81b8e4286e8dfc2cf4ac988e3fe7a4d566f
Hafez <a.hafez852@gmail.com>
2026-09-23 23:33:53 +0200

    fix(indexes): forward collection middleware selection during synchronization

--- types/indexes.d.ts
+++ types/indexes.d.ts
@@ -60,6 +60,7 @@ declare module 'mongoose' {
   type IndexDefinition = Record<string, IndexDirection>;
 
   interface SyncIndexesOptions extends mongodb.CreateIndexesOptions {
+    middleware?: boolean | SkipMiddlewareOptions;
     continueOnError?: boolean;
     hideIndexes?: boolean;
   }
